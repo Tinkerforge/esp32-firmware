f3004c25d477dd234c06a1089360ac838d07d7dd
Matthias Bolte <matthias@tinkerforge.com>
2021-11-12 15:10:20 +0100

    provisioning: Ensure to always flash the latest firmware

--- provisioning/provision_stage_0.sh
+++ provisioning/provision_stage_0.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+pushd ../../firmwares
+git pull
+popd
+
 parallel -j 9 -u python3 provision_stage_0.py ../../firmwares/bricks/warp_charger/brick_warp_charger_firmware_latest.bin '{}' ::: /dev/ttyUSB*
