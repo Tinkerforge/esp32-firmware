f92b006ce31fc5aa2815b949a415f37fc80f1015
Matthias Bolte <matthias@tinkerforge.com>
2021-11-22 16:45:20 +0100

    Replace custom merge_bins.py script with esptool call

--- software/merge_firmware_hook.py
+++ software/merge_firmware_hook.py
@@ -32,12 +32,18 @@ env.AddPostAction(
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.bin",
     env.VerboseAction(" ".join([
-        "python3", "merge_bins.py",
+        "esptool.py",
+        "--chip", "ESP32",
+        "merge_bin",
+        "-o", "build/{}_merged.bin".format(firmware_basename),
+        "--flash_mode", "dio",
+        "--flash_size", flash_size,
+        "--flash_freq", "40m",
+        "--target-offset", "0x1000",
         "0x1000", "bootloader_dio_40m.bin",
         "0x8000", "$BUILD_DIR/partitions.bin",
         "0xd000", "build/fw_info.bin",
         "0xe000", "boot_app0.bin",
-        "0x10000", "$BUILD_DIR/${PROGNAME}.bin",
-        "build/{}_merged.bin".format(firmware_basename)
+        "0x10000", "$BUILD_DIR/${PROGNAME}.bin"
     ]), "Merging firmware.bin")
 )
