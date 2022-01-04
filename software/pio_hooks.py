Tinkerforge/esp32-firmware

commit 3b100bff1612bb81f148e8b0041e73bc5e0d1ab7

web: Extend frontend module .scss support
Replace specific support for branding.scss with generic support
for pre/post.scss per frontend module.

software/pio_hooks.py
@@ -285,6 +285,8 @@ def main():
     content_entries = []
     status_entries = []
     main_ts_entries = []
+    pre_scss_entries = []
+    post_scss_entries = []
     frontend_modules = [FlavoredName(x).get() for x in env.GetProjectOption("frontend_modules").splitlines()]
     translation = collect_translation('web')
 
@@ -308,15 +310,6 @@ def main():
 
                 shutil.copy(img_source_path, img_target_path)
 
-        if os.path.exists(os.path.join(mod_path, 'scss')):
-            for scss_name in os.listdir(os.path.join(mod_path, 'scss')):
-                scss_source_path = os.path.join(mod_path, 'scss', scss_name)
-                scss_target_path = os.path.join('web', 'src', 'scss', 'modules', scss_name)
-
-                assert not os.path.exists(scss_target_path), 'scss collision ' + scss_source_path + ' -> ' + scss_target_path
-
-                shutil.copy(scss_source_path, scss_target_path)
-
         if os.path.exists(os.path.join(mod_path, 'navbar.html')):
             with open(os.path.join(mod_path, 'navbar.html')) as f:
                 navbar_entries.append(f.read())
@@ -332,6 +325,11 @@ def main():
         if os.path.exists(os.path.join(mod_path, 'main.ts')):
             main_ts_entries.append(frontend_module)
             shutil.copy(os.path.join(mod_path, 'main.ts'), os.path.join("web", "src", "ts", "modules", frontend_module.under + ".ts"))
+
+        for phase, scss_entries in [('pre', pre_scss_entries), ('post', post_scss_entries)]:
+            if os.path.exists(os.path.join(mod_path, phase + '.scss')):
+                scss_entries.append(frontend_module)
+                shutil.copy(os.path.join(mod_path, phase + '.scss'), os.path.join("web", "src", "scss", "modules", phase + "_" + frontend_module.under + ".scss"))
 
         update_translation(translation, collect_translation(mod_path))
         update_translation(translation, collect_translation(mod_path, override=True), override=True)
@@ -370,6 +368,11 @@ def main():
         '{{{modules}}}': ', '.join([x.under for x in main_ts_entries]),
         '{{{translation_imports}}}': '\n'.join(['import {{translation_{0}}} from "./ts/translation_{0}";'.format(x) for x in sorted(translation)]),
         '{{{translation_adds}}}': '\n'.join(["    translator.add('{0}', translation_{0});".format(x) for x in sorted(translation)])
+    })
+
+    specialize_template(os.path.join("web", "main.scss.template"), os.path.join("web", "src", "main.scss"), {
+        '{{{module_pre_imports}}}': '\n'.join(['@import "modules/pre_{0}";'.format(x.under) for x in pre_scss_entries]),
+        '{{{module_post_imports}}}': '\n'.join(['@import "modules/post_{0}";'.format(x.under) for x in post_scss_entries])
     })
 
     # Check translation completeness

software/web/main.scss.template
@@ -28,7 +28,7 @@ $container-max-widths: (
         xl: 1230px
 );
 
-@import "modules/branding";
+{{{module_pre_imports}}}
 
 @import "../../node_modules/bootstrap/scss/mixins";
 @import "../../node_modules/bootstrap/scss/root";
@@ -397,3 +397,5 @@ body {
         margin-right: .5rem !important;
     }
 }
+
+{{{module_post_imports}}}

software/.gitignore
@@ -19,6 +19,7 @@ web/node_modules/
 web/assets/
 web/dist/
 web/src/main.ts
+web/src/main.scss
 web/src/index.html
 web/src/img/modules/
 web/src/scss/modules/

commit 1d8a78d26322f29859ac77748f3dba1f3697f227

web: Fix .scss paths
After broken reorganization in 3b100bff1612bb81f148e8b0041e73bc5e0d1ab7.

software/pio_hooks.py
@@ -371,8 +371,8 @@ def main():
     })
 
     specialize_template(os.path.join("web", "main.scss.template"), os.path.join("web", "src", "main.scss"), {
-        '{{{module_pre_imports}}}': '\n'.join(['@import "modules/pre_{0}";'.format(x.under) for x in pre_scss_entries]),
-        '{{{module_post_imports}}}': '\n'.join(['@import "modules/post_{0}";'.format(x.under) for x in post_scss_entries])
+        '{{{module_pre_imports}}}': '\n'.join(['@import "scss/modules/pre_{0}";'.format(x.under) for x in pre_scss_entries]),
+        '{{{module_post_imports}}}': '\n'.join(['@import "scss/modules/post_{0}";'.format(x.under) for x in post_scss_entries])
     })
 
     # Check translation completeness

software/web/gulpfile.js
@@ -81,7 +81,7 @@ gulp.task("sass", function () {
     const cssnano = require("cssnano");
 
     return gulp
-        .src("src/scss/*.scss")
+        .src("src/main.scss")
         .pipe(sass()) // Compile sass to css
         .pipe(
             postcss([

software/web/main.scss.template
@@ -10,8 +10,8 @@ $body-color: black;
 $gray-700: black;
 $gray-900: black;
 
-@import "../../node_modules/bootstrap/scss/functions";
-@import "../../node_modules/bootstrap/scss/variables";
+@import "../node_modules/bootstrap/scss/functions";
+@import "../node_modules/bootstrap/scss/variables";
 
 $grid-breakpoints: (
         xs: 0,
@@ -30,43 +30,43 @@ $container-max-widths: (
 
 {{{module_pre_imports}}}
 
-@import "../../node_modules/bootstrap/scss/mixins";
-@import "../../node_modules/bootstrap/scss/root";
-@import "../../node_modules/bootstrap/scss/reboot";
-@import "../../node_modules/bootstrap/scss/type";
-//@import "../../node_modules/bootstrap/scss/images";
-//@import "../../node_modules/bootstrap/scss/code";
-@import "../../node_modules/bootstrap/scss/grid";
-@import "../../node_modules/bootstrap/scss/tables";
-@import "../../node_modules/bootstrap/scss/forms";
-@import "../../node_modules/bootstrap/scss/buttons";
-@import "../../node_modules/bootstrap/scss/transitions";
-@import "../../node_modules/bootstrap/scss/dropdown";
-@import "../../node_modules/bootstrap/scss/button-group";
-@import "../../node_modules/bootstrap/scss/input-group";
-@import "../../node_modules/bootstrap/scss/custom-forms";
-@import "../../node_modules/bootstrap/scss/nav";
-@import "../../node_modules/bootstrap/scss/navbar";
-@import "../../node_modules/bootstrap/scss/card";
-//@import "../../node_modules/bootstrap/scss/breadcrumb";
-//@import "../../node_modules/bootstrap/scss/pagination";
-//@import "../../node_modules/bootstrap/scss/badge";
-//@import "../../node_modules/bootstrap/scss/jumbotron";
-@import "../../node_modules/bootstrap/scss/alert";
-@import "../../node_modules/bootstrap/scss/progress";
-//@import "../../node_modules/bootstrap/scss/media";
-@import "../../node_modules/bootstrap/scss/list-group";
-@import "../../node_modules/bootstrap/scss/close";
-//@import "../../node_modules/bootstrap/scss/toasts";
-@import "../../node_modules/bootstrap/scss/modal";
-//@import "../../node_modules/bootstrap/scss/tooltip";
-//@import "../../node_modules/bootstrap/scss/popover";
-//@import "../../node_modules/bootstrap/scss/carousel";
-@import "../../node_modules/bootstrap/scss/spinners";
-@import "../../node_modules/bootstrap/scss/utilities";
-//@import "../../node_modules/bootstrap/scss/print";
+@import "../node_modules/bootstrap/scss/mixins";
+@import "../node_modules/bootstrap/scss/root";
+@import "../node_modules/bootstrap/scss/reboot";
+@import "../node_modules/bootstrap/scss/type";
+//@import "../node_modules/bootstrap/scss/images";
+//@import "../node_modules/bootstrap/scss/code";
+@import "../node_modules/bootstrap/scss/grid";
+@import "../node_modules/bootstrap/scss/tables";
+@import "../node_modules/bootstrap/scss/forms";
+@import "../node_modules/bootstrap/scss/buttons";
+@import "../node_modules/bootstrap/scss/transitions";
+@import "../node_modules/bootstrap/scss/dropdown";
+@import "../node_modules/bootstrap/scss/button-group";
+@import "../node_modules/bootstrap/scss/input-group";
+@import "../node_modules/bootstrap/scss/custom-forms";
+@import "../node_modules/bootstrap/scss/nav";
+@import "../node_modules/bootstrap/scss/navbar";
+@import "../node_modules/bootstrap/scss/card";
+//@import "../node_modules/bootstrap/scss/breadcrumb";
+//@import "../node_modules/bootstrap/scss/pagination";
+//@import "../node_modules/bootstrap/scss/badge";
+//@import "../node_modules/bootstrap/scss/jumbotron";
+@import "../node_modules/bootstrap/scss/alert";
+@import "../node_modules/bootstrap/scss/progress";
+//@import "../node_modules/bootstrap/scss/media";
+@import "../node_modules/bootstrap/scss/list-group";
+@import "../node_modules/bootstrap/scss/close";
+//@import "../node_modules/bootstrap/scss/toasts";
+@import "../node_modules/bootstrap/scss/modal";
+//@import "../node_modules/bootstrap/scss/tooltip";
+//@import "../node_modules/bootstrap/scss/popover";
+//@import "../node_modules/bootstrap/scss/carousel";
+@import "../node_modules/bootstrap/scss/spinners";
+@import "../node_modules/bootstrap/scss/utilities";
+//@import "../node_modules/bootstrap/scss/print";
 
-@import "../../node_modules/chartist/dist/scss/chartist.scss";
+@import "../node_modules/chartist/dist/scss/chartist.scss";
 
 .borderless-form-control {
     @extend .form-control;