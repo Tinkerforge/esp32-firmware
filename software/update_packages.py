5bd64fdf0fb726718e895cc6423a14e47385cb7a
Matthias Bolte <matthias@tinkerforge.com>
2022-01-11 15:24:47 +0100

    Cache platform packages locally
    PlatformIO will reclone the platform packages every time an
    environment with a different version for the same platform
    package is build.
    
    Avoid this recloning by caching all version of the platform
    packages locally.

---
+++ software/update_packages.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+
+PACKAGES = [
+    ('arduino-esp32', 'warp-1.3.1', 'https://github.com/Tinkerforge/arduino-esp32'),
+    ('arduino-esp32', 'warp2-1.1.1', 'https://github.com/Tinkerforge/arduino-esp32'),
+]
+
+packages_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'packages')
+
+os.makedirs(packages_dir, exist_ok=True)
+
+for package in PACKAGES:
+    base, branch, url = package
+    name = '{0}-{1}'.format(base, branch)
+    marker_path = os.path.join(packages_dir, name, 'tinkerforge.marker')
+
+    if os.path.exists(marker_path):
+        print('skipping {0}'.format(name))
+        continue
+
+    zip_path = os.path.join(packages_dir, '{0}.zip'.format(name))
+
+    if not os.path.exists(zip_path):
+        print('downloading {0}'.format(name))
+
+        try:
+            os.remove(zip_path + '.tmp')
+        except FileNotFoundError:
+            pass
+
+        urlretrieve('{0}/archive/refs/heads/{1}.zip'.format(url, branch), zip_path + '.tmp')
+
+        os.rename(zip_path + '.tmp', zip_path)
+
+    print('unpacking {0}'.format(name))
+
+    with ZipFile(zip_path) as f:
+        prefix = name + '/'
+
+        for n in f.namelist():
+            if not n.startswith(prefix):
+                print('error: {0} has malformed entry {1}'.format(name, n))
+                sys.exit(1)
+
+        f.extractall(packages_dir)
+
+    with open(marker_path, 'w') as f:
+        pass
