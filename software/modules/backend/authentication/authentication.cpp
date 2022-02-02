bfd7ecf8b73f81fb52369dfcfbdfe0299f67a936
Erik Fleckstein <erik@tinkerforge.com>
2022-02-02 11:34:10 +0100

    authentication: Move login page into own module.

--- software/modules/backend/authentication/authentication.cpp
+++ software/modules/backend/authentication/authentication.cpp
@@ -24,9 +24,6 @@
 #include "api.h"
 #include "event_log.h"
 #include "web_server.h"
-#include "build.h"
-
-#include "login.html.h"
 
 #include "digest_auth.h"
 
@@ -89,45 +86,6 @@ void Authentication::setup()
 void Authentication::register_urls()
 {
     api.addPersistentConfig("authentication/config", &authentication_config, {"password"}, 10000);
-
-    server.onNotAuthorized([](WebServerRequest request) {
-        if (request.uri() == "/") {
-            // Safari does not support an unauthenticated login page and an authenticated main page on the same url,
-            // as it does not proactively send the credentials if the same url is known to have an unauthenticated
-            // version.
-            const String &user_agent = request.header("User-Agent");
-            bool is_safari = user_agent.indexOf("Safari/") >= 0 && user_agent.indexOf("Version/") >= 0 && user_agent.indexOf("Chrome/") == -1 && user_agent.indexOf("Chromium/") == -1;
-            if (is_safari) {
-                request.requestAuthentication();
-                return;
-            }
-
-            request.addResponseHeader("Content-Encoding", "gzip");
-            request.addResponseHeader("ETag", BUILD_TIMESTAMP_HEX_STR);
-            request.send(200, "text/html", login_html_gz, login_html_gz_len);
-        } else if (request.uri() == "/login_state") {
-            // Same reasoning as above. If we don't force Safari, it does not send credentials, which breaks the login_state check.
-            const String &user_agent = request.header("User-Agent");
-            bool is_safari = user_agent.indexOf("Safari/") >= 0 && user_agent.indexOf("Version/") >= 0 && user_agent.indexOf("Chrome/") == -1 && user_agent.indexOf("Chromium/") == -1;
-            if (is_safari) {
-                request.requestAuthentication();
-                return;
-            }
-
-            request.send(200, "text/plain", "Not logged in");
-        } else {
-            request.requestAuthentication();
-            return;
-        }
-    });
-
-    server.on("/credential_check", HTTP_GET, [](WebServerRequest request) {
-        request.send(200, "text/plain", "Credentials okay");
-    });
-
-    server.on("/login_state", HTTP_GET, [](WebServerRequest request) {
-        request.send(200, "text/plain", "Logged in");
-    });
 }
 
 void Authentication::loop()
