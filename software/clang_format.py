f1170ab9c64afe1a5084a1922a365edb01a9a548
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 15:42:11 +0200

    clang-format: Skip bindings directory and files to be embedded.

--- software/clang_format.py
+++ software/clang_format.py
@@ -3,8 +3,13 @@
 import os
 
 for root, dirs, files in sorted(os.walk('src')):
+    if "bindings" in root:
+        continue
+
     for name in sorted(files):
         if not name.endswith('.c') and not name.endswith('.cpp') and not name.endswith('.h'):
+            continue
+        if ".embedded" in name:
             continue
 
         path = os.path.join(root, name)
