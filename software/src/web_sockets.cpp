a6ca582acd64612a10a1f1147a5eb54c3b536765
Erik Fleckstein <erik@tinkerforge.com>
2022-05-02 12:03:15 +0200

    web_sockets: Temporarily add debug API.

--- software/src/web_sockets.cpp
+++ software/src/web_sockets.cpp
@@ -429,6 +429,16 @@ void WebSockets::start(const char *uri)
     task_scheduler.scheduleWithFixedDelay([this](){
         checkActiveClients();
     }, 100, 100);
+
+    server.on("/info/ws", HTTP_GET, [this](WebServerRequest request) {
+        std::lock_guard<std::recursive_mutex> lock{work_queue_mutex};
+        std::lock_guard<std::recursive_mutex> lock2{keep_alive_mutex};
+        logger.printfln("keep_alive_fds   %d %d %d %d %d", keep_alive_fds[0], keep_alive_fds[1], keep_alive_fds[2], keep_alive_fds[3], keep_alive_fds[4]);
+        logger.printfln("keep_alive_pongs %u %u %u %u %u", keep_alive_last_pong[0], keep_alive_last_pong[1], keep_alive_last_pong[2], keep_alive_last_pong[3], keep_alive_last_pong[4]);
+        logger.printfln("queue_len %u", work_queue.size());
+        logger.printfln("worker_active %s", worker_active ? "yes" : "no");
+        request.send(200);
+    });
 }
 
 void WebSockets::onConnect(std::function<void(WebSocketsClient)> fn)
