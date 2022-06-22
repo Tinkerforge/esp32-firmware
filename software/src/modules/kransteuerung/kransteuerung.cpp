64ab59b7f2fcae7642dc6f0f79a5ec5d1253e796
Erik Fleckstein <erik@tinkerforge.com>
2022-06-22 14:46:06 +0200

    Add kransteuerung. For internal use only.

---
+++ software/src/modules/kransteuerung/kransteuerung.cpp
@@ -0,0 +1,117 @@
+/* warp-charger
+ * Copyright (C) 2020-2021 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "kransteuerung.h"
+
+#include "bindings/errors.h"
+
+#include "api.h"
+#include "event_log.h"
+#include "task_scheduler.h"
+#include "tools.h"
+
+extern EventLog logger;
+
+extern TaskScheduler task_scheduler;
+extern TF_HAL hal;
+
+Kransteuerung::Kransteuerung()
+{
+
+}
+
+void Kransteuerung::button_pressed_handler(bool left, uint8_t button_l, uint8_t button_r, uint8_t led_l, uint8_t led_r)
+{
+    bool value[4] = {false};
+    tf_industrial_quad_relay_v2_get_value(&relay, value);
+
+    if (button_l != last_button_states[left ? 0 : 2] && button_l == TF_DUAL_BUTTON_V2_BUTTON_STATE_PRESSED) {
+        tf_industrial_quad_relay_v2_set_selected_value(&relay, (left ? 0 : 2), !value[(left ? 0 : 2)]);
+    }
+
+    if (button_r != last_button_states[left ? 1 : 3] && button_r == TF_DUAL_BUTTON_V2_BUTTON_STATE_PRESSED) {
+        tf_industrial_quad_relay_v2_set_selected_value(&relay, (left ? 1 : 3), !value[(left ? 1 : 3)]);
+    }
+
+    last_button_states[left ? 0 : 2] = button_l;
+    last_button_states[left ? 1 : 3] = button_r;
+}
+
+void Kransteuerung::setup()
+{
+    int result = tf_dual_button_v2_create(&left, "D", &hal);
+    if(result != TF_E_OK) {
+        logger.printfln("Failed to initialize left Dual Button 2.0 Bricklet.");
+        return;
+    }
+
+    result = tf_dual_button_v2_create(&right, "C", &hal);
+    if(result != TF_E_OK) {
+        logger.printfln("Failed to initialize right Dual Button 2.0 Bricklet.");
+        return;
+    }
+
+    result = tf_industrial_quad_relay_v2_create(&relay, "E", &hal);
+    if(result != TF_E_OK) {
+        logger.printfln("Failed to initialize Industrial Quad Relay 2.0 Bricklet.");
+        return;
+    }
+
+    bool value[4];
+    tf_industrial_quad_relay_v2_get_value(&relay, value);
+
+    tf_dual_button_v2_set_led_state(&left,  TF_DUAL_BUTTON_V2_LED_STATE_ON, value[1] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF);
+    tf_dual_button_v2_set_led_state(&right, value[2] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF, value[3] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF);
+
+
+    tf_dual_button_v2_register_state_changed_callback(&left, [](struct TF_DualButtonV2 *device, uint8_t button_l, uint8_t button_r, uint8_t led_l, uint8_t led_r, void *user_data){
+        task_scheduler.scheduleOnce([button_l, button_r, led_l, led_r, user_data](){
+            ((Kransteuerung*)user_data)->button_pressed_handler(true, button_l, button_r, led_l, led_r);
+        }, 0);
+    }, this);
+
+    tf_dual_button_v2_register_state_changed_callback(&right, [](struct TF_DualButtonV2 *device, uint8_t button_l, uint8_t button_r, uint8_t led_l, uint8_t led_r, void *user_data){
+        task_scheduler.scheduleOnce([button_l, button_r, led_l, led_r, user_data](){
+            ((Kransteuerung*)user_data)->button_pressed_handler(false, button_l, button_r, led_l, led_r);
+        }, 0);
+    }, this);
+
+    tf_dual_button_v2_set_state_changed_callback_configuration(&left, true);
+    tf_dual_button_v2_set_state_changed_callback_configuration(&right, true);
+
+    task_scheduler.scheduleWithFixedDelay([this](){
+        bool value[4] = {false};
+        tf_industrial_quad_relay_v2_get_value(&relay, value);
+        tf_dual_button_v2_set_led_state(&left,  TF_DUAL_BUTTON_V2_LED_STATE_ON, value[1] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF);
+        tf_dual_button_v2_set_led_state(&right, value[2] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF, value[3] ? TF_DUAL_BUTTON_V2_LED_STATE_ON : TF_DUAL_BUTTON_V2_LED_STATE_OFF);
+    }, 100, 100);
+
+    initialized = true;
+}
+
+void Kransteuerung::register_urls()
+{
+
+}
+
+
+void Kransteuerung::loop()
+{
+
+}
