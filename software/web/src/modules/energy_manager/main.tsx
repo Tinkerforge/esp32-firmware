a19c2cb9f51188770086430ad7de6f9e68243716
Matthias Bolte <matthias@tinkerforge.com>
2022-08-16 14:41:03 +0200

    em: Adapt to new config handling

--- software/web/src/modules/energy_manager/main.tsx
+++ software/web/src/modules/energy_manager/main.tsx
@@ -40,30 +40,12 @@ function update_energy_manager_state() {
     util.update_button_group(`btn_group_gpio2`, state.gpio_output_state ? 1 : 0);
     $('#state_input_configuration_0').val(state.gpio_input_configuration[0]);
     $('#state_input_configuration_1').val(state.gpio_input_configuration[1]);
-    $('#state_input_voltage').val(state.input_voltage);
+    $('#state_input_voltage').val(`${state.input_voltage} mV`);
     $('#state_contactor_check').val(state.contactor_check_state);
-
-    update_energy_manager_html_visibility();
 }
 
 function update_energy_manager_config() {
-    let config = API.get('energy_manager/config');
-    console.log("update_energy_manager_config", config);
-
-    $('#energy_manager_excess_charging_enable').prop('checked', config.excess_charging_enable);
-    $('#energy_manager_phase_switching_config').val(config.phase_switching.toString()).trigger('change');
-    $('#energy_manager_mains_power_reception').val(config.mains_power_reception);
-    $('#energy_manager_minimum_charging').val(config.minimum_charging);
-    $('#energy_manager_relay_config').val(config.relay_config.toString()).trigger('change');
-    $('#energy_manager_relay_config_if').val(config.relay_config_if.toString()).trigger('change');
-    $('#energy_manager_relay_config_is').val(config.relay_config_is.toString()).trigger('change');
-    $('#energy_manager_relay_config_then').val(config.relay_config_then.toString()).trigger('change');
-    $('#energy_manager_input3_config').val(config.input3_config.toString()).trigger('change');
-    $('#energy_manager_input3_config_if').val(config.input3_config_if.toString()).trigger('change');
-    $('#energy_manager_input3_config_then').val(config.input3_config_then.toString()).trigger('change');
-    $('#energy_manager_input4_config').val(config.input4_config.toString()).trigger('change');
-    $('#energy_manager_input4_config_if').val(config.input4_config_if.toString()).trigger('change');
-    $('#energy_manager_input4_config_then').val(config.input4_config_then.toString()).trigger('change');
+    API.default_updater('energy_manager/config');
 }
 
 // Only show the relevant html elements, drop-down boxes and options
@@ -91,14 +73,14 @@ function update_energy_manager_html_visibility() {
     }
 
     // Update relay section
-    if($('#energy_manager_relay_config').val()== '1') {
-        $('#energy_manager_relay_rules').collapse('show');
+    if($('#energy_manager_config_relay_config').val()== '1') {
+        $('#energy_manager_config_relay_rules').collapse('show');
     } else {
-        $('#energy_manager_relay_rules').collapse('hide');
+        $('#energy_manager_config_relay_rules').collapse('hide');
     }
 
-    let relay_config_is_dd = $('#energy_manager_relay_config_is')
-    let relay_config_if = $('#energy_manager_relay_config_if').val();
+    let relay_config_is_dd = $('#energy_manager_config_relay_config_is')
+    let relay_config_if = $('#energy_manager_config_relay_config_if').val();
     if((relay_config_if == '0') || (relay_config_if == '1')) {
         update_options(relay_config_is_dd, [{"value": 0, name: "high"}, {"value": 1, name: "low"}]);
     } else if(relay_config_if == '2') {
@@ -108,59 +90,17 @@ function update_energy_manager_html_visibility() {
     }
 
     // Update input section
-    if($('#energy_manager_input3_config').val() == "1") {
-        $('#energy_manager_input3_rules').collapse('show');
+    if($('#energy_manager_config_input3_config').val() == "1") {
+        $('#energy_manager_config_input3_rules').collapse('show');
     } else {
-        $('#energy_manager_input3_rules').collapse('hide');
+        $('#energy_manager_config_input3_rules').collapse('hide');
     }
 
-    if($('#energy_manager_input4_config').val() == "1") {
-        $('#energy_manager_input4_rules').collapse('show');
+    if($('#energy_manager_config_input4_config').val() == "1") {
+        $('#energy_manager_config_input4_rules').collapse('show');
     } else {
-        $('#energy_manager_input4_rules').collapse('hide');
+        $('#energy_manager_config_input4_rules').collapse('hide');
     }
-}
-
-function save_energy_manager_config() {
-    console.log("save_energy_manager_config");
-
-    let excess_charging_enable: boolean = $('#energy_manager_excess_charging_enable').is(":checked");
-    let phase_switching: number         = Number($('#energy_manager_phase_switching_config').val());
-    let mains_power_reception: number   = Number($('#energy_manager_mains_power_reception').val());
-    let minimum_charging: number        = Number($('#energy_manager_minimum_charging').val());
-    let relay_config: number            = Number($('#energy_manager_relay_config').val());
-    let relay_config_if: number         = Number($('#energy_manager_relay_config_if').val());
-    let relay_config_is: number         = Number($('#energy_manager_relay_config_is').val());
-    let relay_config_then: number       = Number($('#energy_manager_relay_config_then').val());
-    let input3_config: number           = Number($('#energy_manager_input3_config').val());
-    let input3_config_if: number        = Number($('#energy_manager_input3_config_if').val());
-    let input3_config_then: number      = Number($('#energy_manager_input3_config_then').val());
-    let input4_config: number           = Number($('#energy_manager_input4_config').val());
-    let input4_config_if: number        = Number($('#energy_manager_input4_config_if').val());
-    let input4_config_then: number      = Number($('#energy_manager_input4_config_then').val());
-    console.log(excess_charging_enable, phase_switching, mains_power_reception,minimum_charging, relay_config, relay_config_if, relay_config_is, relay_config_then,
-                input3_config, input3_config_if, input3_config_then, input4_config, input4_config_if, input4_config_then);
-
-
-    API.save('energy_manager/config', {
-            excess_charging_enable: excess_charging_enable,
-            phase_switching:        phase_switching,
-            mains_power_reception:  mains_power_reception,
-            minimum_charging:       minimum_charging,
-            relay_config:           relay_config,
-            relay_config_if:        relay_config_if,
-            relay_config_is:        relay_config_is,
-            relay_config_then:      relay_config_then,
-            input3_config:          input3_config,
-            input3_config_if:       input3_config_if,
-            input3_config_then:     input3_config_then,
-            input4_config:          input4_config,
-            input4_config_if:       input4_config_if,
-            input4_config_then:     input4_config_then
-        },
-        __("energy_manager.script.config_failed"),
-        __("energy_manager.script.reboot_content_changed")
-    );
 }
 
 let debug_log = "";
@@ -218,7 +158,6 @@ function debug_start() {
         });
 }
 
-
 function debug_stop() {
     let status = $('#debug_label')[0] as HTMLInputElement;
 
@@ -257,25 +196,16 @@ export function init() {
     $("#debug_start").on("click", debug_start);
     $("#debug_stop").on("click", debug_stop);
 
-    $("#energy_manager_relay_config, \
-       #energy_manager_relay_config_if, \
-       #energy_manager_input3_config, \
-       #energy_manager_input4_config"
+    API.register_config_form('energy_manager/config', {
+            error_string: __("energy_manager.script.config_failed"),
+            reboot_string: __("energy_manager.script.reboot_content_changed")
+        });
+
+    $("#energy_manager_config_relay_config, \
+       #energy_manager_config_relay_config_if, \
+       #energy_manager_config_input3_config, \
+       #energy_manager_config_input4_config"
     ).on("change", update_energy_manager_html_visibility);
-
-    // Use bootstrap form validation
-    $('#energy_manager_config_form').on('submit', function (this: HTMLFormElement, event: Event) {
-        console.log("energy_manager on submit");
-        this.classList.add('was-validated');
-        event.preventDefault();
-        event.stopPropagation();
-
-        if (this.checkValidity() === false) {
-            console.log("energy_manager form not valid!");
-            return;
-        }
-        save_energy_manager_config();
-    });
 
     allow_debug(true);
 }
