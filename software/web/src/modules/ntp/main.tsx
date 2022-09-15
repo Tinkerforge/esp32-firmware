a96c4793d47f21dfe77ac44905673406d225ce80
Erik Fleckstein <erik@tinkerforge.com>
2022-09-15 16:01:51 +0200

    ntp: Port subpage to preact.

--- software/web/src/modules/ntp/main.tsx
+++ software/web/src/modules/ntp/main.tsx
@@ -19,36 +19,135 @@
 
 import $ from "../../ts/jq";
 
-import feather from "../../ts/feather";
-
 import * as util from "../../ts/util";
 import * as API from "../../ts/api";
 
-import { h, render } from "preact";
+import { h, render, Fragment } from "preact";
 import { __ } from "../../ts/translation";
-import { ConfigPageHeader } from "../../ts/components/config_page_header";
 
-render(<ConfigPageHeader prefix="ntp" title={__("ntp.content.ntp")} />, $('#ntp_header')[0]);
+import { Switch } from "../../ts/components/switch";
+import { ConfigComponent } from "src/ts/components/config_component";
+import { ConfigForm } from "src/ts/components/config_form";
+import { FormRow } from "src/ts/components/form_row";
 
 import timezones from "./timezones";
+import { InputSelect } from "src/ts/components/input_select";
+import { Button } from "react-bootstrap";
+import { InputText } from "src/ts/components/input_text";
 
-function update_timezone(timezone: string) {
-    let splt = timezone.split("/");
-    $('#ntp_config_timezone_area').val(splt[0]);
-    $('#ntp_config_timezone_area').trigger('change');
+type NTPConfig = API.getType['ntp/config'];
 
-    $('#ntp_config_timezone_location').val(splt[1]);
-    $('#ntp_config_timezone_location').trigger('change');
+export class NTP extends ConfigComponent<{}, NTPConfig> {
+    ignore_updates: boolean = false;
 
-    if (splt.length == 3) {
-        $('#ntp_config_timezone_sublocation').val(splt[2]);
+    constructor() {
+        super();
+        util.eventTarget.addEventListener('ntp/config', () => {
+            if (!this.ignore_updates)
+                this.setState(API.get('ntp/config'));
+        });
+    }
+
+    save() {
+        return API.save("ntp/config", this.state,
+            __("ntp.script.save_failed"),
+            __("ntp.script.reboot_content_changed"));
+    }
+
+    updateTimezone(s: string, i: number) {
+        let splt = this.state.timezone.split("/");
+        splt[i] = s;
+        if (i == 0)
+            splt[1] = Object.keys(timezones[splt[0]])[0];
+        else if (i == 1) {
+            if (timezones[splt[0]][splt[1]] != null)
+                splt[2] = Object.keys(timezones[splt[0]][splt[1]])[0];
+            else
+                splt = [splt[0], splt[1]];
+        }
+
+        this.setState({timezone: splt.join("/")});
+    }
+
+    render(props: {}, state: Readonly<NTPConfig>) {
+        if (!state || Object.keys(state).length == 0)
+            return (<></>);
+
+        console.log(state);
+        let splt = state.timezone.split("/");
+
+        return (
+            <>
+                <ConfigForm id="ntp_config_form"
+                            title={__("ntp.content.ntp")}
+                            onSave={() => this.save()}
+                            onDirtyChange={(d) => this.ignore_updates = d}>
+                    <FormRow label={__("ntp.content.enable")}>
+                        <Switch desc={__("ntp.content.enable_desc")}
+                                checked={state.enable}
+                                onClick={this.toggle('enable')}/>
+                    </FormRow>
+
+                    <FormRow label={__("ntp.content.use_dhcp")}>
+                        <Switch desc={__("ntp.content.use_dhcp_desc")}
+                                checked={state.use_dhcp}
+                                onClick={this.toggle('use_dhcp')}/>
+                    </FormRow>
+
+                    <FormRow label={__("ntp.content.timezone")}>
+                        <div class="input-group">
+                            <InputSelect
+                                required
+                                value={splt[0]}
+                                onValue={(v) => this.updateTimezone(v, 0)}
+                                items={
+                                    Object.keys(timezones).map(t => [t, t.replace(/_/g, " ")])
+                                }
+                            />
+                            <InputSelect
+                                required
+                                value={splt[1]}
+                                onValue={(v) => this.updateTimezone(v, 1)}
+                                items={
+                                    Object.keys(timezones[splt[0]]).map(t => [t, t.replace(/_/g, " ")])
+                                }
+                            />
+                            {
+                                timezones[splt[0]][splt[1]] == null ? "" :
+                                <InputSelect
+                                    required
+                                    value={splt[2]}
+                                    onValue={(v) => this.updateTimezone(v, 2)}
+                                    items={
+                                        Object.keys(timezones[splt[0]][splt[1]]).map(t => [t, t.replace(/_/g, " ")])
+                                    }
+                                />
+                            }
+                        </div>
+                        <br/>
+                        <Button variant="primary" className="form-control" onClick={() => this.setState({timezone: Intl.DateTimeFormat().resolvedOptions().timeZone})}>{__("ntp.content.use_browser_timezone")}</Button>
+                    </FormRow>
+
+                    <FormRow label={__("ntp.content.server")}>
+                        <InputText required
+                                   maxLength={64}
+                                   value={state.server}
+                                   onValue={this.set("server")}/>
+                    </FormRow>
+
+                    <FormRow label={__("ntp.content.server2")}>
+                        <InputText required
+                                   maxLength={64}
+                                   value={state.server2}
+                                   onValue={this.set("server2")}/>
+                    </FormRow>
+                </ConfigForm>
+            </>
+        );
     }
 }
 
-function update_config() {
-    let cfg = API.default_updater('ntp/config', ['timezone']);
-    update_timezone(cfg.timezone);
-}
+render(<NTP/>, $('#ntp')[0])
 
 function update_state() {
     let state = API.get('ntp/state');
@@ -57,54 +156,10 @@ function update_state() {
 }
 
 export function init() {
-    $('#ntp_browser_timezone').on('click', () => {
-        update_timezone(Intl.DateTimeFormat().resolvedOptions().timeZone);
-        $('#ntp_config_form').trigger('input');
-    });
 
-    $('#ntp_config_timezone_area').on('change', () => {
-        let area = $('#ntp_config_timezone_area').val() as string;
-        let options = Object.keys(timezones[area]).sort().map((x) => `<option value="${x}">${x}</option>`).join("");
-        $('#ntp_config_timezone_location').empty().append(options);
-        $('#ntp_config_timezone_location').trigger('change');
-    });
-
-    $('#ntp_config_timezone_location').on('change', () => {
-        let area = $('#ntp_config_timezone_area').val() as string;
-        let loc = $('#ntp_config_timezone_location').val() as string;
-        let sub_loc = timezones[area][loc];
-        if (sub_loc === null) {
-            $('#ntp_config_timezone_sublocation').prop("hidden", true);
-            $('#ntp_config_timezone_location').addClass("rounded-right");
-        } else {
-            let options = Object.keys(sub_loc).sort().map((x) => `<option value="${x}">${x}</option>`).join("");
-            $('#ntp_config_timezone_sublocation').empty().append(options);
-            $('#ntp_config_timezone_sublocation').prop("hidden", false);
-            $('#ntp_config_timezone_location').removeClass("rounded-right");
-        }
-    });
-
-    let options = Object.keys(timezones).sort().map((x) => `<option value="${x}">${x}</option>`).join("");
-    $('#ntp_config_timezone_area').empty().append(options);
-    $('#ntp_config_timezone_area').trigger('change');
-
-    API.register_config_form('ntp/config', {
-            overrides: () => {
-                    let timezone = $('#ntp_config_timezone_area').val() +
-                                    "/" + $('#ntp_config_timezone_location').val() +
-                                    ($('#ntp_config_timezone_sublocation').prop("hidden") ? "" : ("/" + $('#ntp_config_timezone_sublocation').val()));
-                    return {
-                        timezone: timezone
-                    };
-                },
-            error_string: __("ntp.script.save_failed"),
-            reboot_string: __("ntp.script.reboot_content_changed")
-        }
-    );
 }
 
 export function add_event_listeners(source: API.APIEventTarget) {
-    source.addEventListener('ntp/config', update_config);
     source.addEventListener('ntp/state', update_state);
 }
 
