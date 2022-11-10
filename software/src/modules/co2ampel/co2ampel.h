66927f99a9f08f1ca0840425f583acf4565427e1
Erik Fleckstein <erik@tinkerforge.com>
2022-11-10 15:31:44 +0100

    Add co2ampel implementation. For internal use only!

---
+++ software/src/modules/co2ampel/co2ampel.h
@@ -0,0 +1,50 @@
+/* warp-charger
+ * Copyright (C) 2020-2021 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#pragma once
+
+#include "bindings/bricklet_co2_v2.h"
+#include "bindings/bricklet_lcd_128x64.h"
+#include "bindings/bricklet_rgb_led_button.h"
+#include "bindings/bricklet_rgb_led_v2.h"
+
+#include "config.h"
+
+class Co2Ampel {
+public:
+    Co2Ampel(){}
+    void pre_setup();
+    void setup();
+    void register_urls();
+    void loop();
+
+    bool initialized = false;
+
+    void set_color(int c);
+
+    ConfigRoot state;
+    ConfigRoot config;
+    ConfigRoot stop_blink;
+
+private:
+    TF_CO2V2 co2;
+    TF_LCD128x64 lcd;
+    TF_RGBLEDButton rlb;
+    TF_RGBLEDV2 rgb;
+};
