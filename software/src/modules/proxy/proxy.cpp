fb505f3d09ab759b8d3cc5668182c1a7364fd153
Erik Fleckstein <erik@tinkerforge.com>
2022-11-21 11:54:23 +0100

    proxy: Make authentication secret, listen address and port configurable.

--- software/src/modules/proxy/proxy.cpp
+++ software/src/modules/proxy/proxy.cpp
@@ -92,12 +92,37 @@ void Proxy::pre_setup()
             })
         }
     });
+
+    config = ConfigRoot(Config::Object({
+         {"authentication_secret", Config::Str("", 0, 64)},
+         {"listen_address", Config::Str("0.0.0.0", 7, 15)},
+         {"listen_port", Config::Uint16(4223)}
+    }),  [](Config &cfg) -> String {
+        const char *listen = cfg.get("listen_address")->asCStr();
+        IPAddress listen_address;
+        if (!listen_address.fromString(listen))
+            return "Failed to parse \"listen_address\": Expected format is dotted decimal, i.e. 10.0.0.1";
+        return "";
+    });
 }
 
 void Proxy::setup()
 {
-    tf_net_create(&net, NULL, 0, NULL);
-    tf_hal_set_net(&hal, &net);
+    api.restorePersistentConfig("proxy/config", &config);
+
+    // We have to hold the secret as a (owning) string here:
+    // config.get("authentication_secret")->asCStr() seems to
+    // still get invalidated between setup and register_urls.
+    this->auth_secret = config.get("authentication_secret")->asString();
+    const char *secret = this->auth_secret.length() == 0 ? nullptr : this->auth_secret.c_str();
+
+    int ret = tf_net_create(&net, config.get("listen_address")->asCStr(), config.get("listen_port")->asUint(), secret);
+
+    if (ret != TF_E_OK) {
+        logger.printfln("Failed to initialize proxy: Listen address invalid?");
+    } else {
+        tf_hal_set_net(&hal, &net);
+    }
 
     uint16_t i = 0;
     char uid[7] = {0};
@@ -120,6 +145,7 @@ void Proxy::register_urls()
 {
     api.addState("proxy/error_counters", &error_counters, {}, 1000);
     api.addState("proxy/devices", &devices, {}, 1000);
+    api.addPersistentConfig("proxy/config", &config, {"authentication_secret"}, 1000);
 
     task_scheduler.scheduleWithFixedDelay([this](){
         for(char c = 'A'; c <= 'F'; ++c) {
