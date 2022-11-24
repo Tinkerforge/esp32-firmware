d54850d30fd4d4b3b3ea4804749e6fef2a4e6b75
Matthias Bolte <matthias@tinkerforge.com>
2022-11-24 15:22:10 +0100

    manage-packages: Fine tune output

--- software/manage_packages.py
+++ software/manage_packages.py
@@ -40,25 +40,31 @@ def main():
 
     exisiting_package_names = set()
 
-    for config in config_json:
+    for index, config in enumerate(config_json):
         exisiting_package_names.add(f"{config['base']}#{config['branch']}_{config['commit']}")
 
-    print('Existing packages:')
-    print(json.dumps(config_json, indent=4) + '\n')
+        print(f'Package {index}')
+        print(f" Base:   {config['base']}")
+        print(f" Branch: {config['branch']}")
+        print(f" Commit: {config['commit']}")
+        print(f" URL:    {config['url']}")
+        print()
 
     answer = choose('[A]dd or [R]emove package?', ['a', 'r'])
 
     if answer == 'a':
-        base = get('Base (default: arduino-esp32):', 'arduino-esp32')
-        branch = get('Branch:', None)
-        commit = get('Commit:', None)
+        print(f'Package {len(config_json)}')
+
+        base = get(' Base (default: arduino-esp32):', 'arduino-esp32')
+        branch = get(' Branch:', None)
+        commit = get(' Commit:', None)
         package_name = f'{base}#{branch}_{commit}'
 
         if package_name in exisiting_package_names:
             print('Error: Package already exists')
             return
 
-        url = get(f'Base (default: https://github.com/Tinkerforge/{base}):', f'https://github.com/Tinkerforge/{base}').rstrip('/')
+        url = get(f' Base (default: https://github.com/Tinkerforge/{base}):', f'https://github.com/Tinkerforge/{base}').rstrip('/')
         package_path = make_absolute_path(os.path.join('packages', package_name))
         package_json_url = f'{url}/raw/{commit}/package.json'
         package_json_path = os.path.join(package_path, 'package.json')
@@ -81,7 +87,7 @@ def main():
 
         print(f'Added package {package_name}')
     elif answer == 'r':
-        index = int(get(f'Index [0..{len(config_json) - 1}]:', None))
+        index = int(get(f'Package [0..{len(config_json) - 1}]:', None))
         config = config_json[index]
         package_name = f"{config['base']}#{config['branch']}_{config['commit']}"
 
@@ -93,4 +99,7 @@ def main():
         print(f'Removed package {package_name}')
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Aborted')
