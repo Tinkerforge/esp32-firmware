4546032e27496d4123ca65839522c56f728865a9
Erik Fleckstein <erik@tinkerforge.com>
2022-12-19 15:12:15 +0100

    users: Unblock user api with RAII object.
    This makes sure that in every early return path, the user API is
    unblocked again.

--- software/src/modules/users/users.cpp
+++ software/src/modules/users/users.cpp
@@ -206,7 +206,36 @@ bool read_user_slot_info(UserSlotInfo *result)
     return result->version == USER_SLOT_INFO_VERSION;
 }
 
+
 volatile bool user_api_blocked = false;
+class RAIIUserApiUnblocker {
+public:
+    RAIIUserApiUnblocker(bool assume_blocked) : released(!assume_blocked) {}
+
+    ~RAIIUserApiUnblocker() {
+        if (!released)
+            user_api_blocked = false;
+
+    }
+
+    bool try_block() {
+        for(int i = 0; i < 50; ++i) {
+            if (!user_api_blocked) {
+                user_api_blocked = true;
+                released = false;
+                return true;
+            }
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        return false;
+    }
+
+    void release() {
+        released = true;
+    }
+
+    bool released = false;
+};
 
 void Users::pre_setup()
 {
@@ -245,15 +274,10 @@ void Users::pre_setup()
         {"username", Config::Str("", 0, USERNAME_LENGTH)},
         {"digest_hash", Config::Str("", 0, 32)},
     }), [this](Config &add) -> String {
-        if (user_api_blocked) {
-            for(int i = 0; i < 50; ++i) {
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                if (!user_api_blocked)
-                    break;
-            }
-            if (user_api_blocked)
-                return "Still applying the last operation. Please retry.";
-        }
+        RAIIUserApiUnblocker unblocker{false};
+
+        if (!unblocker.try_block())
+            return "Still applying the last operation. Please retry.";
 
         if (user_config.get("next_user_id")->asUint() == 0)
             return "Can't add user. All user IDs in use.";
@@ -279,7 +303,8 @@ void Users::pre_setup()
             }
         }
 
-        user_api_blocked = true;
+        // Keep blocked for the users/add callback
+        unblocker.release();
         return "";
     });
     add.permit_null_updates = false;
@@ -287,22 +312,18 @@ void Users::pre_setup()
     remove = ConfigRoot(Config::Object({
         {"id", Config::Uint8(0)}
     }), [this](Config &remove) -> String {
-        if (user_api_blocked) {
-            for (int i = 0; i < 50; ++i) {
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                if (!user_api_blocked)
-                    break;
-            }
-            if (user_api_blocked)
-                return "Still applying the last operation. Please retry.";
-        }
+        RAIIUserApiUnblocker unblocker{false};
+
+        if (!unblocker.try_block())
+            return "Still applying the last operation. Please retry.";
 
         if (remove.get("id")->asUint() == 0)
             return "The anonymous user can't be removed.";
 
         for (int i = 0; i < user_config.get("users")->count(); ++i) {
             if (user_config.get("users")->get(i)->get("id")->asUint() == remove.get("id")->asUint()) {
-                user_api_blocked = true;
+                // Keep blocked for the users/add callback
+                unblocker.release();
                 return "";
             }
         }
@@ -483,16 +504,10 @@ void Users::register_urls()
     }
 
     api.addRawCommand("users/modify", [this](char *c, size_t s) -> String {
-        if (user_api_blocked) {
-            for(int i = 0; i < 50; ++i) {
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                if (!user_api_blocked)
-                    break;
-            }
-            if (user_api_blocked)
-                return "Still applying the last operation. Please retry.";
-        }
-        user_api_blocked = true;
+        RAIIUserApiUnblocker unblocker{false};
+
+        if (!unblocker.try_block())
+            return "Still applying the last operation. Please retry.";
 
         StaticJsonDocument<96> doc;
 
@@ -620,13 +635,16 @@ void Users::register_urls()
         if (err != "")
             return err;
 
+        // Keep blocked for the task below
+        unblocker.release();
+
         task_scheduler.scheduleOnce([this, display_name_changed, username_changed, user](){
+            // Blocked in users/modify raw command handler
+            RAIIUserApiUnblocker inner_unblocker{true};
             API::writeConfig("users/config", &user_config);
 
             if (display_name_changed || username_changed)
                 this->rename_user(user->get("id")->asUint(), user->get("username")->asString(), user->get("display_name")->asString());
-
-            user_api_blocked = false;
         }, 0);
 
         return "";
@@ -634,6 +652,9 @@ void Users::register_urls()
 
     api.addState("users/config", &user_config, {"digest_hash"}, 1000);
     api.addCommand("users/add", &add, {"digest_hash"}, [this](){
+        // Blocked in users/add validator
+        RAIIUserApiUnblocker inner_unblocker{true};
+
         user_config.get("users")->add();
         Config *user = (Config *)user_config.get("users")->get(user_config.get("users")->count() - 1);
 
@@ -648,10 +669,12 @@ void Users::register_urls()
 
         API::writeConfig("users/config", &user_config);
         this->rename_user(user->get("id")->asUint(), user->get("username")->asString(), user->get("display_name")->asString());
-        user_api_blocked = false;
     }, true);
 
     api.addCommand("users/remove", &remove, {}, [this](){
+        // Blocked in users/remove validator
+        RAIIUserApiUnblocker inner_unblocker{true};
+
         int idx = -1;
         for(int i = 0; i < user_config.get("users")->count(); ++i) {
             if (user_config.get("users")->get(i)->get("id")->asUint() == remove.get("id")->asUint()) {
@@ -695,8 +718,6 @@ void Users::register_urls()
                 API::writeConfig("users/config", &user_config);
             }
         }
-
-        user_api_blocked = false;
     }, true);
 
 
