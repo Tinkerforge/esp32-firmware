f58aa04f8726b629ca20d8e1bb8f79f643637c41
Erik Fleckstein <erik@tinkerforge.com>
2022-12-21 10:17:39 +0100

    wifi: Give initial wifi connection attempt 5 seconds to complete.

--- software/src/modules/wifi/wifi.cpp
+++ software/src/modules/wifi/wifi.cpp
@@ -517,7 +517,7 @@ void Wifi::setup()
                     backoff *= 2;
                 backoff_counter = backoff;
             }
-        }, 0, 5000);
+        }, 5000, 5000);
     }
 
     task_scheduler.scheduleWithFixedDelay([this](){
