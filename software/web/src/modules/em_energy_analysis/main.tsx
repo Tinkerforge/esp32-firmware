0d8d3de188685bdc5bf504a7231fe9f52a3707d7
Matthias Bolte <matthias@tinkerforge.com>
2023-01-23 17:48:25 +0100

    em: Add analysis chart skeleton

--- software/web/src/modules/em_energy_analysis/main.tsx
+++ software/web/src/modules/em_energy_analysis/main.tsx
@@ -21,11 +21,179 @@ import $ from "../../ts/jq";
 
 import * as API from "../../ts/api";
 
-import { h, render } from "preact";
+import { h, render, Fragment, Component, ComponentChild } from "preact";
 import { __ } from "../../ts/translation";
 import { PageHeader } from "../../ts/components/page_header";
 
-render(<PageHeader title={__("em_energy_analysis.content.em_energy_analysis")} />, $('#em_energy_analysis_header')[0]);
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface LineProps {
+  options: ChartOptions<"line">;
+  data: ChartData<"line">;
+}
+
+interface ChartExtra {
+    samples: number[];
+    tooltip_titles: string[];
+    grid_ticks: string[];
+    grid_colors: string[];
+}
+
+interface EMEnergyAnalysisState {
+    chart_extra: ChartExtra;
+}
+
+function build_chart_data(chart_extra: ChartExtra) {
+    let data: ChartData<"line"> = {
+        labels: chart_extra.grid_ticks,
+        datasets: [
+            {
+                data: chart_extra.samples,
+                backgroundColor: "#007bff",
+                borderColor: "#007bff",
+                normalized: true,
+            }
+        ]
+    };
+
+    return data;
+}
+
+function build_chart_options(chart_extra: ChartExtra, chart_container_id: string) {
+    let options: ChartOptions<"line"> = {
+        normalized: true,
+        animation: false,
+        onResize: function(chart, size) {
+            let element = document.getElementById(chart_container_id);
+            chart.options.aspectRatio = parseFloat(getComputedStyle(element).aspectRatio);
+        },
+        layout: {
+            autoPadding: false,
+            padding: {
+                right: 25,
+            }
+        },
+        elements: {
+            point: {
+                pointStyle: false,
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                intersect: false,
+                callbacks: {
+                    title: function(context) {
+                        return chart_extra.tooltip_titles[context[0].dataIndex];
+                    },
+                    label: function(context) {
+                        return " " + context.formattedValue + " Watt"; // FIXME
+                    }
+                }
+            }
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: "Hour", // FIXME
+                    font: {
+                        size: 14,
+                    }
+                },
+                ticks: {
+                    autoSkip: false,
+                    maxRotation: 0,
+                    includeBounds: false,
+                    sampleSize: 0,
+                },
+                grid: {
+                    color: chart_extra.grid_colors,
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: "Value", // FIXME
+                    font: {
+                        size: 14,
+                    }
+                },
+                border: {
+                    display: false,
+                },
+                ticks: {
+                    autoSkipPadding: 10,
+                }
+            }
+        }
+    };
+
+    return options;
+}
+
+export class EMEnergyAnalysis extends Component<{}, EMEnergyAnalysisState> {
+    constructor() {
+        super();
+
+        this.state = {
+            chart_extra: {
+                samples: [1, 2, 3],
+                tooltip_titles: ["a!", "b!", "c!"],
+                grid_ticks: ["a", "b", "c"],
+                grid_colors: ["rgba(0,0,0,0.1)", "rgba(0,0,0,0.1)", "rgba(0,0,0,0.1)"],
+            }
+        } as any;
+    }
+
+    render(props: {}, state: Readonly<EMEnergyAnalysisState>) {
+        if (!state || !state.chart_extra) {
+            return (<></>);
+        }
+
+        let data = build_chart_data(state.chart_extra);
+        let options = build_chart_options(state.chart_extra, "em_energy_analysis_chart");
+
+        return (
+            <>
+                <PageHeader title={__("em_energy_analysis.content.em_energy_analysis")} colClasses="col-xl-10"/>
+                <div class="row">
+                    <div class="col-xl-10">
+                        <div id="em_energy_analysis_chart" class="em-energy-analysis-chart">
+                            <Line data={data} options={options} />
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+render(<EMEnergyAnalysis />, $('#em-energy-analysis')[0]);
 
 export function init() {
 
