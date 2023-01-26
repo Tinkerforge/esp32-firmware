7f4e10f1f725a9c0c000fbb645a722c9496d7de2
Erik Fleckstein <erik@tinkerforge.com>
2023-01-26 11:07:35 +0100

    config_form: Simplify reset button code.

--- software/web/src/ts/components/config_form.tsx
+++ software/web/src/ts/components/config_form.tsx
@@ -67,16 +67,11 @@ export class ConfigForm extends Component<ConfigFormProps,ConfigFormState> {
         });
     }
 
-    showResetButton = () => {
-        if (typeof this.props.onReset == 'function')
-        {;
-            return  <button onClick={async () => {await this.props.onReset()}} class="btn btn-danger mb-2 ml-sm-2 col" disabled={!this.props.isModified}>
-                        {__("component.config_page_header.reset")}
-                        <span class="ml-2 spinner-border spinner-border-sm" role="status" style="vertical-align: middle;" hidden={!this.state.showSpinner} ></span>
-                    </button>
-        }
-        return <></>
-    }
+    resetButton = () => this.props.onReset ?
+            <button onClick={async () => {await this.props.onReset()}} class="btn btn-danger mb-2 ml-sm-2 col" disabled={!this.props.isModified}>
+                {__("component.config_page_header.reset")}
+                <span class="ml-2 spinner-border spinner-border-sm" role="status" style="vertical-align: middle;" hidden={!this.state.showSpinner} ></span>
+            </button> : <></>
 
     override render (props: ConfigFormProps, state: Readonly<ConfigFormState>) {
         return (
@@ -86,7 +81,7 @@ export class ConfigForm extends Component<ConfigFormProps,ConfigFormState> {
                         <div class="row no-gutters">
                             <h1 class="config-header col-12 col-sm text-center text-sm-left text-nowrap" dangerouslySetInnerHTML={{__html: props.title}}></h1>
                             <div class="col-12 col-sm row no-gutters">
-                                {this.showResetButton()}
+                                {this.resetButton()}
                                 <button type="submit" form={props.id} class="btn btn-primary col mb-2 ml-2 ml-md-3 mr-0" disabled={state.saveDisabled}>
                                     {__("component.config_page_header.save")}
                                     <span class="ml-2 spinner-border spinner-border-sm" role="status" style="vertical-align: middle;" hidden={!state.showSpinner}></span>
