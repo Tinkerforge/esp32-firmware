efcf68d03eb2af0af9dc8f5f4a24f57148b2fd8a
Erik Fleckstein <erik@tinkerforge.com>
2023-02-07 11:24:28 +0100

    tools: Add defer.
    Usage:
    void *a = malloc(1024);
    defer {free(a);};

--- software/src/tools.h
+++ software/src/tools.h
@@ -99,3 +99,13 @@ public:
 };
 
 void list_dir(fs::FS &fs, const char * dirname, uint8_t depth, uint8_t current_depth = 0);
+
+// https://stackoverflow.com/a/42060129
+#ifndef defer
+struct defer_dummy {};
+template <class F> struct deferrer { F f; ~deferrer() { f(); } };
+template <class F> deferrer<F> operator*(defer_dummy, F f) { return {f}; }
+#define DEFER_(LINE) zz_defer##LINE
+#define DEFER(LINE) DEFER_(LINE)
+#define defer auto DEFER(__LINE__) = defer_dummy{} *[&]()
+#endif
