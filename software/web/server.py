4dad9219a175403c147bbcacc9a7a185953376f9
Matthias Bolte <matthias@tinkerforge.com>
2023-02-17 18:11:10 +0100

    web: Add PUT support for server.py

--- software/web/server.py
+++ software/web/server.py
@@ -3,29 +3,33 @@
 import sys
 import os
 import argparse
-from flask import Flask, Response
-from flask_sock import Sock # pip install flask-sock
+from flask import Flask, Response, request
+from flask_sock import Sock  # pip install flask-sock
 from simple_websocket import ws as WS
-import websocket # pip install websocket-client
-from urllib.request import urlopen
+import websocket  # pip install websocket-client
+from urllib.request import urlopen, Request
 
 app = Flask(__name__)
 sock = Sock(app)
 host = None
 
+
 def make_absolute_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
 
 @app.route('/')
 def index():
     with open(make_absolute_path('build/index.standalone.html'), 'r') as f:
         return f.read()
 
-@app.route('/<path:path>')
+
+@app.route('/<path:path>', methods=['GET', 'PUT'])
 def forward_html(path):
-    with urlopen(f'http://{host}/{path}') as f:
+    with urlopen(Request(f'http://{host}/{path}', data=request.data, method=request.method)) as f:
         # Exclude Transfer-Encoding: chunked header.  The chunked response is already reassembled.
         return Response(f.read(), headers={x: f.headers[x] for x in f.headers if x != "Transfer-Encoding"})
+
 
 @sock.route('/ws')
 def forward_ws(sock):
@@ -55,6 +59,7 @@ def forward_ws(sock):
             pass
         else:
             sock.send(data)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host')
