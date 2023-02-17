4a776374280c97b32ec03a7e70fc3424585a6c4a
Christian Oelschlegel <18003795+oelison@users.noreply.github.com>
2023-02-17 21:50:40 +0100

    typedef removed print corrected break added

--- software/src/modules/ac011k/preinit.cpp
+++ software/src/modules/ac011k/preinit.cpp
@@ -15,7 +15,7 @@ static const int BUTTON_STAGE_DELETE_CONFIG = 15000;
 static const int BUTTON_STAGE_DELETE_ALL = 20000;
 static const int BUTTON_STAGE_OVERTIME = 25000;
 
-typedef enum EraseStages {
+enum EraseStages {
 
     doNothingNoPress = 0,
     deleteNetwork = 1,
@@ -79,6 +79,7 @@ void evse_v2_button_recovery_handler() {
             digitalWrite(RED_LED, HIGH);
             digitalWrite(GREEN_LED, HIGH);
             logger.printfln("Button SW3 is pressed for more than %.3f seconds. Assuming this must be an error. Continuing normal boot.", BUTTON_STAGE_OVERTIME / 1000.0);
+            break;
         case deleteNetwork:
             logger.printfln("Resetting network configuration and disabling web interface login");
 
@@ -105,7 +106,7 @@ void evse_v2_button_recovery_handler() {
             logger.printfln("factory reset done");
             break;
         default:
-            logger.printfln("Oh oh! unknown state in preinit.cpp: %s", stage);
+            logger.printfln("Oh oh! unknown state in preinit.cpp: %d", stage);
             break;
     }
 }
