0ba4a8c6b5ee7d584ad2b2da788dabcc66124a42
Olaf Lüke <olaf@tinkerforge.com>
2023-02-27 17:59:56 +0100

    Add missing warp prefix

--- provisioning/provision_stage_0_energy_manager.sh
+++ provisioning/provision_stage_0_energy_manager.sh
@@ -4,4 +4,4 @@ pushd ../../firmwares
 git pull
 popd
 
-parallel -j 9 -u python3 provision_stage_0_warp2.py ../../firmwares/bricks/energy_manager/brick_energy_manager_firmware_latest.bin '{}' energy_manager ::: /dev/ttyUSB*
+parallel -j 9 -u python3 provision_stage_0_warp2.py ../../firmwares/bricks/warp_energy_manager/brick_warp_energy_manager_firmware_latest.bin '{}' energy_manager ::: /dev/ttyUSB*
