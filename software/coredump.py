b859f3d309cce2430b78eb8683e1da0576fb41c9
Erik Fleckstein <erik@tinkerforge.com>
2023-03-15 13:20:00 +0100

    coredump.py: Show helpful message if esp_coredump is not available.

--- software/coredump.py
+++ software/coredump.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from esp_coredump import CoreDump
 import base64
 import string
 import json
 import argparse
 import os, sys
 
-def get_tf_coredump_json(coredump_path: str):
-    tf_coredump_json = get_tf_coredump_data(coredump_path)[28:]
-    tf_coredump_json = tf_coredump_json[:tf_coredump_json.rfind("___tf_coredump_data_end___")]
-    return tf_coredump_json
+try:
+    from esp_coredump import CoreDump
+except:
+    print("Failed to import esp_coredump. Install ESP-IDF and (on POSIX) run source /path/to/esp-idf/export.sh")
+    sys.exit(-1)
 
 def get_tf_coredump_data(coredump_path: str):
     printable = set(string.printable)
