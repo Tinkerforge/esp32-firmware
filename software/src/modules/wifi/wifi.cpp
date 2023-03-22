bbfc5a60a79255d88ff4e4ce3a7c6b287b62ecfb
Mattias Schäffersmann <mattias@tinkerforge.com>
2023-03-22 15:28:51 +0100

    wifi: Tune WiFi settings.
    - Disable HT40 mode. We don't need the additional speed and it only causes
      a higher error rate.
    - Disable 11b rates. 11b is obsolete for 20 years and compatibility for it
      slows down all devices not 20 years old.

--- software/src/modules/wifi/wifi.cpp
+++ software/src/modules/wifi/wifi.cpp
@@ -490,6 +490,18 @@ void Wifi::setup()
 
     esp_wifi_set_ps(WIFI_PS_NONE);
 
+    // We don't need the additional speed of HT40 and it only causes more errors.
+    if (enable_sta) {
+        esp_err_t err = esp_wifi_set_bandwidth(WIFI_IF_STA, WIFI_BW_HT20);
+        if (err != ESP_OK)
+            logger.printfln("WiFi: Setting HT20 for station failed: %i", err);
+    }
+    if (enable_ap) {
+        esp_err_t err = esp_wifi_set_bandwidth(WIFI_IF_AP, WIFI_BW_HT20);
+        if (err != ESP_OK)
+            logger.printfln("WiFi: Setting HT20 for AP failed: %i", err);
+    }
+
     WiFi.setTxPower(WIFI_POWER_19_5dBm);
 
     wifi_state.get("ap_bssid")->updateString(WiFi.softAPmacAddress());
