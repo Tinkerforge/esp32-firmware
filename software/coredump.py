b5cdae8555842b452becab82f6f9b835fe0ff293
Erik Fleckstein <erik@tinkerforge.com>
2023-04-14 12:00:24 +0200

    coredump.py: Support downloading core dumps from connected ESPs.
    Via ./coredump.py -p[PORT]
    
    -p supports the same short-hand as the other scripts: -p1 expands to
    port /dev/ttyUSB1.

--- software/coredump.py
+++ software/coredump.py
@@ -42,18 +42,7 @@ def get_tf_coredump_data(coredump_path: str):
 
 core_dump_path = os.path.join(tempfile.gettempdir(), "tf_coredump.elf")
 
-if __name__ == '__main__':
-    gdb = find_gdb()
-    if gdb is None:
-        print("Can't find xtensa-esp32-elf-gdb or pio.")
-        print("Make sure that one of them is available in your $PATH.")
-        sys.exit(-1)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path")
-    args = parser.parse_args(sys.argv[1:])
-    path = args.path
-
+def get_core_dump_from_debug_report(path):
     with open(path) as file:
         file_str = file.read()
         core_dump_start_pos = file_str.rfind("___CORE_DUMP_START___\n\n")
@@ -67,25 +56,53 @@ if __name__ == '__main__':
         with open(core_dump_path, "wb") as f:
             f.write(core_dump)
 
-        try:
-            tf_coredump_json = get_tf_coredump_data(core_dump_path)
-            if not tf_coredump_json:
-                print("Core dump in debug log has no TF coredump info.")
-                sys.exit (-1)
+def download_core_dump(port):
+    os.system("pio pkg exec esptool.py -- --port {} --chip esp32 --baud 921600 read_flash 0xff0014 0xFFEC {}".format(port, core_dump_path))
 
-            tf_coredump_data = json.loads(tf_coredump_json)
+if __name__ == '__main__':
+    gdb = find_gdb()
+    if gdb is None:
+        print("Can't find xtensa-esp32-elf-gdb or pio.")
+        print("Make sure that one of them is available in your $PATH.")
+        sys.exit(-1)
 
-            elf_name = tf_coredump_data['firmware_file_name'] + ".elf"
-            script_path = os.path.dirname(os.path.realpath(__file__))
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("path", nargs='?', default=None)
+    group.add_argument("-p", "--port")
+    args = parser.parse_args(sys.argv[1:])
 
-            firmware_path = os.path.join(script_path, "build", elf_name)
-            if not os.path.exists(firmware_path):
-                firmware_path = os.path.join(script_path, "..", "..", "warp-charger", "firmwares", elf_name)
+    if args.port:
+        if len(args.port) < 6:
+            port = "/dev/ttyUSB" + args.port
+        else:
+            port = args.port
 
-            if os.path.exists(firmware_path):
-                os.system("{} -q --batch -ex 'set style enabled on' -ex 'bt full' {} {}".format(gdb, firmware_path, core_dump_path))
-            else:
-                print("Firmware {} not found".format(elf_name))
-        finally:
-            if os.path.exists(core_dump_path):
-                os.remove(core_dump_path)
+    if args.path:
+        get_core_dump_from_debug_report(args.path)
+    if args.port:
+        download_core_dump(port)
+
+
+    try:
+        tf_coredump_json = get_tf_coredump_data(core_dump_path)
+        if not tf_coredump_json:
+            print("Core dump in debug log has no TF coredump info.")
+            sys.exit (-1)
+
+        tf_coredump_data = json.loads(tf_coredump_json)
+        print(tf_coredump_data)
+        elf_name = tf_coredump_data['firmware_file_name'] + ".elf"
+        script_path = os.path.dirname(os.path.realpath(__file__))
+
+        firmware_path = os.path.join(script_path, "build", elf_name)
+        if not os.path.exists(firmware_path):
+            firmware_path = os.path.join(script_path, "..", "..", "warp-charger", "firmwares", elf_name)
+        if os.path.exists(firmware_path):
+            os.system("{} -q --batch -ex 'set style enabled on' -ex 'bt full' {} {}".format(gdb, firmware_path, core_dump_path))
+        else:
+            print("Firmware {} not found".format(elf_name))
+    finally:
+        pass
+        if os.path.exists(core_dump_path):
+            os.remove(core_dump_path)
