cd4ad413892370a53570cc0d87d34031a59db7be
Erik Fleckstein <erik@tinkerforge.com>
2023-04-25 08:51:04 +0200

    charge_limits: Only render if EVSE is available. Simplify with signals.

--- software/web/src/modules/charge_limits/main.tsx
+++ software/web/src/modules/charge_limits/main.tsx
@@ -22,208 +22,150 @@ import $ from "../../ts/jq";
 import * as util from "../../ts/util";
 import * as API from "../../ts/api";
 
-import { h, render, Fragment, Component } from "preact";
-import { translate_unchecked, __ } from "../../ts/translation";
+import { h, render, Fragment } from "preact";
+import { __ } from "../../ts/translation";
 
 import { FormRow } from "../../ts/components/form_row";
-import { InputText } from "../../ts/components/input_text";
-import { Button, InputGroup } from "react-bootstrap";
 import { InputSelect } from "src/ts/components/input_select";
-import { InputFloat } from "src/ts/components/input_float";
 
+function ChargeLimitsOverride() {
+    if (!util.render_allowed() || !API.hasFeature("evse"))
+        return <></>;
 
-interface ChargeLimitsState {
-    state: API.getType["charge_limits/state"]
-    meter_abs: number
-    evse_uptime: number
-}
+    let config_in_use = API.get("charge_limits/active_limits");
+    let config = API.get("charge_limits/default_limits");
+    let state = API.get("charge_limits/state");
+    let meter_abs = API.get("meter/values").energy_abs;
+    let evse_uptime = API.get("evse/low_level_state").uptime;
+    let electricity_price = API.get("charge_tracker/config").electricity_price;
 
-interface ChargeLimitsOverrideState extends ChargeLimitsState
-{
-    config_in_use: API.getType['charge_limits/active_limits']
-    config: API.getType['charge_limits/default_limits']
-    electricity_price: number
-}
+    const has_meter = API.hasFeature("meter");
 
-class ChargeLimitsOverride extends Component<{}, ChargeLimitsOverrideState>
-{
-    timeout_id: number;
-    override_duration: number
+    const get_energy_left = () => {
+        let ret = "";
+        if (config_in_use.energy_wh == 0)
+            ret = __("charge_limits.content.unlimited")
+        else {
+            let energy = state.target_energy_kwh - meter_abs;
+            if (state.start_energy_kwh == null)
+                energy = config_in_use.energy_wh / 1000.0;
+            else if (energy < 0)
+                energy = 0;
 
-    constructor()
-    {
-        super();
+            ret = util.toLocaleFixed(energy, 3) + " kWh";
+        }
 
-        this.timeout_id = null;
-        this.override_duration = 0;
-
-
-
-        util.addApiEventListener("charge_limits/active_limits", () => {
-            this.setState({config_in_use: API.get("charge_limits/active_limits")});
-        })
-
-        util.addApiEventListener("charge_limits/default_limits", () => {
-            this.setState({config: API.get("charge_limits/default_limits")})
-        })
-
-        util.addApiEventListener("charge_limits/state", () => {
-            this.setState({state: API.get("charge_limits/state")});
-        })
-
-        util.addApiEventListener("meter/values", () => {
-            this.setState({meter_abs: API.get("meter/values").energy_abs});
-        })
-
-        util.addApiEventListener("evse/low_level_state", () => {
-            this.setState({evse_uptime: API.get("evse/low_level_state").uptime});
-        })
-
-        util.addApiEventListener("charge_tracker/config", () => {
-            this.setState({electricity_price: API.get("charge_tracker/config").electricity_price});
-        })
+        return ret;
     }
 
-    render(props: {}, s: ChargeLimitsOverrideState)
-    {
-        let {state,
-            config_in_use,
-            config,
-            evse_uptime,
-            meter_abs,
-            electricity_price} = s;
+    const get_duration_left = () => {
+        let ret = "";
+        if (config_in_use.duration == 0)
+            ret = __("charge_limits.content.unlimited")
+        else {
+            let duration: number;
+            duration = state.target_timestamp_ms;
+            if (state.start_timestamp_ms != 0)
+                duration = Math.floor((state.target_timestamp_ms - evse_uptime));
+            if (state.target_timestamp_ms - evse_uptime < 0 && state.start_timestamp_ms != 0)
+                duration = 0;
 
-        if (!state || !config_in_use || !config)
-            return <></>;
-
-        const has_meter = API.hasFeature("meter");
-
-        const get_energy_left = () => {
-            let ret = "";
-            if (config_in_use.energy_wh == 0)
-                ret = __("charge_limits.content.unlimited")
-            else {
-                let energy = state.target_energy_kwh - meter_abs;
-                if (state.start_energy_kwh == null)
-                    energy = config_in_use.energy_wh / 1000.0;
-                else if (energy < 0)
-                    energy = 0;
-
-                ret = util.toLocaleFixed(energy, 3) + " kWh";
-            }
-
-            return ret;
+            ret = util.format_timespan(Math.floor(duration / 1000));
         }
 
-        const get_duration_left = () => {
-            let ret = "";
-            if (config_in_use.duration == 0)
-                ret = __("charge_limits.content.unlimited")
-            else {
-                let duration: number;
-                duration = state.target_timestamp_ms;
-                if (state.start_timestamp_ms != 0)
-                    duration = Math.floor((state.target_timestamp_ms - evse_uptime));
-                if (state.target_timestamp_ms - evse_uptime < 0 && state.start_timestamp_ms != 0)
-                    duration = 0;
+        return ret;
+    }
 
-                ret = util.format_timespan(Math.floor(duration / 1000));
-            }
+    let duration_items: [string, string][] = [
+        ["disabled", get_duration_left()],
+        ["0", __("charge_limits.content.unlimited")],
+        ["1", __("charge_limits.content.min15")],
+        ["2", __("charge_limits.content.min30")],
+        ["3", __("charge_limits.content.min45")],
+        ["4", __("charge_limits.content.h1")],
+        ["5", __("charge_limits.content.h2")],
+        ["6", __("charge_limits.content.h3")],
+        ["7", __("charge_limits.content.h4")],
+        ["8", __("charge_limits.content.h6")],
+        ["9", __("charge_limits.content.h8")],
+        ["10", __("charge_limits.content.h12")]
+    ];
 
-            return ret;
+    if (config_in_use.duration != 0)
+        duration_items[0][1] = duration_items[config_in_use.duration + 1][1] + " | " + get_duration_left() + __("charge_limits.content.left");
+
+    let energy_items: [string, string][] = [
+        ["disabled", get_energy_left()],
+        ["0", __("charge_limits.content.unlimited")],
+        ["5000", util.toLocaleFixed(5, 0) + " kWh"],
+        ["10000", util.toLocaleFixed(10, 0) + " kWh"],
+        ["15000", util.toLocaleFixed(15, 0) + " kWh"],
+        ["20000", util.toLocaleFixed(20, 0) + " kWh"],
+        ["25000", util.toLocaleFixed(25, 0) + " kWh"],
+        ["30000", util.toLocaleFixed(30, 0) + " kWh"],
+        ["40000", util.toLocaleFixed(40, 0) + " kWh"],
+        ["50000", util.toLocaleFixed(50, 0) + " kWh"],
+        ["60000", util.toLocaleFixed(60, 0) + " kWh"],
+        ["70000", util.toLocaleFixed(70, 0) + " kWh"],
+        ["80000", util.toLocaleFixed(80, 0) + " kWh"],
+        ["90000", util.toLocaleFixed(90, 0) + " kWh"],
+        ["100000", util.toLocaleFixed(100, 0) + " kWh"]
+    ];
+
+    if (electricity_price > 0) {
+        for(let i = 2; i < energy_items.length; ++i) {
+            energy_items[i][1] += ` (~ ${util.toLocaleFixed(electricity_price / 10000 * parseFloat(energy_items[i][0]) / 1000, 2)} €)`
         }
+    }
 
-        let duration_items: [string, string][] = [
-            ["disabled", get_duration_left()],
-            ["0", __("charge_limits.content.unlimited")],
-            ["1", __("charge_limits.content.min15")],
-            ["2", __("charge_limits.content.min30")],
-            ["3", __("charge_limits.content.min45")],
-            ["4", __("charge_limits.content.h1")],
-            ["5", __("charge_limits.content.h2")],
-            ["6", __("charge_limits.content.h3")],
-            ["7", __("charge_limits.content.h4")],
-            ["8", __("charge_limits.content.h6")],
-            ["9", __("charge_limits.content.h8")],
-            ["10", __("charge_limits.content.h12")]
-        ];
+    if (config_in_use.energy_wh != 0)
+        energy_items[0][1] = util.toLocaleFixed(config_in_use.energy_wh / 1000, 0) + " kWh" + " | " + get_energy_left() + __("charge_limits.content.left");
 
-        if (config_in_use.duration != 0)
-            duration_items[0][1] = duration_items[config_in_use.duration + 1][1] + " | " + get_duration_left() + __("charge_limits.content.left");
+    let conf_idx = energy_items.findIndex(x => x[0] == config.energy_wh.toString())
+    if (conf_idx == -1)
+        energy_items = [energy_items[0],
+                        [config.energy_wh.toString(),
+                            util.toLocaleFixed(config.energy_wh / 1000.0, 3)
+                            + " kWh "
+                            + (electricity_price > 0 ? ` (~ ${util.toLocaleFixed(electricity_price / 10000 * config.energy_wh / 1000, 2)} €)` : "")
+                            + __("charge_limits.content.configured")],
+                        ...energy_items.slice(1)];
+    else
+        energy_items[conf_idx][1] += " " + __("charge_limits.content.configured");
 
-        let energy_items: [string, string][] = [
-            ["disabled", get_energy_left()],
-            ["0", __("charge_limits.content.unlimited")],
-            ["5000", util.toLocaleFixed(5, 0) + " kWh"],
-            ["10000", util.toLocaleFixed(10, 0) + " kWh"],
-            ["15000", util.toLocaleFixed(15, 0) + " kWh"],
-            ["20000", util.toLocaleFixed(20, 0) + " kWh"],
-            ["25000", util.toLocaleFixed(25, 0) + " kWh"],
-            ["30000", util.toLocaleFixed(30, 0) + " kWh"],
-            ["40000", util.toLocaleFixed(40, 0) + " kWh"],
-            ["50000", util.toLocaleFixed(50, 0) + " kWh"],
-            ["60000", util.toLocaleFixed(60, 0) + " kWh"],
-            ["70000", util.toLocaleFixed(70, 0) + " kWh"],
-            ["80000", util.toLocaleFixed(80, 0) + " kWh"],
-            ["90000", util.toLocaleFixed(90, 0) + " kWh"],
-            ["100000", util.toLocaleFixed(100, 0) + " kWh"]
-        ];
-
-        if (electricity_price > 0) {
-            for(let i = 2; i < energy_items.length; ++i) {
-                energy_items[i][1] += ` (~ ${util.toLocaleFixed(electricity_price / 10000 * parseFloat(energy_items[i][0]) / 1000, 2)} €)`
-            }
-        }
-
-        if (config_in_use.energy_wh != 0)
-            energy_items[0][1] = util.toLocaleFixed(config_in_use.energy_wh / 1000, 0) + " kWh" + " | " + get_energy_left() + __("charge_limits.content.left");
-
-        let conf_idx = energy_items.findIndex(x => x[0] == config.energy_wh.toString())
+    if (config.energy_wh != config_in_use.energy_wh) {
+        let conf_idx = energy_items.findIndex(x => x[0] == config_in_use.energy_wh.toString())
         if (conf_idx == -1)
-            energy_items = [energy_items[0],
-                            [config.energy_wh.toString(),
-                             util.toLocaleFixed(config.energy_wh / 1000.0, 3)
-                             + " kWh "
-                             + (electricity_price > 0 ? ` (~ ${util.toLocaleFixed(electricity_price / 10000 * config.energy_wh / 1000, 2)} €)` : "")
-                             + __("charge_limits.content.configured")],
-                            ...energy_items.slice(1)];
+            console.log("ups");
         else
-            energy_items[conf_idx][1] += " " + __("charge_limits.content.configured");
-
-        if (config.energy_wh != config_in_use.energy_wh) {
-            let conf_idx = energy_items.findIndex(x => x[0] == config_in_use.energy_wh.toString())
-            if (conf_idx == -1)
-                console.log("ups");
-            else
-                energy_items[conf_idx][1] += " " + __("charge_limits.content.overridden");
-        }
+            energy_items[conf_idx][1] += " " + __("charge_limits.content.overridden");
+    }
 
 
-        duration_items[config.duration + 1][1] += " " + __("charge_limits.content.configured");
-        if (config.duration != config_in_use.duration)
-            duration_items[config_in_use.duration + 1][1] += " " + __("charge_limits.content.overridden");
+    duration_items[config.duration + 1][1] += " " + __("charge_limits.content.configured");
+    if (config.duration != config_in_use.duration)
+        duration_items[config_in_use.duration + 1][1] += " " + __("charge_limits.content.overridden");
 
-        return <>
-                <FormRow label={__("charge_limits.content.override_duration")} labelColClasses="col-sm-4" contentColClasses="col-lg-8 col-xl-4">
-                    <InputSelect items={duration_items}
+    return <>
+            <FormRow label={__("charge_limits.content.override_duration")} labelColClasses="col-sm-4" contentColClasses="col-lg-8 col-xl-4">
+                <InputSelect items={duration_items}
+                    value={"disabled"}
+                    onValue={(v) => {
+                        this.setState({config_in_use: {...config_in_use, duration: Number(v)}})
+                        API.call("charge_limits/override_duration", {duration: Number(v)}, __("charge_limits.script.override_failed"));
+                }}/>
+            </FormRow>
+            {has_meter ? <>{
+                <FormRow label={__("charge_limits.content.override_energy")} labelColClasses="col-sm-4" contentColClasses="col-lg-8 col-xl-4">
+                    <InputSelect items={energy_items}
                         value={"disabled"}
                         onValue={(v) => {
-                            this.setState({config_in_use: {...config_in_use, duration: Number(v)}})
-                            API.call("charge_limits/override_duration", {duration: Number(v)}, __("charge_limits.script.override_failed"));
+                            this.setState({config_in_use: {...config_in_use, energy_wh: Number(v)}})
+                            API.call("charge_limits/override_energy", {energy_wh: Number(v)}, __("charge_limits.script.override_failed"));
                     }}/>
                 </FormRow>
-                {has_meter ? <>{
-                    <FormRow label={__("charge_limits.content.override_energy")} labelColClasses="col-sm-4" contentColClasses="col-lg-8 col-xl-4">
-                        <InputSelect items={energy_items}
-                            value={"disabled"}
-                            onValue={(v) => {
-                                this.setState({config_in_use: {...config_in_use, energy_wh: Number(v)}})
-                                API.call("charge_limits/override_energy", {energy_wh: Number(v)}, __("charge_limits.script.override_failed"));
-                        }}/>
-                    </FormRow>
-                }</> : <></>}
-            </>
-    }
+            }</> : <></>}
+        </>
 }
 
 render(<ChargeLimitsOverride/>, $('#charge_limits_override')[0]);
