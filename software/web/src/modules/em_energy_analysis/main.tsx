3183375fd7038f0ea84ad31528960d683727f083
Matthias Bolte <matthias@tinkerforge.com>
2023-05-23 18:43:58 +0200

    em-analysis,meter: Draw null value grid line full width

--- software/web/src/modules/em_energy_analysis/main.tsx
+++ software/web/src/modules/em_energy_analysis/main.tsx
@@ -334,7 +334,7 @@ class UplotWrapper extends Component<UplotWrapperProps, {}> {
                                 let s  = self.series[0];
                                 let xd = self.data[0];
                                 let [i0, i1] = s.idxs;
-                                let x0 = self.valToPos(xd[i0], 'x', true);
+                                let x0 = self.valToPos(xd[i0], 'x', true) - self.axes[0].ticks.size;
                                 let y0 = self.valToPos(0, 'y', true);
                                 let x1 = self.valToPos(xd[i1], 'x', true);
                                 let y1 = self.valToPos(0, 'y', true);
