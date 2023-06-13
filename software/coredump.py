549a21f9aa35c338c19c0ba6d5bf1b3441290ca8
Erik Fleckstein <erik@tinkerforge.com>
2023-06-13 11:13:41 +0200

    coredump.py: Parse crash reason and extra infos out of ELF file.

--- software/coredump.py
+++ software/coredump.py
@@ -22,6 +22,101 @@ def find_gdb():
 
 PREFIX = b"___tf_coredump_info_start___"
 SUFFIX = b"___tf_coredump_info_end___"
+EXTRA_INFO_HEADER = b'\xA5\x02\x00\x00EXTRA_INFO'
+
+def extra_info_reg_name(reg):
+    return {
+        232: 'EXCCAUSE',
+        238: 'EXCVADDR',
+        177: 'EPC1',
+        178: 'EPC2',
+        179: 'EPC3',
+        180: 'EPC4',
+        181: 'EPC5',
+        182: 'EPC6',
+        183: 'EPC7',
+        194: 'EPS2',
+        195: 'EPS3',
+        196: 'EPS4',
+        197: 'EPS5',
+        198: 'EPS6',
+        199: 'EPS7'
+    }.get(reg, 'unknown register')
+
+# https://github.com/espressif/esp-coredump/blob/08f92cf36e7a649650a8b722cc96217026fd0174/esp_coredump/corefile/xtensa.py#L13-L72
+INVALID_CAUSE_VALUE = 0xFFFF
+XCHAL_EXCCAUSE_NUM = 64
+
+# Exception cause dictionary to get translation of exccause register
+# From 4.4.1.5 table 4-64 Exception Causes of Xtensa
+# Instruction Set Architecture (ISA) Reference Manual
+
+XTENSA_EXCEPTION_CAUSE_DICT = {
+    0: ('IllegalInstructionCause', 'Illegal instruction'),
+    1: ('SyscallCause', 'SYSCALL instruction'),
+    2: ('InstructionFetchErrorCause',
+        'Processor internal physical address or data error during instruction fetch. (See EXCVADDR for more information)'),
+    3: ('LoadStoreErrorCause',
+        'Processor internal physical address or data error during load or store. (See EXCVADDR for more information)'),
+    4: ('Level1InterruptCause', 'Level-1 interrupt as indicated by set level-1 bits in the INTERRUPT register'),
+    5: ('AllocaCause', 'MOVSP instruction, if caller`s registers are not in the register file'),
+    6: ('IntegerDivideByZeroCause', 'QUOS: QUOU, REMS: or REMU divisor operand is zero'),
+    8: ('PrivilegedCause', 'Attempt to execute a privileged operation when CRING ? 0'),
+    9: ('LoadStoreAlignmentCause', 'Load or store to an unaligned address. (See EXCVADDR for more information)'),
+    12: ('InstrPIFDataErrorCause', 'PIF data error during instruction fetch. (See EXCVADDR for more information)'),
+    13: ('LoadStorePIFDataErrorCause',
+         'Synchronous PIF data error during LoadStore access. (See EXCVADDR for more information)'),
+    14: ('InstrPIFAddrErrorCause', 'PIF address error during instruction fetch. (See EXCVADDR for more information)'),
+    15: ('LoadStorePIFAddrErrorCause',
+         'Synchronous PIF address error during LoadStore access. (See EXCVADDR for more information)'),
+    16: ('InstTLBMissCause', 'Error during Instruction TLB refill. (See EXCVADDR for more information)'),
+    17: ('InstTLBMultiHitCause', 'Multiple instruction TLB entries matched. (See EXCVADDR for more information)'),
+    18: ('InstFetchPrivilegeCause',
+         'An instruction fetch referenced a virtual address at a ring level less than CRING. (See EXCVADDR for more information)'),
+    20: ('InstFetchProhibitedCause',
+         'An instruction fetch referenced a page mapped with an attribute that does not permit instruction fetch (EXCVADDR).'),
+    24: ('LoadStoreTLBMissCause', 'Error during TLB refill for a load or store. (See EXCVADDR for more information)'),
+    25: ('LoadStoreTLBMultiHitCause',
+         'Multiple TLB entries matched for a load or store. (See EXCVADDR for more information)'),
+    26: ('LoadStorePrivilegeCause',
+         'A load or store referenced a virtual address at a ring level less than CRING. (See EXCVADDR for more information)'),
+    28: ('LoadProhibitedCause',
+         'A load referenced a page mapped with an attribute that does not permit loads. (See EXCVADDR for more information)'),
+    29: ('StoreProhibitedCause',
+         'A store referenced a page mapped with an attribute that does not permit stores [Region Protection Option or MMU Option].'),
+    32: ('Coprocessor0Disabled', 'Coprocessor 0 instruction when cp0 disabled'),
+    33: ('Coprocessor1Disabled', 'Coprocessor 1 instruction when cp1 disabled'),
+    34: ('Coprocessor2Disabled', 'Coprocessor 2 instruction when cp2 disabled'),
+    35: ('Coprocessor3Disabled', 'Coprocessor 3 instruction when cp3 disabled'),
+    36: ('Coprocessor4Disabled', 'Coprocessor 4 instruction when cp4 disabled'),
+    37: ('Coprocessor5Disabled', 'Coprocessor 5 instruction when cp5 disabled'),
+    38: ('Coprocessor6Disabled', 'Coprocessor 6 instruction when cp6 disabled'),
+    39: ('Coprocessor7Disabled', 'Coprocessor 7 instruction when cp7 disabled'),
+    INVALID_CAUSE_VALUE: (
+        'InvalidCauseRegister', 'Invalid EXCCAUSE register value or current task is broken and was skipped'),
+    # ESP panic pseudo reasons
+    XCHAL_EXCCAUSE_NUM + 0: ('UnknownException', 'Unknown exception'),
+    XCHAL_EXCCAUSE_NUM + 1: ('DebugException', 'Unhandled debug exception'),
+    XCHAL_EXCCAUSE_NUM + 2: ('DoubleException', 'Double exception'),
+    XCHAL_EXCCAUSE_NUM + 3: ('KernelException', 'Unhandled kernel exception'),
+    XCHAL_EXCCAUSE_NUM + 4: ('CoprocessorException', 'Coprocessor exception'),
+    XCHAL_EXCCAUSE_NUM + 5: ('InterruptWDTTimoutCPU0', 'Interrupt wdt timeout on CPU0'),
+    XCHAL_EXCCAUSE_NUM + 6: ('InterruptWDTTimoutCPU1', 'Interrupt wdt timeout on CPU1'),
+    XCHAL_EXCCAUSE_NUM + 7: ('CacheError', 'Cache disabled but cached memory region accessed'),
+}
+
+def format_extra_data(extra_data):
+    result = ""
+    if 'crashed_task_handle' in extra_data:
+        result += f"Crashed task handle {hex(extra_data['crashed_task_handle'])}\n"
+        del extra_data['crashed_task_handle']
+
+    for k, v in extra_data.items():
+        if k == 'EXCCAUSE':
+            result += f"{k.ljust(16)}{hex(v).ljust(16)}{': '.join(XTENSA_EXCEPTION_CAUSE_DICT[v])}\n"
+        else:
+            result += f"{k.ljust(16)}{hex(v).ljust(16)}{v}\n"
+    return result
 
 def get_tf_coredump_data(coredump_path: str):
     printable = set(string.printable)
@@ -31,15 +126,44 @@ def get_tf_coredump_data(coredump_path: str):
         start = b.find(PREFIX)
 
         if start < 0:
-            return None
+            return (None, None)
 
-        end = b.find(SUFFIX)
+        end = b.find(SUFFIX, start)
 
-        if end < 0 or end < start:
-            return None
+        if end < 0:
+            return (None, None)
 
         start += len(PREFIX)
-        return b[start:end]
+        tf_core_dump_data = b[start:end]
+
+        # based on https://github.com/espressif/esp-coredump/blob/08f92cf36e7a649650a8b722cc96217026fd0174/esp_coredump/corefile/xtensa.py#L120
+        # and staring at an ELF file
+
+        # search xtensa extra info behind out core dump data
+        extra_info_idx = b.find(EXTRA_INFO_HEADER, end)
+        if extra_info_idx < 0 or (len(b) - extra_info_idx < (len(EXTRA_INFO_HEADER) + 2 + 164)):
+            return (tf_core_dump_data, None)
+
+        extra_data = {}
+
+        # skip header
+        extra_info_idx += len(EXTRA_INFO_HEADER)
+        # skip two bytes (probably part of the ELF section header)
+        extra_info_idx += 2
+
+        extra_info = b[extra_info_idx:extra_info_idx+164]
+        extra_data['crashed_task_handle'] = int.from_bytes(extra_info[:4], byteorder='little')
+
+        for i in range(4, len(extra_info), 8):
+            reg = int.from_bytes(extra_info[i:i+4], byteorder='little')
+            if reg == 0:
+                continue
+
+            value = int.from_bytes(extra_info[i+4:i+8], byteorder='little')
+
+            extra_data[extra_info_reg_name(reg)] = value
+
+        return (tf_core_dump_data, extra_data)
 
 core_dump_path = os.path.join(tempfile.gettempdir(), "tf_coredump.elf")
 
@@ -101,12 +225,14 @@ if __name__ == '__main__':
 
 
     try:
-        tf_coredump_json = get_tf_coredump_data(core_dump_path)
+        tf_coredump_json, extra_data = get_tf_coredump_data(core_dump_path)
         if not tf_coredump_json:
             print("Core dump in debug log has no TF coredump info.")
             sys.exit (-1)
 
         tf_coredump_data = json.loads(tf_coredump_json)
+        extra_data = format_extra_data(extra_data).replace('\n', '\\n')
+
         elf_name = tf_coredump_data['firmware_file_name'] + ".elf"
         script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -139,6 +265,7 @@ if __name__ == '__main__':
                            "-ex 'echo     - Run \"thread apply all bt full\" to print traces of all threads.\n' " +
                            "-ex 'echo\n' " +
                           f"-ex 'echo Crashed firmware {tf_coredump_data['firmware_file_name']}\n' " +
+                          f"-ex 'echo {extra_data}\n' " +
                            "-ex 'echo ================================================================================\n' " +
                            "-ex 'echo ============================= Registers at crash ===============================\n' " +
                            "-ex 'echo ================================================================================\n' " +
