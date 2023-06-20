d00d33ceec000130fb9c7d0cd5178a3b191a0297
Matthias Bolte <matthias@tinkerforge.com>
2023-06-20 17:42:50 +0200

    em-analysis: Fix flags data lookup for legend in case of hidden series
    The quad tree lookup doesn't work in case of moving hidden series out
    of the way.

--- software/web/src/ts/uplot-plugins.ts
+++ software/web/src/ts/uplot-plugins.ts
@@ -21,179 +21,8 @@
 
 import uPlot from 'uplot';
 
-function pointWithin(px: number, py: number, rlft: number, rtop: number, rrgt: number, rbtm: number) {
-    return px >= rlft && px <= rrgt && py >= rtop && py <= rbtm;
-}
-
-class Quadtree {
-    MAX_OBJECTS = 10;
-    MAX_LEVELS = 4;
-
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-    l: number;
-    o: {x: number, y: number, w: number, h: number, sidx: number, didx: number}[];
-    q: Quadtree[];
-
-    constructor(x: number, y: number, w: number, h: number, l?: number) {
-        let t = this;
-
-        t.x = x;
-        t.y = y;
-        t.w = w;
-        t.h = h;
-        t.l = l || 0;
-        t.o = [];
-        t.q = null;
-    };
-
-    split() {
-        let t = this,
-            x = t.x,
-            y = t.y,
-            w = t.w / 2,
-            h = t.h / 2,
-            l = t.l + 1;
-
-        t.q = [
-            // top right
-            new Quadtree(x + w, y,     w, h, l),
-            // top left
-            new Quadtree(x,     y,     w, h, l),
-            // bottom left
-            new Quadtree(x,     y + h, w, h, l),
-            // bottom right
-            new Quadtree(x + w, y + h, w, h, l),
-        ];
-    }
-
-    // invokes callback with index of each overlapping quad
-    quads(x: number, y: number, w: number, h: number, cb: (q: Quadtree) => void) {
-        let t            = this,
-            q            = t.q,
-            hzMid        = t.x + t.w / 2,
-            vtMid        = t.y + t.h / 2,
-            startIsNorth = y     < vtMid,
-            startIsWest  = x     < hzMid,
-            endIsEast    = x + w > hzMid,
-            endIsSouth   = y + h > vtMid;
-
-        // top-right quad
-        startIsNorth && endIsEast && cb(q[0]);
-        // top-left quad
-        startIsWest && startIsNorth && cb(q[1]);
-        // bottom-left quad
-        startIsWest && endIsSouth && cb(q[2]);
-        // bottom-right quad
-        endIsEast && endIsSouth && cb(q[3]);
-    }
-
-    add(o: {x: number, y: number, w: number, h: number, sidx: number, didx: number}) {
-        let t = this;
-
-        if (t.q != null) {
-            t.quads(o.x, o.y, o.w, o.h, (q: Quadtree) => {
-                q.add(o);
-            });
-        }
-        else {
-            let os = t.o;
-
-            os.push(o);
-
-            if (os.length > this.MAX_OBJECTS && t.l < this.MAX_LEVELS) {
-                t.split();
-
-                for (let i = 0; i < os.length; i++) {
-                    let oi = os[i];
-
-                    t.quads(oi.x, oi.y, oi.w, oi.h, (q: Quadtree) => {
-                        q.add(oi);
-                    });
-                }
-
-                t.o.length = 0;
-            }
-        }
-    }
-
-    get(x: number, y: number, w: number, h: number, cb: (q: {x: number, y: number, w: number, h: number, sidx: number, didx: number}) => void) {
-        let t = this;
-        let os = t.o;
-
-        for (let i = 0; i < os.length; i++)
-            cb(os[i]);
-
-        if (t.q != null) {
-            t.quads(x, y, w, h, (q: Quadtree) => {
-                q.get(x, y, w, h, cb);
-            });
-        }
-    }
-
-    clear() {
-        this.o.length = 0;
-        this.q = null;
-    }
-}
-
-function roundDec(val: number, dec: number) {
-    return Math.round(val * (dec = 10**dec)) / dec;
-}
-
-const SPACE_BETWEEN = 1;
-const SPACE_AROUND  = 2;
-const SPACE_EVENLY  = 3;
-
-const coord = (i: number, offs: number, iwid: number, gap: number) => roundDec(offs + i * (iwid + gap), 6);
-
-function distr(numItems: number, sizeFactor: number, justify: number, onlyIdx: number, each: (i: number, offPct: number, dimPct: number) => void) {
-    let space = 1 - sizeFactor;
-
-    let gap =  (
-        justify == SPACE_BETWEEN ? space / (numItems - 1) :
-        justify == SPACE_AROUND  ? space / (numItems    ) :
-        justify == SPACE_EVENLY  ? space / (numItems + 1) : 0
-    );
-
-    if (isNaN(gap) || gap == Infinity)
-        gap = 0;
-
-    let offs = (
-        justify == SPACE_BETWEEN ? 0       :
-        justify == SPACE_AROUND  ? gap / 2 :
-        justify == SPACE_EVENLY  ? gap     : 0
-    );
-
-    let iwid = sizeFactor / numItems;
-    let _iwid = roundDec(iwid, 6);
-
-    if (onlyIdx == null) {
-        for (let i = 0; i < numItems; i++)
-            each(i, coord(i, offs, iwid, gap), _iwid);
-    }
-    else
-        each(onlyIdx, coord(onlyIdx, offs, iwid, gap), _iwid);
-}
-
-const { round, min, max, ceil } = Math;
-
-export function uPlotTimelinePlugin(opts: any) {
-    const { fill, stroke } = opts;
-
-    const laneWidth   = 0.9;
-    const laneDistr   = SPACE_BETWEEN;
-
-    function walk(yIdx: number, count: number, dim: number, draw: (iy: number, y0: number, hgt: number) => void) {
-        distr(count, laneWidth, laneDistr, yIdx, (i: number, offPct: number, dimPct: number) => {
-            let laneOffPx = dim * offPct;
-            let laneWidPx = dim * dimPct;
-
-            draw(i, laneOffPx, laneWidPx);
-        });
-    }
+export function uPlotTimelinePlugin(options: any) {
+    const { fill, stroke } = options;
 
     const fillPaths = new Map();
     const strokePaths = new Map();
@@ -213,7 +42,7 @@ export function uPlotTimelinePlugin(opts: any) {
         strokePaths.clear();
     }
 
-    function putBox(ctx: CanvasRenderingContext2D, rect: uPlot.RectH | uPlot.RectV, xOff: number, yOff: number, lft: number, top: number, wid: number, hgt: number, strokeWidth: number, iy: number, ix: number, value: number|null) {
+    function putBox(ctx: CanvasRenderingContext2D, rect: uPlot.RectH | uPlot.RectV, lft: number, top: number, wid: number, hgt: number, strokeWidth: number, iy: number, ix: number, value: number|null) {
         let fillStyle = fill(iy + 1, ix, value);
         let fillPath = fillPaths.get(fillStyle);
 
@@ -231,33 +60,24 @@ export function uPlotTimelinePlugin(opts: any) {
 
             rect(strokePath, lft + strokeWidth / 2, top + strokeWidth / 2, wid - strokeWidth, hgt - strokeWidth);
         }
-
-        qt.add({
-            x: round(lft - xOff),
-            y: round(top - yOff),
-            w: wid,
-            h: hgt,
-            sidx: iy + 1,
-            didx: ix
-        });
     }
 
     function drawPaths(u: uPlot, sidx: number, idx0: number, idx1: number): uPlot.Series.Paths|null {
         uPlot.orient(u, sidx, (series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim, moveTo, lineTo, rect) => {
-            let strokeWidth = round((series.width || 0) * devicePixelRatio);
+            let strokeWidth = Math.round((series.width || 0) * devicePixelRatio);
 
             u.ctx.save();
             rect(u.ctx, u.bbox.left, u.bbox.top, u.bbox.width, u.bbox.height);
             u.ctx.clip();
 
-            let hgt = opts.bar_height * devicePixelRatio;
+            let hgt = options.bar_height * devicePixelRatio;
             let iy = sidx - 1;
             let y0 = 0;
             let last_rgt: number = undefined;
 
             for (let i = 1; i < sidx; i++) {
                 if (u.series[i].show) {
-                    y0 += (opts.bar_height + opts.bar_spacing) * devicePixelRatio;
+                    y0 += (options.bar_height + options.bar_spacing) * devicePixelRatio;
                 }
             }
 
@@ -272,7 +92,7 @@ export function uPlotTimelinePlugin(opts: any) {
                         lft = last_rgt;
                     }
                     else {
-                        lft = round(valToPosX(dataX[ix], scaleX, xDim, xOff));
+                        lft = Math.round(valToPosX(dataX[ix], scaleX, xDim, xOff));
                     }
 
                     let nextIx = ix;
@@ -285,19 +105,17 @@ export function uPlotTimelinePlugin(opts: any) {
                         rgt1 = valToPosX(dataX[nextIx], scaleX, xDim, xOff);
                     }
 
-                    let rgt = round(rgt0 + (rgt1 - rgt0) / 2);
+                    let rgt = Math.round(rgt0 + (rgt1 - rgt0) / 2);
 
                     last_rgt = rgt;
 
                     putBox(
                         u.ctx,
                         rect,
-                        xOff,
-                        yOff,
                         lft,
-                        round(yOff + y0),
-                        max(rgt - lft, 1),
-                        round(hgt),
+                        Math.round(yOff + y0),
+                        Math.max(rgt - lft, 1),
+                        Math.round(hgt),
                         strokeWidth,
                         iy,
                         ix,
@@ -317,19 +135,8 @@ export function uPlotTimelinePlugin(opts: any) {
         return null;
     }
 
-    let qt: Quadtree;
-
     return {
         hooks: {
-            drawClear: (u: uPlot) => {
-                qt = qt || new Quadtree(0, 0, u.bbox.width, u.bbox.height);
-                qt.clear();
-
-                // force-clear the path cache to cause drawBars() to rebuild new quadtree
-                u.series.forEach(s => {
-                    (s as any)._paths = null;
-                });
-            },
             addSeries: (u: uPlot, seriesIdx: number) => {
                 if (seriesIdx > 0) {
                     uPlot.assign(u.series[seriesIdx], {
@@ -343,31 +150,23 @@ export function uPlotTimelinePlugin(opts: any) {
                 cursor: {
                     y: false,
                     dataIdx: (u: uPlot, seriesIdx: number, closestIdx: number, xValue: number) => {
-                        if (seriesIdx == 0)
+                        if (seriesIdx == 0) {
                             return closestIdx;
-
-                        let cx = round(u.cursor.left * devicePixelRatio);
-                        let hovered = Array(u.series.length - 1).fill(null);
-
-                        if (cx >= 0) {
-                            let yMids = Array(u.series.length - 1).fill(0);
-
-                            walk(null, u.series.length - 1, u.bbox.height, (iy: number, y0: number, hgt: number) => {
-                                // vertical midpoints of each series' timeline (stored relative to .u-over)
-                                yMids[iy] = round(y0 + hgt / 2);
-                            });
-
-                            let cy = yMids[seriesIdx - 1];
-
-                            hovered[seriesIdx - 1] = null;
-
-                            qt.get(cx, cy, 1, 1, (o: {x: number, y: number, w: number, h: number, sidx: number, didx: number}) => {
-                                if (pointWithin(cx, cy, o.x, o.y, o.x + o.w, o.y + o.h))
-                                    hovered[seriesIdx - 1] = o;
-                            });
                         }
 
-                        return hovered[seriesIdx - 1]?.didx;
+                        let didx = u.posToIdx(Math.round(u.cursor.left));
+                        let data = u.data[seriesIdx][didx];
+
+                        while (data === undefined && didx > 0) {
+                            --didx;
+                            data = u.data[seriesIdx][didx];
+                        }
+
+                        if (data === undefined) {
+                            return undefined;
+                        }
+
+                        return didx;
                     },
                     points: {
                         show: false,
@@ -383,23 +182,19 @@ export function uPlotTimelinePlugin(opts: any) {
             uPlot.assign(opts.axes[1], {
                 splits: (u: uPlot, axisIdx: number) => {
                     let count = 0;
+                    let splits = [];
 
                     for (let i = 1; i < u.series.length; i++) {
                         if (u.series[i].show) {
+                            let mid = Math.round(options.bar_height / 2 + count * (options.bar_height +  options.bar_spacing));
+
+                            splits.push(u.posToVal(mid, "y"));
+
                             ++count;
                         }
                     }
 
-                    let yMids = Array(count).fill(0);
-                    let ySplits = Array(count).fill(0);
-
-                    walk(null, count, u.bbox.height, (iy: number, y0: number, hgt: number) => {
-                        // vertical midpoints of each series' timeline (stored relative to .u-over)
-                        yMids[iy] = round(y0 + hgt / 2);
-                        ySplits[iy] = u.posToVal(yMids[iy] / devicePixelRatio, "y");
-                    });
-
-                    return ySplits;
+                    return splits;
                 },
                 values: (u: uPlot) => {
                     let values: string[] = [];
@@ -418,9 +213,13 @@ export function uPlotTimelinePlugin(opts: any) {
 
                     return values;
                 },
-                grid:       {show: false},
-                ticks:      {show: false},
-                side:       3,
+                grid: {
+                    show: false,
+                },
+                ticks: {
+                    show: false,
+                },
+                side: 3,
             });
 
             opts.series.forEach((s: uPlot.Series, i: number) => {
