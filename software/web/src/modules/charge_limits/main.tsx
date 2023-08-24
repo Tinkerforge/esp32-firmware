b2bef7b662806ec466e1a1a1d641fe76074f7c11
Frederic Henrichs <frederic@tinkerforge.com>
2023-08-24 13:20:54 +0200

    cron: add charge_limits frontend.

--- software/web/src/modules/charge_limits/main.tsx
+++ software/web/src/modules/charge_limits/main.tsx
@@ -26,7 +26,11 @@ import { h, render, Fragment } from "preact";
 import { __ } from "../../ts/translation";
 
 import { FormRow } from "../../ts/components/form_row";
-import { InputSelect } from "../../ts/components/input_select";
+import { InputSelect } from "src/ts/components/input_select";
+import { cron_action, cron_action_configs, cron_action_defaults, cron_action_dict, cron_action_names, cron_trigger, cron_trigger_configs, cron_trigger_defaults, cron_trigger_dict, cron_trigger_names } from "../cron/api";
+import { ChargeLimitsCronTrigger } from "./cron_trigger";
+import { Cron } from "../cron/main";
+import { ChargeLimitsCronAction } from "./cron_action";
 
 function ChargeLimitsStatus() {
     if (!util.render_allowed() || !API.hasFeature("evse"))
@@ -189,3 +193,113 @@ export function add_event_listeners()
 export function update_sidebar_state(module_init: any) {
     $('#sidebar-charge_limits').prop('hidden', !module_init.charge_limits);
 }
+
+// TODO: Think about a nice explanation why this does not need a config
+function ChargeLimitsCronTriggerComponent(_: cron_trigger) {
+    return __("charge_limits.content.charge_limits_expiration");
+}
+
+function ChargeLimitsCronTriggerConfig(_: Cron, __: cron_trigger): any[] {
+    return []
+}
+
+function ChargeLimitsCronTriggerFactory(): cron_trigger {
+    return [6 as any, {}];
+}
+
+cron_trigger_configs[6] = ChargeLimitsCronTriggerConfig;
+cron_trigger_dict[6] = ChargeLimitsCronTriggerComponent;
+cron_trigger_defaults[6] = ChargeLimitsCronTriggerFactory;
+cron_trigger_names[6] = __("charge_limits.content.charge_limits");
+
+function ChargeLimitsCronActionComponent(cron: cron_action) {
+    const props = (cron as any as ChargeLimitsCronAction)[1];
+    const durations = [
+        __("charge_limits.content.unlimited"),
+        __("charge_limits.content.min15"),
+        __("charge_limits.content.min30"),
+        __("charge_limits.content.min45"),
+        __("charge_limits.content.h1"),
+        __("charge_limits.content.h2"),
+        __("charge_limits.content.h3"),
+        __("charge_limits.content.h4"),
+        __("charge_limits.content.h6"),
+        __("charge_limits.content.h8"),
+        __("charge_limits.content.h12")
+    ]
+    let ret = __("charge_limits.content.energy") + ": " + (props.energy_wh != 0 ? props.energy_wh / 1000 + " kWh\n" : __("charge_limits.content.unlimited")) +"\n";
+    ret += __("charge_limits.content.duration") + ": " + durations[props.duration];
+    return ret;
+}
+
+function ChargeLimitsCronActionConfig(cron_object: Cron, props: cron_action) {
+    const state = props as any as ChargeLimitsCronAction;
+    const energy_items: [string, string][] = [
+        ["0", __("charge_limits.content.unlimited")],
+        ["5000", util.toLocaleFixed(5, 0) + " kWh"],
+        ["10000", util.toLocaleFixed(10, 0) + " kWh"],
+        ["15000", util.toLocaleFixed(15, 0) + " kWh"],
+        ["20000", util.toLocaleFixed(20, 0) + " kWh"],
+        ["25000", util.toLocaleFixed(25, 0) + " kWh"],
+        ["30000", util.toLocaleFixed(30, 0) + " kWh"],
+        ["40000", util.toLocaleFixed(40, 0) + " kWh"],
+        ["50000", util.toLocaleFixed(50, 0) + " kWh"],
+        ["60000", util.toLocaleFixed(60, 0) + " kWh"],
+        ["70000", util.toLocaleFixed(70, 0) + " kWh"],
+        ["80000", util.toLocaleFixed(80, 0) + " kWh"],
+        ["90000", util.toLocaleFixed(90, 0) + " kWh"],
+        ["100000", util.toLocaleFixed(100, 0) + " kWh"]
+    ];
+
+    const duration_items: [string, string][] = [
+        ["0", __("charge_limits.content.unlimited")],
+        ["1", __("charge_limits.content.min15")],
+        ["2", __("charge_limits.content.min30")],
+        ["3", __("charge_limits.content.min45")],
+        ["4", __("charge_limits.content.h1")],
+        ["5", __("charge_limits.content.h2")],
+        ["6", __("charge_limits.content.h3")],
+        ["7", __("charge_limits.content.h4")],
+        ["8", __("charge_limits.content.h6")],
+        ["9", __("charge_limits.content.h8")],
+        ["10", __("charge_limits.content.h12")]
+    ];
+
+    return [
+        {
+            name: __("charge_limits.content.energy"),
+            value:  <InputSelect
+                    items={energy_items}
+                    value={state[1].energy_wh.toString()}
+                    onValue={(v) => {
+                        state[1].energy_wh = parseInt(v);
+                        cron_object.setActionFromComponent(state as any)
+                    }}/>
+        },
+        {
+            name: __("charge_limits.content.duration"),
+            value: <InputSelect
+                items={duration_items}
+                value={state[1].duration.toString()}
+                onValue={(v) => {
+                    state[1].duration = parseInt(v);
+                    cron_object.setActionFromComponent(state as any);
+                }}/>
+        }
+    ]
+}
+
+function ChargeLimitsCronActionFactory(): cron_action {
+    return [
+        8 as any,
+        {
+            duration: 0,
+            energy_wh: 0
+        }
+    ];
+}
+
+cron_action_configs[8] = ChargeLimitsCronActionConfig;
+cron_action_dict[8] = ChargeLimitsCronActionComponent;
+cron_action_defaults[8] = ChargeLimitsCronActionFactory;
+cron_action_names[8] = __("charge_limits.content.charge_limits");
