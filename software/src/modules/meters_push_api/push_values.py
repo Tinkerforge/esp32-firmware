e8441b77b4d481fd97a65a3036712a1b65f7d838
Matthias Bolte <matthias@tinkerforge.com>
2023-09-08 11:29:29 +0200

    meters-push-api: Add push test script

---
+++ software/src/modules/meters_push_api/push_values.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3 -u
+
+import urllib.request
+import json
+import argparse
+from datetime import datetime
+import time
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host')
+    parser.add_argument('meter', type=int)
+    parser.add_argument('value', type=float)
+    parser.add_argument('--interval', type=float, default='0.2')
+    parser.add_argument('--timeout', type=float, default='0.5')
+
+    args = parser.parse_args()
+    print(args)
+    counter = 0
+
+    while True:
+        start = time.time()
+        request = urllib.request.Request(f'http://{args.host}/meters/{args.meter}/push_values',
+                                         data=json.dumps([args.value]).encode('utf-8'),
+                                         method='PUT',
+                                         headers={'Content-Type': 'application/json'})
+        try:
+            with urllib.request.urlopen(request, timeout=args.timeout) as f:
+                response = f.read().decode('utf-8')
+
+                if len(response) > 0:
+                    print(f'{datetime.now().isoformat()} [{counter}] response: {response}')
+        except Exception as e:
+            print(f'{datetime.now().isoformat()} [{counter}] error: {e}')
+
+        end = time.time()
+        duration = end - start
+
+        if duration < args.interval:
+            time.sleep(args.interval - duration)
+
+        counter += 1
+
+if __name__ == '__main__':
+    main()
