2481819c223fc97ec885bc4ba198e3ef065f97fc
Matthias Bolte <matthias@tinkerforge.com>
2023-10-06 16:39:46 +0200

    web/meters: Start adding meter config

---
+++ software/web/src/modules/meters_em/translation_en.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource preact */
+import { h } from "preact";
+let x = {
+    "meters_em": {
+        "content": {
+            "meter_class": "Energy manager (locally connected)",
+
+            "config_display_name": "Name"
+        }
+    }
+}
--- software/web/src/modules/meters_push_api/plugin_meters_config.tsx
+++ software/web/src/modules/meters_push_api/plugin_meters_config.tsx
@@ -17,9 +17,13 @@
  * Boston, MA 02111-1307, USA.
  */
 
+import { h } from 'preact'
+import { __, translate_unchecked } from "../../ts/translation";
 import { MeterClassID } from "../meters/meters_defs";
+import { MeterValueID } from "../meters/meter_value_id";
 import { MeterConfig } from "../meters/types";
 import { TableModalRow } from "../../ts/components/table";
+import { InputText } from "../../ts/components/input_text";
 
 export type PushAPIMetersConfig = [
     MeterClassID.PushAPI,
@@ -32,12 +36,29 @@ export type PushAPIMetersConfig = [
 export function init() {
     return {
         [MeterClassID.PushAPI]: {
+            name: __("meters_push_api.content.meter_class"),
+            init: () => [MeterClassID.PushAPI, {display_name: "", value_ids: new Array<number>()}] as MeterConfig,
             clone: (config: MeterConfig) => [config[0], {...config[1]}] as MeterConfig,
-            get_edit_rows: (config: MeterConfig, on_value: (key: string, value: any) => void): TableModalRow[] => {
-                return []
-            },
-            get_add_rows: (config: MeterConfig, on_value: (key: string, value: any) => void): TableModalRow[] => {
-                return []
+            get_edit_rows: (config: PushAPIMetersConfig, on_value: (config: PushAPIMetersConfig) => void): TableModalRow[] => {
+                return [
+                    {
+                        name: __("meters_push_api.content.config_display_name"),
+                        value: <InputText
+                            required
+                            maxLength={32}
+                            value={config[1].display_name}
+                            onValue={(v) => {
+                                config[1].display_name = v;
+                                on_value(config);
+                            }}/>
+                    },
+                    {
+                        name: __("meters_push_api.content.config_value_ids"),
+                        value: <select class="form-control custom-select" multiple>
+                            {config[1].value_ids.map((value_id) => <option>{translate_unchecked(`meters.content.value_${value_id}`)}</option>)}
+                        </select>
+                    },
+                ];
             },
         },
     };
