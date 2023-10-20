b2407f5942521a4ed20ce9b0dd1c2e36b56cc860
Matthias Bolte <matthias@tinkerforge.com>
2023-10-20 18:21:05 +0200

    meters-sun-spec: Report scan results in API and show them in frontend

--- software/web/src/modules/meters_sun_spec/plugin_meters_config.tsx
+++ software/web/src/modules/meters_sun_spec/plugin_meters_config.tsx
@@ -17,13 +17,17 @@
  * Boston, MA 02111-1307, USA.
  */
 
-import { h, Fragment } from "preact";
+import * as API from "../../ts/api";
+import * as util from "../../ts/util";
+import { h, Fragment, Component } from "preact";
 import { __ } from "../../ts/translation";
 import { MeterClassID } from "../meters/meters_defs";
 import { MeterConfig } from "../meters/types";
 import { TableModalRow } from "../../ts/components/table";
 import { InputText } from "../../ts/components/input_text";
 import { InputNumber } from "../../ts/components/input_number";
+import { FormRow } from "../../ts/components/form_row";
+import { Button, ListGroup, ListGroupItem, Spinner } from "react-bootstrap";
 
 export type SunSpecMetersConfig = [
     MeterClassID.SunSpec,
@@ -36,25 +40,123 @@ export type SunSpecMetersConfig = [
     },
 ];
 
+interface DeviceScannerResult {
+    display_name: string
+    device_address: number
+    model_id: number
+}
+
+interface DeviceScannerProps {
+    host: string
+    port: number
+    onResultSelected: (result: DeviceScannerResult) => void
+}
+
+interface DeviceScannerState {
+    scan_host: string
+    scan_port: number
+    scan_running: boolean
+    scan_log: string
+    scan_results: DeviceScannerResult[]
+}
+
+class DeviceScanner extends Component<DeviceScannerProps, DeviceScannerState> {
+    constructor() {
+        super();
+
+        this.state = {
+            scan_host: '',
+            scan_port: 0,
+            scan_running: false,
+            scan_log: '',
+            scan_results: [],
+        } as any;
+
+        util.addApiEventListener('meters_sun_spec/scan_log', (e) => {
+            if (e.data == '<<<clear_scan_log>>>') {
+                this.setState({scan_log: ''})
+            }
+            else {
+                this.setState({scan_log: this.state.scan_log + e.data + '\n'})
+            }
+        });
+
+        util.addApiEventListener('meters_sun_spec/scan_result', () => {
+            let scan_result = API.get('meters_sun_spec/scan_result');
+
+            if (scan_result.host === this.props.host && scan_result.port === this.props.port) {
+                this.setState({scan_results: this.state.scan_results.concat({
+                    display_name: scan_result.display_name,
+                    device_address: scan_result.device_address,
+                    model_id: scan_result.model_id,
+                })});
+            }
+        });
+
+        util.addApiEventListener('meters_sun_spec/scan_done', () => {
+            let scan_done = API.get('meters_sun_spec/scan_done');
+
+            if (scan_done.host === this.state.scan_host && scan_done.port === this.state.scan_port) {
+                this.setState({scan_host: "", scan_port: 0, scan_running: false});
+            }
+        });
+    }
+
+    render() {
+        return <>
+            <FormRow label="">
+                <Button variant="primary"
+                        className="form-control"
+                        onClick={async () => {
+                            this.setState({scan_host: this.props.host, scan_port: this.props.port, scan_running: true, scan_results: []});
+                            try {
+                                await API.call('meters_sun_spec/scan',
+                                               {host: this.props.host, port: this.props.port},
+                                               __("meters_sun_spec.content.scan_failed"));
+                            } catch {
+                                this.setState({scan_host: "", scan_port: 0, scan_running: false, scan_results: []});
+                            }
+                        }}
+                        disabled={this.props.host.trim().length == 0 || !util.hasValue(this.props.port) || this.state.scan_running}>
+                    {__("meters_sun_spec.content.scan")} <Spinner animation="border" size="sm" as="span" className="ml-2" hidden={!this.state.scan_running}/>
+                </Button>
+            </FormRow>
+
+            <FormRow label="">
+                <textarea class="text-monospace mb-1 form-control"
+                    readonly
+                    rows={20}
+                    style="resize: both; width: 100%; white-space: pre; line-height: 1.2; text-shadow: none; font-size: 0.875rem;">
+                    {this.state.scan_log}
+                </textarea>
+            </FormRow>
+
+            {this.state.scan_results.length > 0 ?
+                <FormRow label="">
+                    <ListGroup>
+                        {this.state.scan_results.map(scan_result =>
+                            <ListGroupItem action type="button" onClick={() => {this.props.onResultSelected(scan_result)}}>
+                                <h5 class="mb-1 pr-2">{scan_result.display_name}</h5>
+                                <div class="d-flex w-100 justify-content-between">
+                                    <span>{__("meters_sun_spec.content.config_device_address")}: {scan_result.device_address}</span>
+                                    <span>{__("meters_sun_spec.content.config_model_id")}: {scan_result.model_id}</span>
+                                </div>
+                            </ListGroupItem>)}
+                    </ListGroup>
+                </FormRow>
+                : undefined}
+        </>
+    }
+}
+
 export function init() {
     return {
         [MeterClassID.SunSpec]: {
             name: __("meters_sun_spec.content.meter_class"),
-            init: () => [MeterClassID.SunSpec, {display_name: "", host: "", port: 502, device_address: 0, model_id: 0}] as MeterConfig,
+            init: () => [MeterClassID.SunSpec, {display_name: "", host: "", port: 502, device_address: null, model_id: null}] as MeterConfig,
             clone: (config: MeterConfig) => [config[0], {...config[1]}] as MeterConfig,
             get_edit_rows: (config: SunSpecMetersConfig, on_value: (config: SunSpecMetersConfig) => void): TableModalRow[] => {
                 return [
-                    {
-                        name: __("meters_sun_spec.content.config_display_name"),
-                        value: <InputText
-                            required
-                            maxLength={32}
-                            value={config[1].display_name}
-                            onValue={(v) => {
-                                config[1].display_name = v;
-                                on_value(config);
-                            }}/>
-                    },
                     {
                         name: __("meters_sun_spec.content.config_host"),
                         value: <InputText
@@ -81,10 +183,34 @@ export function init() {
                             }}/>
                     },
                     {
+                        name: null,
+                        value: <>
+                            <hr/>
+                            <DeviceScanner host={config[1].host} port={config[1].port} onResultSelected={(result: DeviceScannerResult) => {
+                                config[1].display_name = result.display_name;
+                                config[1].device_address = result.device_address;
+                                config[1].model_id = result.model_id;
+                                on_value(config);
+                            }} />
+                            <hr/>
+                        </>
+                    },
+                    {
+                        name: __("meters_sun_spec.content.config_display_name"),
+                        value: <InputText
+                            required
+                            maxLength={32}
+                            value={config[1].display_name}
+                            onValue={(v) => {
+                                config[1].display_name = v;
+                                on_value(config);
+                            }}/>
+                    },
+                    {
                         name: __("meters_sun_spec.content.config_device_address"),
                         value: <InputNumber
                             required
-                            min={0}
+                            min={1}
                             max={247}
                             value={config[1].device_address}
                             onValue={(v) => {
@@ -96,7 +222,7 @@ export function init() {
                         name: __("meters_sun_spec.content.config_model_id"),
                         value: <InputNumber
                             required
-                            min={0}
+                            min={2}
                             max={65535}
                             value={config[1].model_id}
                             onValue={(v) => {
