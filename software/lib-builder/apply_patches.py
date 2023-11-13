1fcc0ffd29a21c1f1c17ba48a2f9edcfa25b16e7
Erik Fleckstein <erik@tinkerforge.com>
2023-11-13 13:40:09 +0100

    lib_builder/apply_patches: Add skip argument.

--- software/lib-builder/apply_patches.py
+++ software/lib-builder/apply_patches.py
@@ -17,12 +17,13 @@ class ChangedDirectory(object):
     def __exit__(self, type_, value, traceback):
         os.chdir(self.previous_path)
 
-if len(sys.argv) != 3:
-    print("Usage: {} path/to/esp32-arduino-lib-builder path/to/patches".format(sys.argv[0]))
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+    print("Usage: {} path/to/esp32-arduino-lib-builder path/to/patches [number of first to apply]".format(sys.argv[0]))
     sys.exit(1)
 
 root_dir = sys.argv[1]
 patch_dir = sys.argv[2]
+skip = int(sys.argv[3]) if len(sys.argv) > 3 else -1
 
 for root, dirs, files in os.walk(patch_dir):
     if len(files) == 0:
@@ -31,6 +32,10 @@ for root, dirs, files in os.walk(patch_dir):
     files = [os.path.abspath(os.path.join(root, x)) for x in files]
     with ChangedDirectory(os.path.join(root_dir, dir_to_apply_in)):
         for patch in sorted(files):
+            i = int(os.path.basename(patch).split("-")[0])
+            if i < skip:
+                print("Skipping", patch)
+                continue
             print("Calling", *["git", "am", patch], "in", os.path.join(root_dir, dir_to_apply_in))
             subprocess.call(["git", "am", patch], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
     #print(os.path.relpath(root, start=patch_dir), dirs, files)
