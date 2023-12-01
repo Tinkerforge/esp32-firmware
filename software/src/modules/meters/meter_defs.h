c091f335582d62aab6949e02014066b4dfcce7ea
Erik Fleckstein <erik@tinkerforge.com>
2023-12-01 14:13:09 +0100

    meters: Fix meter_type define order.

--- software/src/modules/meters/meter_defs.h
+++ software/src/modules/meters/meter_defs.h
@@ -30,13 +30,13 @@
 #define METER_TYPE_SDM630 2
 #define METER_TYPE_SDM72DMV2 3
 
-// Supported by evse_v2 module
-#define METER_TYPE_DSZ15DZMOD 6
-#define METER_TYPE_DEM4A 7
-
 // Supported by energy_manager module
 #define METER_TYPE_SDM72CTM 4
 #define METER_TYPE_SDM630MCTV2 5
+
+// Supported by evse_v2 module
+#define METER_TYPE_DSZ15DZMOD 6
+#define METER_TYPE_DEM4A 7
 
 // Supported by mqtt_meter module
 #define METER_TYPE_CUSTOM_BASIC 200
