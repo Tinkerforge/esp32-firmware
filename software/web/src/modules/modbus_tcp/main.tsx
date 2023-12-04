162ffc51ecd16757256731682ccab68ec9867f3e
Frederic Henrichs <frederic@tinkerforge.com>
2023-12-04 12:19:10 +0100

    modbus_tcp_frontend: remove unused variables.

--- software/web/src/modules/modbus_tcp/main.tsx
+++ software/web/src/modules/modbus_tcp/main.tsx
@@ -40,11 +40,6 @@ interface config {
     evse_enable: boolean
 }
 
-const input_count = 23;
-const holding_count = 7 + input_count;
-const discrete_count = 12 + holding_count;
-const coil_count = 2 + discrete_count;
-
 export class ModbusTCP extends ConfigComponent<'modbus_tcp/config', {}, config> {
     constructor() {
         super('modbus_tcp/config',
