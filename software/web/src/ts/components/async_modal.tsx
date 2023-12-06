9478d4467be118a28c811e37016f520c384937f6
Frederic Henrichs <frederic@tinkerforge.com>
2023-12-06 10:08:37 +0100

    async_modal: enable nesting the async modal.

--- software/web/src/ts/components/async_modal.tsx
+++ software/web/src/ts/components/async_modal.tsx
@@ -24,7 +24,7 @@ import * as util from "../../ts/util";
 interface AsyncModalProps {
 }
 
-interface AsyncModalStrings {
+interface AsyncModalParams {
     title: string;
     body: ComponentChildren;
     no_variant: string;
@@ -32,16 +32,18 @@ interface AsyncModalStrings {
 
     yes_variant: string;
     yes_text: string;
+
+    nestingDepth?: number
 }
 
-interface AsyncModalState extends AsyncModalStrings {
+interface AsyncModalState extends AsyncModalParams {
     show: boolean;
 
     promiseResolve: (value: boolean | PromiseLike<boolean>) => void;
 }
 
 export class AsyncModal extends Component<AsyncModalProps, AsyncModalState> {
-    show = async (strings: AsyncModalStrings) => {
+    show = async (strings: AsyncModalParams) => {
         return new Promise<boolean>((resolve) => {
             this.setState({
                 show: true,
@@ -61,7 +63,11 @@ export class AsyncModal extends Component<AsyncModalProps, AsyncModalState> {
 
     render(props: AsyncModalProps, state: Readonly<AsyncModalState>) {
         return (
-           <Modal show={state.show} onHide={() => {this.hide(false)}} centered>
+           <Modal show={state.show}
+                    onHide={() => {this.hide(false)}}
+                    centered
+                    backdropClassName={this.state.nestingDepth ? "modal-backdrop-" + this.state.nestingDepth : ""}
+                    className={this.state.nestingDepth ? "modal-" + this.state.nestingDepth : ""}>
                 {/* There seems to be an incompatibility between preact's and react-bootstrap's typings*/ }
                 <Modal.Header {...{closeButton: true} as any}>
                     <label class="modal-title form-label">{state.title}</label>
