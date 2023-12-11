4527a940c7603d2fc0534cf6df3f436d2925b0d8
Frederic Henrichs <frederic@tinkerforge.com>
2023-12-11 09:45:56 +0100

    input_password: ensure that invalid-feedback is shown.

--- software/web/src/ts/components/input_password.tsx
+++ software/web/src/ts/components/input_password.tsx
@@ -80,7 +80,7 @@ export class InputPassword extends Component<InputPasswordProps, InputPasswordSt
 
         return (
             <>
-                <div class="input-group">
+                <div class="input-group has-validation">
                     <input class={"form-control" + (props.showAlways && props.hideClear ? " rounded-right" : "")}
                         id={id}
                         type={state.show || props.showAlways ? "text" : "password"}
