4b7c0b3be408951d69b35c0f5daaf0430a0109e7
Frederic Henrichs <frederic@tinkerforge.com>
2023-12-12 13:16:56 +0100

    cron_evse: fix crash when providing a nullptr as data for StateChange trigger.

--- software/src/modules/evse_common/evse_common.cpp
+++ software/src/modules/evse_common/evse_common.cpp
@@ -311,10 +311,16 @@ bool EvseCommon::action_triggered(Config *config, void *data) {
     uint32_t *states = (uint32_t*)data;
     switch (config->getTag<CronTriggerID>()) {
         case CronTriggerID::IECChange:
-                if ((cfg->get("new_charger_state")->asInt() == states[1]
-                    || cfg->get("new_charger_state")->asInt() == -1)
-                    && (cfg->get("old_charger_state")->asInt() == states[0] || cfg->get("old_charger_state")->asInt() == -1) )
-                    return true;
+        {
+            if (states == nullptr) {
+                states = new uint32_t[2];
+            }
+            defer { delete[] states;};
+            if ((cfg->get("new_charger_state")->asInt() == states[1]
+                || cfg->get("new_charger_state")->asInt() == -1)
+                && (cfg->get("old_charger_state")->asInt() == states[0] || cfg->get("old_charger_state")->asInt() == -1) )
+                return true;
+        }
             break;
 
         case CronTriggerID::EVSEExternalCurrentWd:
