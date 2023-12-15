4899654215592bab39482a55a1f037880473c3aa
Matthias Bolte <matthias@tinkerforge.com>
2023-12-15 15:52:33 +0100

    web/em-analysis: Make retry in case of cache update error less aggressive

--- software/web/src/modules/em_energy_analysis/main.tsx
+++ software/web/src/modules/em_energy_analysis/main.tsx
@@ -38,6 +38,8 @@ import { uPlotTimelinePlugin } from "../../ts/uplot-plugins";
 import { MeterValueID } from "../meters/meter_value_id";
 import { MeterConfig } from "../meters/types";
 
+const UPDATE_RETRY_DELAY = 500; // ms
+
 interface CachedData {
     update_timestamp: number;
     use_timestamp: number;
@@ -2579,7 +2581,7 @@ export class EMEnergyAnalysis extends Component<EMEnergyAnalysisProps, EMEnergyA
                 if (!success) {
                     window.setTimeout(() => {
                         this.reload_wallbox_cache();
-                    }, 100);
+                    }, UPDATE_RETRY_DELAY);
 
                     return;
                 }
@@ -2623,7 +2625,7 @@ export class EMEnergyAnalysis extends Component<EMEnergyAnalysisProps, EMEnergyA
                 if (!success) {
                     window.setTimeout(() => {
                         this.update_current_5min_cache();
-                    }, 100);
+                    }, UPDATE_RETRY_DELAY);
 
                     return;
                 }
@@ -2645,7 +2647,7 @@ export class EMEnergyAnalysis extends Component<EMEnergyAnalysisProps, EMEnergyA
                 if (!success) {
                     window.setTimeout(() => {
                         this.update_current_daily_cache();
-                    }, 100);
+                    }, UPDATE_RETRY_DELAY);
 
                     return;
                 }
