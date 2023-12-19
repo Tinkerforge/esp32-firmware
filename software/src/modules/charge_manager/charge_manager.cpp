49340b9ddcfd24cbd1d005204fef49376e2d142c
Erik Fleckstein <erik@tinkerforge.com>
2023-12-19 15:50:47 +0100

    charge_manager: Fix shadowing of available_current ConfigRoot.

--- software/src/modules/charge_manager/charge_manager.cpp
+++ software/src/modules/charge_manager/charge_manager.cpp
@@ -561,7 +561,7 @@ void ChargeManager::distribute_current()
         printed_all_chargers_seen = true;
     }
 
-    uint32_t available_current = seen_all_chargers_local ? available_current.get("current")->asUint() : 0;
+    uint32_t available = seen_all_chargers_local ? available_current.get("current")->asUint() : 0;
 
     bool use_3phase_minimum_current = available_phases.get("phases")->asUint() >= 3;
     uint32_t minimum_current = use_3phase_minimum_current ? this->minimum_current :
@@ -647,7 +647,7 @@ void ChargeManager::distribute_current()
 
         if (unreachable_evse_found) {
             // Shut down everything.
-            available_current = 0;
+            available = 0;
             LOCAL_LOG("%s", "stage 0: Unreachable, unreactive or misconfigured EVSE(s) found. Setting available current to 0 mA.");
             state.get("state")->updateUint(2);
 
@@ -684,7 +684,7 @@ void ChargeManager::distribute_current()
                   chargers_requesting_current,
                   chargers_requesting_current == 1 ? "" : "s",
                   chargers_requesting_current == 1 ? "s" : "",
-                  available_current);
+                  available);
 
         std::stable_sort(idx_array, idx_array + charger_count, [this](int left, int right) {
             return this->charger_state[left].requested_current < this->charger_state[right].requested_current;
@@ -722,8 +722,8 @@ void ChargeManager::distribute_current()
                 continue;
             }
 
-            if (available_current < current_to_set) {
-                LOCAL_LOG("stage 0: %u mA left, but %u mA required to unblock another charger. Blocking all following chargers.",available_current, current_to_set);
+            if (available < current_to_set) {
+                LOCAL_LOG("stage 0: %u mA left, but %u mA required to unblock another charger. Blocking all following chargers.",available, current_to_set);
                 current_to_set = 0;
             }
 
@@ -732,17 +732,17 @@ void ChargeManager::distribute_current()
             }
 
             current_array[idx_array[i]] = current_to_set;
-            available_current -= current_to_set;
+            available -= current_to_set;
 
             LOCAL_LOG("stage 0: Calculated target for %s (%s) of %u mA. %u mA left.",
                       this->get_charger_name(idx_array[i]),
                       this->hosts[idx_array[i]],
                       current_to_set,
-                      available_current);
+                      available);
         }
 
-        if (available_current > 0) {
-            LOCAL_LOG("stage 0: %u mA still available. Recalculating targets.", available_current);
+        if (available > 0) {
+            LOCAL_LOG("stage 0: %u mA still available. Recalculating targets.", available);
 
             int chargers_reallocated = 0;
             for (int i = 0; i < charger_count; ++i) {
@@ -750,7 +750,7 @@ void ChargeManager::distribute_current()
                     continue;
 
                 auto &charger = this->charger_state[idx_array[i]];
-                uint16_t current_per_charger = MIN(32000, available_current / (chargers_allocated_current_to - chargers_reallocated));
+                uint16_t current_per_charger = MIN(32000, available / (chargers_allocated_current_to - chargers_reallocated));
 
                 uint16_t requested_current = charger.requested_current;
                 // Protect against overflow.
@@ -762,21 +762,21 @@ void ChargeManager::distribute_current()
                 ++chargers_reallocated;
 
                 current_array[idx_array[i]] += current_to_add;
-                available_current -= current_to_add;
+                available -= current_to_add;
 
                 LOCAL_LOG("stage 0: Recalculated target for %s (%s) of %u mA. %u mA left.",
                           this->get_charger_name(idx_array[i]),
                           this->hosts[idx_array[i]],
                           current_array[idx_array[i]],
-                          available_current);
+                          available);
             }
         }
     }
 
     // Wake up chargers that already charged once.
     {
-        if (available_current > 0) {
-            LOCAL_LOG("stage 0: %u mA still available. Attempting to wake up chargers that already charged their vehicle once.", available_current);
+        if (available > 0) {
+            LOCAL_LOG("stage 0: %u mA still available. Attempting to wake up chargers that already charged their vehicle once.", available);
 
             uint16_t current_to_set = minimum_current;
             for (int i = 0; i < charger_count; ++i) {
@@ -800,8 +800,8 @@ void ChargeManager::distribute_current()
                               current_to_set);
                 }
 
-                if (available_current < current_to_set) {
-                    LOCAL_LOG("stage 0: %u mA left, but %u mA required to unblock another charger. Blocking all following chargers.",available_current, current_to_set);
+                if (available < current_to_set) {
+                    LOCAL_LOG("stage 0: %u mA left, but %u mA required to unblock another charger. Blocking all following chargers.",available, current_to_set);
                     current_to_set = 0;
                 }
 
@@ -810,13 +810,13 @@ void ChargeManager::distribute_current()
                 }*/
 
                 current_array[idx_array[i]] = current_to_set;
-                available_current -= current_to_set;
+                available -= current_to_set;
 
                 LOCAL_LOG("stage 0: Calculated target for %s (%s) of %u mA. %u mA left.",
                           this->get_charger_name(idx_array[i]),
                           this->hosts[idx_array[i]],
                           current_to_set,
-                          available_current);
+                          available);
             }
         }
     }
