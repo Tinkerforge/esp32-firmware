8a6cff2243f2c8814e5bf5e8c8af8fda5a6c9e1a
Frederic Henrichs <frederic@tinkerforge.com>
2023-12-21 14:49:28 +0100

    cron_web: move evse v2 specific translations from evse_common to evse_v2.

--- software/web/src/modules/evse_v2/translation_de.tsx
+++ software/web/src/modules/evse_v2/translation_de.tsx
@@ -10,7 +10,11 @@ let x = {
             "cron_trigger_shutdown_input": "Abschalteingang geschaltet",
             "cron_trigger_gp_input": "Konfigurierbarer Eingang geschaltet",
             "cron_trigger_active_low": "auf geschlossen",
-            "cron_trigger_active_high": "auf geöffnet"
+            "cron_trigger_active_high": "auf geöffnet",
+            "cron_sd_trigger_text": /*FFN*/(state: boolean) => <>Wenn der Abschalteingang <b>{state ? "geöffnet" : "geschlossen"}</b> wird,{" "}</>/*NF*/,
+            "cron_gpin_trigger_text": /*FFN*/(state: boolean) => <>Wenn der Konfigurierbare Eingang <b>{state ? "geöffnet" : "geschlossen"}</b> wird,{" "}</>/*NF*/,
+            "cron_button_trigger_text": /*FFN*/(state: boolean) => <>Wenn der Fronttaster <b>{state ? "gedrückt" : "losgelassen"}</b> wird,{" "}</>/*NF*/,
+            "cron_gpout_action_text": /*FFN*/(state: number) => state ? <><b>schalte</b> den Konfigurierbaren Ausgang <b>hochohmig</b>.</> : <><b>verbinde</b> den Konfigurierbaren Ausgang <b>mit Masse</b>.</>/*NF*/
         }
     }
 }
--- software/web/src/modules/evse_v2/translation_en.tsx
+++ software/web/src/modules/evse_v2/translation_en.tsx
@@ -10,7 +10,11 @@ let x = {
             "cron_trigger_shutdown_input": "Shutdown input switched",
             "cron_trigger_gp_input": "General purpose input switched",
             "cron_trigger_active_low": "to closed",
-            "cron_trigger_active_high": "to open"
+            "cron_trigger_active_high": "to open",
+            "cron_sd_trigger_text": /*FFN*/(state: boolean) => <>If the shutdown input switches to <b>{state ? "open" : "closed"}</b>{" "}</>/*NF*/,
+            "cron_gpin_trigger_text": /*FFN*/(state: boolean) => <>If the General Purpose input switches to <b>{state ? "open" : "closed"}</b>{" "}</>/*NF*/,
+            "cron_button_trigger_text": /*FFN*/(state: boolean) => <>If the button is <b>{state ? "pressed" : "released"}</b>{" "}</>/*NF*/,
+            "cron_gpout_action_text": /*FFN*/(state: number) => state ? <>set general purpose output to <b>high impedance</b>.</> : <><b>connect</b> general purpose output <b>to ground</b>.</>/*NF*/
         }
     }
 }
