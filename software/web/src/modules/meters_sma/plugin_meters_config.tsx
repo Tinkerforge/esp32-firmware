d4d43f04c6efb92fe5d291b37bcf89a2971220f0
Thomas Hein <github@poohnet.de>
2024-02-07 13:26:41 +0100

    WARP Energy Manager: SMA Meter

---
+++ software/web/src/modules/meters_sma/plugin_meters_config.tsx
@@ -0,0 +1,58 @@
+/* esp32-firmware
+ * Copyright (C) 2023 Matthias Bolte <matthias@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+import { h, ComponentChildren } from "preact";
+import { __, translate_unchecked } from "../../ts/translation";
+import * as util from "../../ts/util";
+import { MeterClassID } from "../meters/meters_defs";
+import { MeterConfig } from "../meters/types";
+import { InputText } from "../../ts/components/input_text";
+import { FormRow } from "../../ts/components/form_row";
+import * as API from "../../ts/api";
+
+export type SMAMetersConfig = [
+    MeterClassID.SMA,
+    {
+        display_name: string;
+    },
+];
+
+export function init() {
+    return {
+        [MeterClassID.SMA]: {
+            name: __("meters_sma.content.meter_class"),
+            new_config: () => [MeterClassID.SMA, {display_name: ""}] as MeterConfig,
+            clone_config: (config: MeterConfig) => [config[0], {...config[1]}] as MeterConfig,
+            get_edit_children: (config: SMAMetersConfig, on_config: (config: SMAMetersConfig) => void): ComponentChildren => {
+                return [
+                    <FormRow label={__("meters_sma.content.config_display_name")}>
+                        <InputText
+                            required
+                            maxLength={32}
+                            value={config[1].display_name}
+                            onValue={(v) => {
+                                on_config(util.get_updated_union(config, {display_name: v}));
+                            }}
+                        />
+                    </FormRow>,
+                ];
+            },
+        },
+    };
+}
---
+++ software/web/src/modules/meters_sma/translation_de.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource preact */
+import { h } from "preact";
+let x = {
+    "meters_sma": {
+        "content": {
+            "meter_class": "SMA Energy Meter 2.0",
+
+            "config_display_name": "Anzeigename"
+        }
+    }
+}
