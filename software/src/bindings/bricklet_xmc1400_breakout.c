2e2e3023eee0123ae05542356ecefa754b235da7
Matthias Bolte <matthias@tinkerforge.com>
2024-02-20 15:56:26 +0100

    Update bindings

--- software/src/bindings/bricklet_xmc1400_breakout.c
+++ software/src/bindings/bricklet_xmc1400_breakout.c
@@ -1,7 +1,7 @@
 /* ***********************************************************
- * This file was automatically generated on 2023-01-25.      *
+ * This file was automatically generated on 2024-02-20.      *
  *                                                           *
- * C/C++ for Microcontrollers Bindings Version 2.0.3         *
+ * C/C++ for Microcontrollers Bindings Version 2.0.4         *
  *                                                           *
  * If you have a bugfix for this file and want to commit it, *
  * please fix the bug in the generator. You can find a link  *
