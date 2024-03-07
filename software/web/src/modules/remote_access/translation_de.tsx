f5dd3a87203bf481272bb12f550b072196f6c6c7
Frederic Henrichs <frederic@tinkerforge.com>
2024-03-07 07:49:31 +0100

    add remote access frontend and backend modules.

---
+++ software/web/src/modules/remote_access/translation_de.tsx
@@ -0,0 +1,22 @@
+
+/** @jsxImportSource preact */
+import { h } from "preact";
+let x = {
+    "remote_access": {
+        "navbar": {
+            "remote_access": "Fernzugriff"
+        },
+        "content": {
+            "remote_access": "Fernzugriff",
+            "email": "Email",
+            "password": "Passwort",
+            "relay_host": "Relay Host",
+            "relay_host_muted": "Hostname des Relay Servers",
+            "enable": "Fernzugriff aktivieren"
+        },
+        "script": {
+            "save_failed": "Speichern der Fernzugriffs-Einstellungen fehlgeschlagen.",
+            "reboot_content_changed": "real-time clock settings"
+        }
+    }
+}
