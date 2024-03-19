Tinkerforge/esp32-firmware

commit 63b28d0a16895852ae5f2ced617eb007a120f928

provision/stage_2,3_warp2: Only test WARP3 features if this is a WARP3.

provisioning/provision_stage_3_warp2.py
@@ -741,11 +741,12 @@ class Stage3:
             fatal_error('Wallbox not in IEC state C')
 
     # requires power_on
-    def test_wallbox(self):
+    def test_wallbox(self, has_phase_switch):
         assert self.has_evse_error_function != None
         assert self.get_iec_state_function != None
         assert self.reset_dc_fault_function != None
-        assert self.switch_phases_function != None
+        if has_phase_switch:
+            assert self.switch_phases_function != None
 
         if self.read_meter_qr_code() != '01':
             fatal_error('Meter in wrong step')
@@ -877,61 +878,62 @@ class Stage3:
 
         self.connect_warp_power(['L1', 'L2', 'L3'])
 
-        time.sleep(RELAY_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
+        if has_phase_switch:
+            time.sleep(RELAY_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
 
-        voltages = self.read_voltage_monitors()
+            voltages = self.read_voltage_monitors()
 
-        print('Reading voltages as {0}'.format(voltages))
+            print('Reading voltages as {0}'.format(voltages))
 
-        if voltages[0] < VOLTAGE_ON_THRESHOLD:
-            fatal_error('Missing voltage on L1')
+            if voltages[0] < VOLTAGE_ON_THRESHOLD:
+                fatal_error('Missing voltage on L1')
 
-        if voltages[1] < VOLTAGE_OFF_THRESHOLD:
-            fatal_error('Missing voltage on L2')
+            if voltages[1] < VOLTAGE_OFF_THRESHOLD:
+                fatal_error('Missing voltage on L2')
 
-        if voltages[2] < VOLTAGE_ON_THRESHOLD:
-            fatal_error('Missing voltage on L3')
+            if voltages[2] < VOLTAGE_ON_THRESHOLD:
+                fatal_error('Missing voltage on L3')
 
-        print('Testing phase switch')
+            print('Testing phase switch')
 
-        self.switch_phases_function(1)
+            self.switch_phases_function(1)
 
-        time.sleep(PHASE_SWITCH_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
+            time.sleep(PHASE_SWITCH_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
 
-        voltages = self.read_voltage_monitors()
+            voltages = self.read_voltage_monitors()
 
-        print('Reading voltages as {0}'.format(voltages))
+            print('Reading voltages as {0}'.format(voltages))
 
-        if voltages[0] < VOLTAGE_ON_THRESHOLD:
-            fatal_error('Missing voltage on L1')
+            if voltages[0] < VOLTAGE_ON_THRESHOLD:
+                fatal_error('Missing voltage on L1')
 
-        if voltages[1] > VOLTAGE_OFF_THRESHOLD:
-            fatal_error('Unexpected voltage on L2')
+            if voltages[1] > VOLTAGE_OFF_THRESHOLD:
+                fatal_error('Unexpected voltage on L2')
 
-        if voltages[2] > VOLTAGE_ON_THRESHOLD:
-            fatal_error('Unexpected voltage on L3')
+            if voltages[2] > VOLTAGE_ON_THRESHOLD:
+                fatal_error('Unexpected voltage on L3')
 
 
-        self.switch_phases_function(3)
+            self.switch_phases_function(3)
 
-        time.sleep(PHASE_SWITCH_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
+            time.sleep(PHASE_SWITCH_SETTLE_DURATION + VOLTAGE_SETTLE_DURATION)
 
-        voltages = self.read_voltage_monitors()
+            voltages = self.read_voltage_monitors()
 
-        print('Reading voltages as {0}'.format(voltages))
+            print('Reading voltages as {0}'.format(voltages))
 
-        if voltages[0] < VOLTAGE_ON_THRESHOLD:
-            fatal_error('Missing voltage on L1')
+            if voltages[0] < VOLTAGE_ON_THRESHOLD:
+                fatal_error('Missing voltage on L1')
 
-        if voltages[1] < VOLTAGE_OFF_THRESHOLD:
-            fatal_error('Missing voltage on L2')
+            if voltages[1] < VOLTAGE_OFF_THRESHOLD:
+                fatal_error('Missing voltage on L2')
 
-        if voltages[2] < VOLTAGE_ON_THRESHOLD:
-            fatal_error('Missing voltage on L3')
+            if voltages[2] < VOLTAGE_ON_THRESHOLD:
+                fatal_error('Missing voltage on L3')
 
-        self.connect_voltage_monitors(False)
+            self.connect_voltage_monitors(False)
 
-        time.sleep(RELAY_SETTLE_DURATION)
+            time.sleep(RELAY_SETTLE_DURATION)
 
         # step 01: test PE disconnect
         print('Disconnecting PE')

provisioning/provision_stage_2_warp2.py
@@ -40,6 +40,7 @@ generation = None
 
 def run_bricklet_tests(ipcon, result, qr_variant, qr_power, qr_stand, qr_stand_wiring, ssid, stage3):
     global evse
+    global generation
     enumerations = enumerate_devices(ipcon)
 
     master = next((e for e in enumerations if e.device_identifier == 13), None)
@@ -62,30 +63,31 @@ def run_bricklet_tests(ipcon, result, qr_variant, qr_power, qr_stand, qr_stand_w
     stage3.test_front_panel_button(qr_stand == '0' or qr_stand_wiring == '0')
     result["front_panel_button_tested"] = True
 
-    if not (qr_stand == '0' or qr_stand_wiring == '0'):
-        fatal_error("WARP 3 RGB LED test not yet implemented for stand. " + blink("Complain to Erik!"))
+    if generation >= 3:
+        if not (qr_stand == '0' or qr_stand_wiring == '0'):
+            fatal_error("WARP 3 RGB LED test not yet implemented for stand. " + blink("Complain to Erik!"))
 
-    evse.set_indicator_led(255, 1000, 0, 255, 255)
-    time.sleep(0.5)
-    if not stage3.is_front_panel_led_red():
-        fatal_error("Front LED not red!")
+        evse.set_indicator_led(255, 1000, 0, 255, 255)
+        time.sleep(0.5)
+        if not stage3.is_front_panel_led_red():
+            fatal_error("Front LED not red!")
 
-    evse.set_indicator_led(255, 1000, 120, 255, 255)
-    time.sleep(0.5)
-    if not stage3.is_front_panel_led_green():
-        fatal_error("Front LED not green!")
+        evse.set_indicator_led(255, 1000, 120, 255, 255)
+        time.sleep(0.5)
+        if not stage3.is_front_panel_led_green():
+            fatal_error("Front LED not green!")
 
-    evse.set_indicator_led(255, 1000, 240, 255, 255)
-    time.sleep(0.5)
-    if not stage3.is_front_panel_led_blue():
-        fatal_error("Front LED not blue!")
+        evse.set_indicator_led(255, 1000, 240, 255, 255)
+        time.sleep(0.5)
+        if not stage3.is_front_panel_led_blue():
+            fatal_error("Front LED not blue!")
 
-    evse.set_indicator_led(255, 1000, 0, 0, 255)
-    time.sleep(0.5)
-    if not stage3.is_front_panel_led_white():
-        fatal_error("Front LED not white!")
+        evse.set_indicator_led(255, 1000, 0, 0, 255)
+        time.sleep(0.5)
+        if not stage3.is_front_panel_led_white():
+            fatal_error("Front LED not white!")
 
-    result["front_panel_led_tested"] = True
+        result["front_panel_led_tested"] = True
 
     seen_tags = []
     if is_smart or is_pro:
@@ -684,7 +686,7 @@ def main(stage3):
             browser.get("http://{}/#evse".format(ssid))
 
         print("Performing the electrical tests")
-        stage3.test_wallbox()
+        stage3.test_wallbox(has_phase_switch=generation >= 3)
     finally:
         if browser is not None:
             browser.quit()

commit 11f5648a3e05c52aea4bacfb78375695770387fc

provisioning/warp2: Fix Metrel R-iso test for L2
Broken by commit 63b28d0a16895852ae5f2ced617eb007a120f928.

provisioning/provision_stage_3_warp2.py
@@ -933,9 +933,8 @@ class Stage3:
             if voltages[2] < VOLTAGE_ON_THRESHOLD:
                 fatal_error('Missing voltage on L3')
 
-            self.connect_voltage_monitors(False)
-
-            time.sleep(RELAY_SETTLE_DURATION)
+        self.connect_voltage_monitors(False)
+        time.sleep(RELAY_SETTLE_DURATION)
 
         # step 01: test PE disconnect
         print('Disconnecting PE')