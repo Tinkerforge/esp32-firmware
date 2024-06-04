0f53291384a1993fa358a14932b93ed42048aa7c
Erik Fleckstein <erik@tinkerforge.com>
2024-06-04 10:27:40 +0200

    charge_manager: Add stages 2-5 and 8. 3-8 untested yet.

--- software/src/modules/charge_manager/charge_manager_private.h
+++ software/src/modules/charge_manager/charge_manager_private.h
@@ -12,6 +12,7 @@
 struct CurrentAllocatorConfig {
     uint32_t minimum_current_3p;
     uint32_t minimum_current_1p;
+    uint32_t enable_current;
     std::unique_ptr<char[]> distribution_log;
     size_t distribution_log_len;
     size_t charger_count;
