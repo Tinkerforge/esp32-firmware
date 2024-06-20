602951bb1daa77e383d7c54fd98e66a63e03d0b3
Erik Fleckstein <erik@tinkerforge.com>
2024-06-20 12:10:49 +0200

    charge_manager: Add debug API to set limits.

--- software/src/modules/charge_manager/charge_manager.cpp
+++ software/src/modules/charge_manager/charge_manager.cpp
@@ -181,6 +181,13 @@ void ChargeManager::pre_setup()
         {"disconnect", Config::Bool(false)},
     });
 
+    limits_cfg = Config::Array({
+        Config::Int32(0),
+        Config::Int32(0),
+        Config::Int32(0),
+        Config::Int32(0)
+    }, new Config{Config::Int32(0)}, 4, 4, Config::type_id<Config::ConfInt>());
+
 #if MODULE_AUTOMATION_AVAILABLE() && !MODULE_ENERGY_MANAGER_AVAILABLE()
     automation.register_trigger(
         AutomationTriggerID::ChargeManagerWd,
@@ -481,6 +488,13 @@ void ChargeManager::register_urls()
         logger.printfln("Available phases: %u", phases);
     }, false);
 
+    api.addState("charge_manager/debug_limits", &limits_cfg);
+    api.addCommand("charge_manager/debug_limits_update", &limits_cfg, {}, [this](){
+        for(size_t i = 0; i < 4; ++i) {
+            this->limits.raw[i] = limits_cfg.get(i)->asInt();
+            this->limits.filtered[i] = limits_cfg.get(i)->asInt();
+        }
+    }, true);
 }
 
 void ChargeManager::update_charger_state_config(uint8_t idx) {
