2406ef5306201d4017b1639995f6bbca3476b93c
Matthias Bolte <matthias@tinkerforge.com>
2024-07-04 13:04:16 +0200

    Add missing copyright headers

--- software/src/modules/meters_rs485_bricklet/meter_defs.h
+++ software/src/modules/meters_rs485_bricklet/meter_defs.h
@@ -1,10 +1,28 @@
+/* esp32-firmware
+ * Copyright (C) 2023 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #pragma once
 
 #include <stdint.h>
 #include <stddef.h>
 
 #include "modules/meters/meter_defs.h"
-
 #include "config.h"
 
 #define PHASE_ACTIVE_CURRENT_THRES 0.3f // ampere
