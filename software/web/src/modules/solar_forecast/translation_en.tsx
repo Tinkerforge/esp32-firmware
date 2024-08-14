0454cc7dde10b3a67d7c0f7650cf4fa409978344
Olaf Lüke <olaf@tinkerforge.com>
2024-08-14 23:13:34 +0200

    web/solar_forecast: Add more translations

--- software/web/src/modules/solar_forecast/translation_en.tsx
+++ software/web/src/modules/solar_forecast/translation_en.tsx
@@ -9,6 +9,11 @@ let x = {
         "content": {
             "solar_forecast": "Solar Forecast",
             "planes": "PV planes",
+            "plane": "PV plane",
+            "no_data": "No data available!",
+            "loading": "Loading data...",
+            "plane_config_name": "Name",
+            "plane_config_name_muted": "Display name of the PV plane",
             "plane_config_latitude": "Latitude",
             "plane_config_latitude_muted": "-90° south to 90° north",
             "plane_config_longitude": "Longitude",
@@ -20,15 +25,22 @@ let x = {
             "plane_config_kwp": "kWp",
             "plane_config_kwp_muted": "Peak power of the PV plane",
             "add_plane_config_title": "New PV plane",
-            "add_plane_config_message": "Add new PV plane",
+            "add_plane_config_prefix": "Add new PV plane (",
+            "add_plane_config_infix": " of ",
+            "add_plane_config_postfix": ")",
+            "add_plane_config_done": "All PV planes added (6 of 6)",
             "edit_plane_config_title": "Edit PV plane",
             "solar_forecast_desc": "Enable solar forecast",
             "solar_forecast_muted": "",
+            "table_name": "Name",
             "table_azimuth": "Azimuth",
             "table_declination": "Declination",
             "table_kwp": "kWp",
             "table_latitude": "Latitude",
-            "table_longitude": "Longitude"
+            "table_longitude": "Longitude",
+            "time": "Time",
+            "solar_forecast_chart_heading": "Solar forecast chart"
+
         },
         "script": {
             "save_failed": "Failed to save the solar forecast settings",
