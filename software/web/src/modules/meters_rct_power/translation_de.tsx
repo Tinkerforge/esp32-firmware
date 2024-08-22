88adf1949ae381fc65729656a655bb3152973806
Matthias Bolte <matthias@tinkerforge.com>
2024-08-22 15:59:50 +0200

    meters-rct-power: Add skeleton

---
+++ software/web/src/modules/meters_rct_power/translation_de.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource preact */
+import { h } from "preact";
+let x = {
+    "meters_rct_power": {
+        "content": {
+            "meter_class": "RCT Power",
+
+            "display_name": "Anzeigename",
+            "host": "Host",
+            "host_invalid": "Host ist ungültig",
+            "port": "Port",
+            "port_muted": "typischerweise 8899"
+        }
+    }
+}
