Tinkerforge/esp32-firmware

commit bc2e116224034ea91ee5420d8aa83d215f8bbd23

automation_mqtt_trigger: rename topic to topic_filter.

software/src/modules/mqtt/mqtt.cpp
@@ -80,13 +80,13 @@ void Mqtt::pre_setup()
     automation.register_trigger(
         AutomationTriggerID::MQTT,
         Config::Object({
-            {"topic", Config::Str("", 0, 32)},
+            {"topic_filter", Config::Str("", 0, 32)},
             {"payload", Config::Str("", 0, 32)},
             {"retain", Config::Bool(false)},
             {"use_prefix", Config::Bool(false)}
         }),
         [this](const Config *cfg) {
-            const CoolString &topic = cfg->get("topic")->asString();
+            const CoolString &topic = cfg->get("topic_filter")->asString();
             if (topic.startsWith(this->config.get("global_topic_prefix")->asString())) {
                 return String("Mqtt-topic must not contain the global prefix.");
             }
@@ -593,12 +593,12 @@ void Mqtt::register_urls()
         for (auto &conf: trigger_config) {
             bool already_subscribed = false;
             for (auto &new_topic: subscribed_topics) {
-                if (conf.second->get("topic")->asString() == new_topic)
+                if (conf.second->get("topic_filter")->asString() == new_topic)
                     already_subscribed = true;
             }
             const size_t idx = conf.first;
             if (!already_subscribed) {
-                String topic = conf.second->get("topic")->asString();
+                String topic = conf.second->get("topic_filter")->asString();
                 if (conf.second->get("use_prefix")->asBool()) {
                     topic = config.get("global_topic_prefix")->asString() + "/automation_trigger/" + topic;
                 }
@@ -651,7 +651,7 @@ bool Mqtt::action_triggered(Config *config, void *data) {
     MqttMessage *msg = (MqttMessage *)data;
     const CoolString &payload = cfg->get("payload")->asString();
 
-    CoolString topic = cfg->get("topic")->asString();
+    CoolString topic = cfg->get("topic_filter")->asString();
     if (cfg->get("use_prefix")->asBool()) {
         topic = this->config.get("global_topic_prefix")->asString();
         topic += "/automation_trigger/";

software/web/src/modules/mqtt/plugin_automation_trigger.tsx
@@ -31,7 +31,7 @@ import * as util from "../../ts/util";
 export type MqttAutomationTrigger = [
     AutomationTriggerID.MQTT,
     {
-        topic: string;
+        topic_filter: string;
         payload: string;
         retain: boolean;
         use_prefix: boolean;
@@ -40,7 +40,7 @@ export type MqttAutomationTrigger = [
 
 function get_mqtt_table_children(trigger: MqttAutomationTrigger) {
     const mqtt_config = API.get("mqtt/config");
-    const topic = trigger[1].use_prefix ? mqtt_config.global_topic_prefix + "/automation_trigger/" + trigger[1].topic : trigger[1].topic;
+    const topic = trigger[1].use_prefix ? mqtt_config.global_topic_prefix + "/automation_trigger/" + trigger[1].topic_filter : trigger[1].topic_filter;
 
     return __("mqtt.automation.automation_trigger_text")(topic, trigger[1].payload, trigger[1].retain);
 }
@@ -61,7 +61,7 @@ function get_mqtt_edit_children(trigger: MqttAutomationTrigger, on_trigger: (tri
         <FormRow label={__("mqtt.automation.topic")}>
             <InputText
                 required
-                value={trigger[1].topic}
+                value={trigger[1].topic_filter}
                 class={isInvalid ? "is-invalid" : undefined}
                 maxLength={64}
                 onValue={(v) => {
@@ -71,14 +71,14 @@ function get_mqtt_edit_children(trigger: MqttAutomationTrigger, on_trigger: (tri
                         isInvalidSetter(false);
                     }
 
-                    on_trigger(util.get_updated_union(trigger, {topic: v}));
+                    on_trigger(util.get_updated_union(trigger, {topic_filter: v}));
                 }}
                 invalidFeedback={__("mqtt.automation.use_topic_prefix_invalid")} />
         </FormRow>
         <FormRow label={__("mqtt.automation.full_topic")} hidden={!trigger[1].use_prefix}>
             <InputText
                 class="mt-2"
-                value={mqtt_config.global_topic_prefix + "/automation_trigger/" + trigger[1].topic} />
+                value={mqtt_config.global_topic_prefix + "/automation_trigger/" + trigger[1].topic_filter} />
         </FormRow>
         <FormRow label={__("mqtt.automation.payload")}>
             <InputText
@@ -103,7 +103,7 @@ function new_mqtt_config(): AutomationTrigger {
     return [
         AutomationTriggerID.MQTT,
         {
-            topic: "",
+            topic_filter: "",
             payload: "",
             retain: false,
             use_prefix: false,

commit 5158ae954dc68069d1c3b1e6fff47b1134439172

mqtt: Fix crash when using automation rule with prefix
Introduced by incomplete rename in commit bc2e116.

software/src/modules/mqtt/mqtt.cpp
@@ -741,20 +741,21 @@ void Mqtt::pre_reboot()
 bool Mqtt::has_triggered(const Config *conf, void *data)
 {
     const Config *cfg = static_cast<const Config *>(conf->get());
-    MqttMessage *msg = (MqttMessage *)data;
-    const CoolString &payload = cfg->get("payload")->asString();
-    CoolString topic = cfg->get("topic_filter")->asString();
+    const MqttMessage *msg = (const MqttMessage *)data;
+    const String &payload = cfg->get("payload")->asString();
 
+    String topic(static_cast<const char *>(nullptr));
     if (cfg->get("use_prefix")->asBool()) {
+        topic.reserve(64);
         topic = this->config.get("global_topic_prefix")->asString();
         topic += "/automation_trigger/";
-        topic += cfg->get("topic")->asString();
     }
+    topic += cfg->get("topic_filter")->asString();
 
     switch (conf->getTag<AutomationTriggerID>())
     {
     case AutomationTriggerID::MQTT:
-        if (msg->topic == topic && (payload == msg->payload || payload.length() == 0))
+        if (msg->topic == topic && (payload.length() == 0 || msg->payload == payload))
             return true;
         break;
 