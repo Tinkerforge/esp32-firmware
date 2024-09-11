8e59b169f7a9f8e22eb336c901b9cc96d587bc86
Mattias Schäffersmann <mattias@tinkerforge.com>
2024-09-11 16:49:10 +0200

    energy_manager, em_energy_analysis: Move API declarations

---
+++ software/web/src/modules/em_energy_analysis/api.ts
@@ -0,0 +1,38 @@
+//APIPath:energy_manager/
+
+export interface history_wallbox_5min_changed {
+    uid: number;
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+    flags: number;
+    power: number;
+}
+
+export interface history_wallbox_daily_changed {
+    uid: number;
+    year: number;
+    month: number;
+    day: number;
+    energy: number;
+}
+
+export interface history_energy_manager_5min_changed {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+    flags: number;
+    power: number[];
+}
+
+export interface history_energy_manager_daily_changed {
+    year: number;
+    month: number;
+    day: number;
+    energy_import: number[];
+    energy_export: number[];
+}
