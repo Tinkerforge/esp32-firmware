cd1d1555fc41edb7eb59401427d0699bfadbc64c
Olaf Lüke <olaf@tinkerforge.com>
2024-09-30 09:12:04 +0200

    web/heating: Add chart for heating depending on day ahead prices

--- software/web/src/modules/heating/main.tsx
+++ software/web/src/modules/heating/main.tsx
@@ -19,7 +19,7 @@
 
 import * as util from "../../ts/util";
 import * as API from "../../ts/api";
-import { h, Fragment } from "preact";
+import { h, Fragment, createRef } from "preact";
 import { __ } from "../../ts/translation";
 import { METERS_SLOTS } from "../../build";
 import { Switch } from "../../ts/components/switch";
@@ -37,6 +37,8 @@ import { Collapse } from "react-bootstrap";
 import { InputSelect } from "../../ts/components/input_select";
 import { MeterValueID    } from "../meters/meter_value_id";
 import { get_noninternal_meter_slots } from "../power_manager/main";
+import { UplotLoader } from "../../ts/components/uplot_loader";
+import { UplotData, UplotWrapper, UplotPath } from "../../ts/components/uplot_wrapper_2nd";
 
 export function HeatingNavbar() {
     return <NavbarItem name="heating" title={__("heating.navbar.heating")} symbol={<Thermometer />} hidden={false} />;
@@ -44,7 +46,18 @@ export function HeatingNavbar() {
 
 type HeatingConfig = API.getType["heating/config"];
 
-export class Heating extends ConfigComponent<'heating/config'> {
+interface HeatingState {
+    dap_config: API.getType["day_ahead_prices/config"];
+    dap_state:  API.getType["day_ahead_prices/state"];
+    dap_prices: API.getType["day_ahead_prices/prices"];
+}
+
+export class Heating extends ConfigComponent<'heating/config', {}, HeatingState> {
+    uplot_loader_ref        = createRef();
+    uplot_wrapper_ref       = createRef();
+    uplot_legend_div_ref    = createRef();
+    uplot_wrapper_flags_ref = createRef();
+
     summer_start_day:   number;
     summer_start_month: number;
     summer_end_day:     number;
@@ -73,6 +86,116 @@ export class Heating extends ConfigComponent<'heating/config'> {
     constructor() {
         super('heating/config',
               __("heating.script.save_failed"));
+
+        util.addApiEventListener("day_ahead_prices/config", () => {
+            this.setState({dap_config: API.get("day_ahead_prices/config")});
+        });
+
+        util.addApiEventListener("day_ahead_prices/state", () => {
+            this.setState({dap_state: API.get("day_ahead_prices/state")});
+        });
+
+        util.addApiEventListener("day_ahead_prices/prices", () => {
+            this.setState({dap_prices: API.get("day_ahead_prices/prices")});
+            // Update chart every time new price data comes in
+            this.update_uplot();
+        });
+    }
+
+    get_price_timeframe() {
+        let time = new Date();
+        let s = ""
+        if(this.state.dap_prices.resolution == 0) {
+            time.setMilliseconds(Math.floor(time.getMilliseconds() / 1000) * 1000);
+            time.setSeconds(Math.floor(time.getSeconds() / 60) * 60);
+            time.setMinutes(Math.floor(time.getMinutes() / 15) * 15);
+            s += time.toLocaleTimeString() + '-';
+            time.setMinutes(time.getMinutes() + 15);
+            s += time.toLocaleTimeString()
+        } else {
+            time.setMilliseconds(Math.floor(time.getMilliseconds() / 1000) * 1000);
+            time.setSeconds(Math.floor(time.getSeconds() / 60) * 60);
+            time.setMinutes(Math.floor(time.getMinutes() / 60) * 60);
+            s += time.toLocaleTimeString() + '-';
+            time.setMinutes(time.getMinutes() + 60);
+            s += time.toLocaleTimeString()
+        }
+
+        return s
+    }
+
+    update_uplot() {
+        if (this.uplot_wrapper_ref.current == null) {
+            return;
+        }
+
+        let data: UplotData;
+
+        // If we have not got any prices yet, use empty data
+        if (this.state.dap_prices.prices.length == 0) {
+            data = {
+                keys: [],
+                names: [],
+                values: [],
+                stacked: [],
+                paths: [],
+            }
+        // Else fill with time and the three different prices we want to show
+        } else {
+            data = {
+                keys: ['time', 'price'],
+                names: [__("day_ahead_prices.content.time"), __("day_ahead_prices.content.electricity_price")],
+                values: [[], [], [], []],
+                stacked: [null, true],
+                paths: [null, UplotPath.Step],
+                // Only enable the electricity price by default.
+                // The chart with only electricity price is the most useful in most cases.
+                default_visibilty: [null, true],
+                lines_vertical: []
+            }
+            let resolution_multiplier = this.state.dap_prices.resolution == 0 ? 15 : 60
+            const grid_costs_and_taxes_and_supplier_markup = this.state.dap_config.grid_costs_and_taxes/1000.0 + this.state.dap_config.supplier_markup/1000.0;
+            for (let i = 0; i < this.state.dap_prices.prices.length; i++) {
+                data.values[0].push(this.state.dap_prices.first_date*60 + i*60*resolution_multiplier);
+                data.values[1].push(this.state.dap_prices.prices[i]/1000.0 + grid_costs_and_taxes_and_supplier_markup);
+            }
+
+            const num_per_day = 24*60/resolution_multiplier;
+            console.log(num_per_day)
+
+            if (this.state.dap_prices.prices.length >= num_per_day*2) {
+                let avg_price_day1 = this.state.dap_prices.prices.slice(0, num_per_day).reduce((a, b) => a + b, 0) / num_per_day;
+                let avg_price_day2 = this.state.dap_prices.prices.slice(num_per_day).reduce((a, b) => a + b, 0) / num_per_day;
+
+                for (let i = 0; i < num_per_day; i++) {
+                    console.log(this.state.dap_prices.prices[i], this.state.summer_dpc_extended_threshold, avg_price_day1);
+                    if (this.state.dap_prices.prices[i] < avg_price_day1*this.state.summer_dpc_extended_threshold/100) {
+                        console.log("green");
+                        data.lines_vertical.push({'index': i, 'text': '', 'color': [0, 255, 0, 0.5]});
+                    } else if(this.state.dap_prices.prices[i] > avg_price_day1*this.state.summer_dpc_blocking_threshold/100) {
+                        console.log("red");
+                        data.lines_vertical.push({'index': i, 'text': '', 'color': [255, 0, 0, 0.5]});
+                    }
+                }
+                for (let i = num_per_day; i < num_per_day*2; i++) {
+                    if (this.state.dap_prices.prices[i] < avg_price_day2*this.state.summer_dpc_extended_threshold/100) {
+                        data.lines_vertical.push({'index': i, 'text': '', 'color': [0, 255, 0, 0.5]});
+                    } else if(this.state.dap_prices.prices[i] > avg_price_day2*this.state.summer_dpc_blocking_threshold/100) {
+                        data.lines_vertical.push({'index': i, 'text': '', 'color': [255, 0, 0, 0.5]});
+                    }
+                }
+            }
+
+            // Add vertical line at current time
+            const resolution_divisor = this.state.dap_prices.resolution == 0 ? 15 : 60;
+            const diff = Math.floor(Date.now() / 60000) - this.state.dap_prices.first_date;
+            const index = Math.floor(diff / resolution_divisor);
+            data.lines_vertical.push({'index': index, 'text': __("day_ahead_prices.content.now"), 'color': [64, 64, 64, 0.2]});
+        }
+
+        // Show loader or data depending on the availability of data
+        this.uplot_loader_ref.current.set_data(data && data.keys.length > 1);
+        this.uplot_wrapper_ref.current.set_data(data);
     }
 
     get_date_from_minutes(minutes: number) {
@@ -271,7 +394,7 @@ export class Heating extends ConfigComponent<'heating/config'> {
                     </FormRow>
 
                     <FormSeparator heading={__("heating.content.summer_mode")}/>
-                    <FormRow label="Sommer Start" label_muted="">
+                    <FormRow label="Sommer Start" label_muted="Zeitraum anhand des Wintermodus berechnet">
                         <div class="row no-gutters">
                             <div class="col-md-6">
                                 <div class="input-group">
@@ -295,7 +418,7 @@ export class Heating extends ConfigComponent<'heating/config'> {
                             </div>
                         </div>
                     </FormRow>
-                    <FormRow label="Sommer Ende" label_muted="">
+                    <FormRow label="Sommer Ende" label_muted="Zeitraum anhand des Wintermodus berechnet">
                         <div class="row no-gutters">
                             <div class="col-md-6">
                                 <div class="input-group">
@@ -377,7 +500,7 @@ export class Heating extends ConfigComponent<'heating/config'> {
                                     </div>
                                 </div>
                             </FormRow>
-                            <FormRow label={__("heating.content.pv_yield_forecast")} label_muted="Optimiert die Blockierzeit anhand des erwarteten PV-Ertrag" help={__("heating.content.pv_yield_forecast_help")}>
+                            <FormRow label={__("heating.content.pv_yield_forecast")} label_muted="Setzt die Blockierzeit anhand des erwarteten PV-Ertrag außer Kraft" help={__("heating.content.pv_yield_forecast_help")}>
                                 <SwitchableInputNumber
                                     switch_label_active="Aktiv"
                                     switch_label_inactive="Inaktiv"
@@ -415,7 +538,7 @@ export class Heating extends ConfigComponent<'heating/config'> {
                             checked={state.summer_dpc_extended_active}
                             onClick={this.toggle('summer_dpc_extended_active')}
                             value={state.summer_dpc_extended_threshold}
-                            onValue={this.set("summer_dpc_extended_threshold")}
+                            onValue={(v) => {this.setState({summer_dpc_extended_threshold: v}, function() {this.update_uplot()})}}
                             min={0}
                             max={100}
                             switch_label_min_width="100px"
@@ -429,12 +552,50 @@ export class Heating extends ConfigComponent<'heating/config'> {
                             checked={state.summer_dpc_blocking_active}
                             onClick={this.toggle('summer_dpc_blocking_active')}
                             value={state.summer_dpc_blocking_threshold}
-                            onValue={this.set("summer_dpc_blocking_threshold")}
+                            onValue={(v) => {this.setState({summer_dpc_blocking_threshold: v}, function() {this.update_uplot()})}}
                             min={100}
                             max={1000}
                             switch_label_min_width="100px"
                         />
                     </FormRow>
+                    <FormRow label="Heizplan Sommer" label_muted="Heizplan anhand der dynamischen Preise. Rot = blockierender Betrieb, Grün = Einschaltempfehlung">
+                    <div class="card pl-1 pb-1">
+                    <div style="position: relative;"> {/* this plain div is neccessary to make the size calculation stable in safari. without this div the height continues to grow */}
+                        <UplotLoader
+                            ref={this.uplot_loader_ref}
+                            show={true}
+                            marker_class={'h4'}
+                            no_data={__("day_ahead_prices.content.no_data")}
+                            loading={__("day_ahead_prices.content.loading")}>
+                            <UplotWrapper
+                                ref={this.uplot_wrapper_ref}
+                                class="heating--chart pb-3"
+                                sub_page="heating"
+                                color_cache_group="heating.default"
+                                show={true}
+                                on_mount={() => this.update_uplot()}
+                                legend_time_label={__("day_ahead_prices.content.time")}
+                                legend_time_with_minutes={true}
+                                legend_div_ref={this.uplot_legend_div_ref}
+                                aspect_ratio={3}
+                                x_height={50}
+                                x_format={{hour: '2-digit', minute: '2-digit'}}
+                                x_padding_factor={0}
+                                x_include_date={true}
+                                y_min={0}
+                                y_max={5}
+                                y_unit={"ct/kWh"}
+                                y_label={__("day_ahead_prices.content.price_ct_per_kwh")}
+                                y_digits={3}
+                                y_skip_upper={true}
+                                y_sync_ref={this.uplot_wrapper_flags_ref}
+                                only_show_visible={true}
+                                padding={[15, 5, null, null]}
+                            />
+                        </UplotLoader>
+                    </div>
+                </div>
+                </FormRow>
 
                     <FormSeparator heading="§14 EnWG"/>
                     <FormRow label="§14 EnWG">
