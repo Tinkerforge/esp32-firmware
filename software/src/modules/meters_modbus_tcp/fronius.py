681182853333c072c72c5a36a09b09762093b2f2
Matthias Bolte <matthias@tinkerforge.com>
2024-10-16 10:34:42 +0200

    meters-modbus-tcp: Handle missing energy values for Fronius GEN24 Plus

--- software/src/modules/meters_modbus_tcp/fronius.py
+++ software/src/modules/meters_modbus_tcp/fronius.py
@@ -9,85 +9,85 @@ specs = [
                 'name': 'DCA_SF: Current Scale Factor',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40266,
-                'value_type': 'S16',
+                'value_type': 'S16',  # SunSpec: sunssf
             },
             {
                 'name': 'DCV_SF: Voltage Scale Factor',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40267,
-                'value_type': 'S16',
+                'value_type': 'S16',  # SunSpec: sunssf
             },
             {
                 'name': 'DCW_SF: Power Scale Factor',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40268,
-                'value_type': 'S16',
+                'value_type': 'S16',  # SunSpec: sunssf
             },
             {
                 'name': 'DCWH_SF: Energy Scale Factor',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40269,
-                'value_type': 'S16',
+                'value_type': 'S16',  # SunSpec: sunssf
             },
             {
                 'name': 'module/3/DCA: DC Charge Current [A]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40323,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/3/DCV: DC Charge Voltage [V]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40324,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/3/DCW: DC Charge Power [W]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40325,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/3/DCWH: Lifetime Charge Energy [Wh]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40326,
-                'value_type': 'U32BE',
+                'value_type': 'U32BE',  # SunSpec: acc32
             },
             {
                 'name': 'module/4/DCA: DC Discharge Current [A]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40343,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/4/DCV: DC Discharge Voltage [V]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40344,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/4/DCW: DC Discharge Power [W]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40345,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'module/4/DCWH: Lifetime Discharge Energy [Wh]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40346,
-                'value_type': 'U32BE',
+                'value_type': 'U32BE',  # SunSpec: acc32
             },
             {
                 'name': 'ChaState: Currently available energy as a percent of the capacity rating [%]',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40362,
-                'value_type': 'U16',
+                'value_type': 'U16',  # SunSpec: uint16
             },
             {
                 'name': 'ChaState_SF: Scale factor for available energy percent',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40376,
-                'value_type': 'S16',
+                'value_type': 'S16',  # SunSpec: sunssf
             },
             {
                 'name': 'Battery Current [A]',
