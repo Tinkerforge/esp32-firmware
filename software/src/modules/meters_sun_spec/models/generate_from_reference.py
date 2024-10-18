3a368594b9df62194c34c25a7f5e29563eee3d7c
Erik Fleckstein <erik@tinkerforge.com>
2024-10-18 10:38:03 +0200

    meters_sun_spec: Disable address-of-packed-member warning for now.
    Getting a pointer to an unaligned uint32_t is not a good idea,
    however this works on the Xtensa and the warning spams a lot.

--- software/src/modules/meters_sun_spec/models/generate_from_reference.py
+++ software/src/modules/meters_sun_spec/models/generate_from_reference.py
@@ -486,6 +486,8 @@ r"""// WARNING: This file is generated.
 
 #if defined(__GNUC__)
     #pragma GCC diagnostic ignored "-Wpedantic"
+    #pragma GCC diagnostic ignored "-Waddress-of-packed-member"
+    #warning "-Waddress-of-packed-member disabled! We should fix this soon."
 #endif
 
 static const float scale_factors[21] = {
