1f7b3386982e19d0fac593d4a375704afc5e3b3b
Matthias Bolte <matthias@tinkerforge.com>
2024-10-23 17:16:50 +0200

    meters-modbus-tcp: Fix battery power mapping for Alpha ESS

--- software/src/modules/meters_modbus_tcp/alpha_ess.py
+++ software/src/modules/meters_modbus_tcp/alpha_ess.py
@@ -322,10 +322,10 @@ specs = [
             },
             {
                 'name': 'Battery power [W]',
-                'value_id': 'PowerDC',
+                'value_id': 'PowerDCChaDisDiff',
                 'start_address': 0x0126,
                 'value_type': 'S16',
-                'scale_factor': -1.0,
+                'scale_factor': -1.0,  # FIXME: sign?
             },
         ],
     },
