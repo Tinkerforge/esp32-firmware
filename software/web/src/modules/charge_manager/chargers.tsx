758e89407155511544b4db4b0299ac8779dcf8d6
Mattias Schäffersmann <mattias@tinkerforge.com>
2024-11-25 20:07:44 +0100

    web/charge_manager: Require a valid total current
    Mark the total current input field as required when CM is enabled.
    Without "required", the minimum value is only checked when the field
    loses focus and it's possible to save with a zero current setting.

--- software/web/src/modules/charge_manager/chargers.tsx
+++ software/web/src/modules/charge_manager/chargers.tsx
@@ -526,6 +526,7 @@ export class ChargeManagerChargers extends ConfigComponent<'charge_manager/confi
                 digits={3}
                 min={state.enable_charge_manager ? 6000 : 0}
                 max={1000000}
+                required={state.enable_charge_manager}
                 />
             </FormRow>
 
