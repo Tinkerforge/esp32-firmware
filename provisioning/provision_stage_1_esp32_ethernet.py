82fe81e7f38b9e8cee00861c8dfb8665d663f2d9
Olaf Lüke <olaf@tinkerforge.com>
2024-12-06 16:28:31 +0100

    provisioning: Add --skip-tests parameter to stage1
    This is so we can re-label already flashed ESP32 Ethernet Bricks

--- provisioning/provision_stage_1_esp32_ethernet.py
+++ provisioning/provision_stage_1_esp32_ethernet.py
@@ -32,12 +32,14 @@ ESP_ETHERNET_DEVICE_ID = 115
 def main():
     common_init('/dev/ttyUSB0')
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         fatal_error("Usage: {} firmware_type".format(sys.argv[0]))
 
     firmware_type = sys.argv[1]
     if firmware_type not in ["esp32_ethernet", "warp2", "energy_manager", "energy_manager_v2", "smart_energy_broker"]:
         fatal_error("Unknown firmware type {}".format(firmware_type))
+
+    skip_tests = (len(sys.argv) == 3) and (sys.argv[2] == "--skip-tests")
 
     result = {"start": now()}
 
@@ -65,106 +67,107 @@ def main():
     else:
         ssid = "esp32-" + uid
 
-    run(["systemctl", "restart", "NetworkManager.service"])
-    run(["iw", "reg", "set", "DE"])
+    if not skip_tests:
+        run(["systemctl", "restart", "NetworkManager.service"])
+        run(["iw", "reg", "set", "DE"])
 
-    print("Waiting for ESP wifi. Takes about one minute.")
-    if not wait_for_wifi(ssid, 90):
-        fatal_error("ESP wifi not found after 90 seconds")
+        print("Waiting for ESP wifi. Takes about one minute.")
+        if not wait_for_wifi(ssid, 90):
+            fatal_error("ESP wifi not found after 90 seconds")
 
-    print("Testing ESP Wifi.")
-    with wifi(ssid, passphrase):
-        req = urllib.request.Request("http://10.0.0.1/ethernet/config_update",
-                                     data=json.dumps({"enable_ethernet":True,
-                                                      "ip": "192.168.123.123",
-                                                      "gateway":"0.0.0.0",
-                                                      "subnet":"255.255.0.0",
-                                                      "dns":"0.0.0.0",
-                                                      "dns2":"0.0.0.0"}).encode("utf-8"),
-                                     method='PUT',
-                                     headers={"Content-Type": "application/json"})
+        print("Testing ESP Wifi.")
+        with wifi(ssid, passphrase):
+            req = urllib.request.Request("http://10.0.0.1/ethernet/config_update",
+                                        data=json.dumps({"enable_ethernet":True,
+                                                        "ip": "192.168.123.123",
+                                                        "gateway":"0.0.0.0",
+                                                        "subnet":"255.255.0.0",
+                                                        "dns":"0.0.0.0",
+                                                        "dns2":"0.0.0.0"}).encode("utf-8"),
+                                        method='PUT',
+                                        headers={"Content-Type": "application/json"})
+            try:
+                with urllib.request.urlopen(req, timeout=10) as f:
+                    f.read()
+            except Exception as e:
+                print(e)
+                fatal_error("Failed to set ethernet config!")
+            req = urllib.request.Request("http://10.0.0.1/reboot", data=b'null', method='PUT', headers={"Content-Type": "application/json"})
+            try:
+                with urllib.request.urlopen(req, timeout=10) as f:
+                    f.read()
+            except Exception as e:
+                print("Failed to initiate reboot! Attempting to connect via ethernet anyway.")
+
+            result["wifi_test_successful"] = True
+
+        time.sleep(3)
+        print("Connecting via ethernet to 192.168.123.123", end="")
+        for i in range(30):
+            start = time.time()
+            req = urllib.request.Request("http://192.168.123.123/ethernet/config_update",
+                                    data=json.dumps({"enable_ethernet":True,
+                                                    "ip":"0.0.0.0",
+                                                    "gateway":"0.0.0.0",
+                                                    "subnet":"0.0.0.0",
+                                                    "dns":"0.0.0.0",
+                                                    "dns2":"0.0.0.0"}).encode("utf-8"),
+                                    method='PUT',
+                                    headers={"Content-Type": "application/json"})
+            try:
+                with urllib.request.urlopen(req, timeout=1) as f:
+                    f.read()
+                    break
+            except:
+                pass
+            t = max(0, 1 - (time.time() - start))
+            time.sleep(t)
+            print(".", end="")
+        else:
+            print("Failed to connect via ethernet!")
+            raise Exception("exit 1")
+        print(" Connected.")
+
+        req = urllib.request.Request("http://192.168.123.123/info/version")
         try:
             with urllib.request.urlopen(req, timeout=10) as f:
-                f.read()
+                fw_version = json.loads(f.read().decode("utf-8"))["firmware"].split("-")[0].split("+")[0]
         except Exception as e:
-            print(e)
-            fatal_error("Failed to set ethernet config!")
-        req = urllib.request.Request("http://10.0.0.1/reboot", data=b'null', method='PUT', headers={"Content-Type": "application/json"})
-        try:
-            with urllib.request.urlopen(req, timeout=10) as f:
-                f.read()
-        except Exception as e:
-            print("Failed to initiate reboot! Attempting to connect via ethernet anyway.")
+            fatal_error("Failed to read firmware version!")
 
-        result["wifi_test_successful"] = True
+        if firmware_type in ["warp2", "energy_manager", "energy_manager_v2", "smart_energy_broker"]:
+            try:
+                with urllib.request.urlopen("http://192.168.123.123/hidden_proxy/enable", timeout=10) as f:
+                    f.read()
+            except Exception as e:
+                print(e)
+                fatal_error("Failed to enable hidden_proxy!")
 
-    time.sleep(3)
-    print("Connecting via ethernet to 192.168.123.123", end="")
-    for i in range(30):
-        start = time.time()
-        req = urllib.request.Request("http://192.168.123.123/ethernet/config_update",
-                                 data=json.dumps({"enable_ethernet":True,
-                                                  "ip":"0.0.0.0",
-                                                  "gateway":"0.0.0.0",
-                                                  "subnet":"0.0.0.0",
-                                                  "dns":"0.0.0.0",
-                                                  "dns2":"0.0.0.0"}).encode("utf-8"),
-                                 method='PUT',
-                                 headers={"Content-Type": "application/json"})
-        try:
-            with urllib.request.urlopen(req, timeout=1) as f:
-                f.read()
-                break
-        except:
-            pass
-        t = max(0, 1 - (time.time() - start))
-        time.sleep(t)
-        print(".", end="")
-    else:
-        print("Failed to connect via ethernet!")
-        raise Exception("exit 1")
-    print(" Connected.")
+        time.sleep(3)
+        ipcon = IPConnection()
+        ipcon.connect("192.168.123.123", 4223)
+        result["ethernet_test_successful"] = True
+        print("Connected. Testing bricklet ports")
 
-    req = urllib.request.Request("http://192.168.123.123/info/version")
-    try:
-        with urllib.request.urlopen(req, timeout=10) as f:
-            fw_version = json.loads(f.read().decode("utf-8"))["firmware"].split("-")[0].split("+")[0]
-    except Exception as e:
-        fatal_error("Failed to read firmware version!")
+        test_bricklet_ports(ipcon, ESP_ETHERNET_DEVICE_ID, firmware_type in ["warp2", "energy_manager", "energy_manager_v2", "smart_energy_broker"])
+        result["bricklet_port_test_successful"] = True
 
-    if firmware_type in ["warp2", "energy_manager", "energy_manager_v2", "smart_energy_broker"]:
-        try:
-            with urllib.request.urlopen("http://192.168.123.123/hidden_proxy/enable", timeout=10) as f:
-                f.read()
-        except Exception as e:
-            print(e)
-            fatal_error("Failed to enable hidden_proxy!")
-
-    time.sleep(3)
-    ipcon = IPConnection()
-    ipcon.connect("192.168.123.123", 4223)
-    result["ethernet_test_successful"] = True
-    print("Connected. Testing bricklet ports")
-
-    test_bricklet_ports(ipcon, ESP_ETHERNET_DEVICE_ID, firmware_type in ["warp2", "energy_manager", "energy_manager_v2", "smart_energy_broker"])
-    result["bricklet_port_test_successful"] = True
-
-    led0 = input("Does the status LED blink blue? [y/n]")
-    while led0 not in ("y", "n"):
         led0 = input("Does the status LED blink blue? [y/n]")
-    result["status_led_test_successful"] = led0 == "y"
-    if led0 == "n":
-        fatal_error("Status LED does not work")
+        while led0 not in ("y", "n"):
+            led0 = input("Does the status LED blink blue? [y/n]")
+        result["status_led_test_successful"] = led0 == "y"
+        if led0 == "n":
+            fatal_error("Status LED does not work")
 
-    # We don't test the IO0 button anymore
-    result["io0_test_successful"] = None
+        # We don't test the IO0 button anymore
+        result["io0_test_successful"] = None
 
-    led0_stop = input("Press EN button. Does the status LED stop blinking for some seconds? [y/n]")
-    while led0_stop not in ("y", "n"):
         led0_stop = input("Press EN button. Does the status LED stop blinking for some seconds? [y/n]")
-    result["enable_test_successful"] = led0_stop == "y"
-    if led0_stop == "n":
-        fatal_error("EN button does not work")
+        while led0_stop not in ("y", "n"):
+            led0_stop = input("Press EN button. Does the status LED stop blinking for some seconds? [y/n]")
+        result["enable_test_successful"] = led0_stop == "y"
+        if led0_stop == "n":
+            fatal_error("EN button does not work")
 
     result["tests_successful"] = True
     result["end"] = now()
