4f02b19e6d0a90854b58263555be9ad4a102f655
Erik Fleckstein <erik@tinkerforge.com>
2025-02-13 11:23:22 +0100

    nfc, charge_tracker: Add APIs to api.ts.

--- software/web/src/modules/charge_tracker/api.ts
+++ software/web/src/modules/charge_tracker/api.ts
@@ -11,12 +11,43 @@ export interface config {
 
 export type last_charges = Charge[];
 
-export interface current_charge {
+interface current_charge_base {
     user_id: number;
     meter_start: number;
     evse_uptime_start: number;
     timestamp_minutes: number;
 }
+
+interface current_charge_nfc {
+    authorization_type: 2;
+    authorization_info?: {
+        tag_type: number;
+        tag_id: string;
+    };
+}
+
+interface current_charge_nfc_inject {
+    authorization_type: 3;
+    authorization_info?: {
+        tag_type: number;
+        tag_id: string;
+    };
+}
+
+interface current_charge_lost {
+    authorization_type: 1;
+    authorization_info: null
+}
+
+interface current_charge_none {
+    authorization_type: 0;
+    authorization_info: null
+}
+
+export type current_charge = current_charge_base & (current_charge_nfc
+                                                    | current_charge_nfc_inject
+                                                    | current_charge_lost
+                                                    | current_charge_none);
 
 export interface state {
     tracked_charges: number;
--- software/web/src/modules/nfc/api.ts
+++ software/web/src/modules/nfc/api.ts
@@ -16,3 +16,12 @@ interface SeenTag {
 }
 
 export type seen_tags = SeenTag[];
+
+export interface inject_tag {
+    tag_type: number;
+    tag_id: string;
+}
+
+export type inject_tag_start = inject_tag;
+
+export type inject_tag_stop = inject_tag;
