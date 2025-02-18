Tinkerforge/esp32-firmware

commit 09cee79b347e669780930a195b676f0045eaa35b

meters-sun-spec: Add quirk for SolarEdge model 701
Phase current and phase power factor

software/src/modules/meters_sun_spec/models/generate_from_reference.py
@@ -151,10 +151,10 @@ value_id_mappings_der = {
     "VL3L1"        : [ "VoltageL3L1",                  None  ],
     "LNV"          : [ "VoltageLNAvg",                 None  ],
     "LLV"          : [ "VoltageLLAvg",                 None  ],
-    "AL1"          : [ "CurrentL1ImExSum",             None  ],
-    "AL2"          : [ "CurrentL2ImExSum",             None  ],
-    "AL3"          : [ "CurrentL3ImExSum",             None  ],
-    "A"            : [ "CurrentLSumImExSum",           None  ],
+    "AL1"          : [ "CurrentL1ImExDiff",            None  ],
+    "AL2"          : [ "CurrentL2ImExDiff",            None  ],
+    "AL3"          : [ "CurrentL3ImExDiff",            None  ],
+    "A"            : [ "CurrentLSumImExDiff",          None  ],
 
     "WL1"          : [ "PowerActiveL1ImExDiff",        -1.0  ],
     "WL2"          : [ "PowerActiveL2ImExDiff",        -1.0  ],
@@ -615,6 +615,8 @@ for model in models:
         value_is_inverter_current = model_id >= 100 and model_id < 200 and re.match(r"^CurrentL.Export$", value_id_mapping[0])
         value_is_integer_meter_power_factor = model_id >= 200 and model_id < 210 and re.match(r"^PowerFactorL.+Directional$", value_id_mapping[0])
         value_is_integer_inverter_power_factor = model_id >= 100 and model_id < 110 and re.match(r"^PowerFactorL.+Directional$", value_id_mapping[0])
+        value_is_der_phase_current = model_id >= 700 and model_id < 800 and re.match(r"^CurrentL[123]ImExDiff$", value_id_mapping[0])
+        value_is_der_phase_power_factor = model_id >= 700 and model_id < 800 and re.match(r"^PowerFactorL[123]Directional$", value_id_mapping[0])
 
         get_fn_name =  f"get_model_{model_id:03d}_{name}"
         value['get_fn_name'] = get_fn_name
@@ -645,7 +647,14 @@ for model in models:
 
         # Check for non-implemented value
         if field_type == "int16":
-            print_cpp(r"    if (val == INT16_MIN) return NAN;")
+            if value_is_der_phase_current:
+                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;")
+                print_cpp(r"    if (val == not_implemented_val) return NAN;")
+            elif value_is_der_phase_power_factor:
+                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;")
+                print_cpp(r"    if (val == not_implemented_val) return NAN;")
+            else:
+                print_cpp(r"    if (val == INT16_MIN) return NAN;")
         elif field_type == "uint16":
             if value_is_inverter_current:
                 print_cpp(r"    uint16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_INVERTER_CURRENT_IS_INT16) == 0 ? UINT16_MAX : 0x8000u;")

software/src/modules/meters_sun_spec/models/model_parser_gen.cpp
@@ -7721,7 +7721,8 @@ static float get_model_701_PFL1(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL1;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_scale_factor(model->PF_SF) * -1.0f);
     return fval;
@@ -7731,7 +7732,8 @@ static float get_model_701_AL1(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL1;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_scale_factor(model->A_SF);
     return fval;
@@ -7832,7 +7834,8 @@ static float get_model_701_PFL2(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL2;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_scale_factor(model->PF_SF) * -1.0f);
     return fval;
@@ -7842,7 +7845,8 @@ static float get_model_701_AL2(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL2;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_scale_factor(model->A_SF);
     return fval;
@@ -7943,7 +7947,8 @@ static float get_model_701_PFL3(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL3;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_scale_factor(model->PF_SF) * -1.0f);
     return fval;
@@ -7953,7 +7958,8 @@ static float get_model_701_AL3(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL3;
-    if (val == INT16_MIN) return NAN;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_scale_factor(model->A_SF);
     return fval;
@@ -8053,7 +8059,7 @@ static const MetersSunSpecParser::ModelData model_701_data = {
         { &get_model_701_VA, MeterValueID::PowerApparentLSumImExDiff, 118 },
         { &get_model_701_Var, MeterValueID::PowerReactiveLSumIndCapDiff, 119 },
         { &get_model_701_PF, MeterValueID::PowerFactorLSumDirectional, 117 },
-        { &get_model_701_A, MeterValueID::CurrentLSumImExSum, 113 },
+        { &get_model_701_A, MeterValueID::CurrentLSumImExDiff, 113 },
         { &get_model_701_LLV, MeterValueID::VoltageLLAvg, 114 },
         { &get_model_701_LNV, MeterValueID::VoltageLNAvg, 114 },
         { &get_model_701_Hz, MeterValueID::FrequencyLAvg, 115 },
@@ -8069,7 +8075,7 @@ static const MetersSunSpecParser::ModelData model_701_data = {
         { &get_model_701_VAL1, MeterValueID::PowerApparentL1ImExDiff, 118 },
         { &get_model_701_VarL1, MeterValueID::PowerReactiveL1IndCapDiff, 119 },
         { &get_model_701_PFL1, MeterValueID::PowerFactorL1Directional, 117 },
-        { &get_model_701_AL1, MeterValueID::CurrentL1ImExSum, 113 },
+        { &get_model_701_AL1, MeterValueID::CurrentL1ImExDiff, 113 },
         { &get_model_701_VL1L2, MeterValueID::VoltageL1L2, 114 },
         { &get_model_701_VL1, MeterValueID::VoltageL1N, 114 },
         { &get_model_701_TotWhInjL1, MeterValueID::EnergyActiveL1Export, 120 },
@@ -8080,7 +8086,7 @@ static const MetersSunSpecParser::ModelData model_701_data = {
         { &get_model_701_VAL2, MeterValueID::PowerApparentL2ImExDiff, 118 },
         { &get_model_701_VarL2, MeterValueID::PowerReactiveL2IndCapDiff, 119 },
         { &get_model_701_PFL2, MeterValueID::PowerFactorL2Directional, 117 },
-        { &get_model_701_AL2, MeterValueID::CurrentL2ImExSum, 113 },
+        { &get_model_701_AL2, MeterValueID::CurrentL2ImExDiff, 113 },
         { &get_model_701_VL2L3, MeterValueID::VoltageL2L3, 114 },
         { &get_model_701_VL2, MeterValueID::VoltageL2N, 114 },
         { &get_model_701_TotWhInjL2, MeterValueID::EnergyActiveL2Export, 120 },
@@ -8091,7 +8097,7 @@ static const MetersSunSpecParser::ModelData model_701_data = {
         { &get_model_701_VAL3, MeterValueID::PowerApparentL3ImExDiff, 118 },
         { &get_model_701_VarL3, MeterValueID::PowerReactiveL3IndCapDiff, 119 },
         { &get_model_701_PFL3, MeterValueID::PowerFactorL3Directional, 117 },
-        { &get_model_701_AL3, MeterValueID::CurrentL3ImExSum, 113 },
+        { &get_model_701_AL3, MeterValueID::CurrentL3ImExDiff, 113 },
         { &get_model_701_VL3L1, MeterValueID::VoltageL3L1, 114 },
         { &get_model_701_VL3, MeterValueID::VoltageL3N, 114 },
         { &get_model_701_TotWhInjL3, MeterValueID::EnergyActiveL3Export, 120 },

software/src/modules/meters_sun_spec/meter_sun_spec.cpp
@@ -382,6 +382,9 @@ void MeterSunSpec::scan_next()
                                 // Only meters are inverted, inverters are not.
                                 quirks |= SUN_SPEC_QUIRKS_ACTIVE_POWER_IS_INVERTED;
                             }
+
+                            quirks |= SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16;
+                            quirks |= SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16;
                         }
                         else if (strncmp(m->Mn, "SUNGROW", 32) == 0) {
                             quirks |= SUN_SPEC_QUIRKS_INTEGER_INVERTER_POWER_FACTOR_IS_UNITY;

software/src/modules/meters_sun_spec/model_parser.h
@@ -37,6 +37,8 @@
 #define SUN_SPEC_QUIRKS_ACTIVE_POWER_IS_INVERTED               (1u << 3)
 #define SUN_SPEC_QUIRKS_INTEGER_INVERTER_POWER_FACTOR_IS_UNITY (1u << 4)
 #define SUN_SPEC_QUIRKS_FLOAT_IS_LE32                          (1u << 5)
+#define SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16            (1u << 6) // FIXME: currently this quirk only handles the non-implemented value
+#define SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16       (1u << 7) // FIXME: currently this quirk only handles the non-implemented value
 
 class MetersSunSpecParser
 {

commit a06acb6ed9c566e33efb19b4caa2a4abbcc0353b

meter-sun-spec: Fix wrong not-implemented check for model 701 int16 values
Broken in commit 09cee79b347e669780930a195b676f0045eaa35b.

software/src/modules/meters_sun_spec/models/generate_from_reference.py
@@ -651,10 +651,10 @@ for model in models:
         # Check for non-implemented value
         if field_type == "int16":
             if value_is_der_phase_current:
-                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;")
+                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MIN : -1;")
                 print_cpp(r"    if (val == not_implemented_val) return NAN;")
             elif value_is_der_phase_power_factor:
-                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;")
+                print_cpp(r"    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MIN : -1;")
                 print_cpp(r"    if (val == not_implemented_val) return NAN;")
             else:
                 print_cpp(r"    if (val == INT16_MIN) return NAN;")

software/src/modules/meters_sun_spec/models/model_parser_gen.cpp
@@ -7724,7 +7724,7 @@ static float get_model_701_PFL1(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL1;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_sun_spec_scale_factor(model->PF_SF) * -1.0f);
@@ -7735,7 +7735,7 @@ static float get_model_701_AL1(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL1;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_sun_spec_scale_factor(model->A_SF);
@@ -7837,7 +7837,7 @@ static float get_model_701_PFL2(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL2;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_sun_spec_scale_factor(model->PF_SF) * -1.0f);
@@ -7848,7 +7848,7 @@ static float get_model_701_AL2(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL2;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_sun_spec_scale_factor(model->A_SF);
@@ -7950,7 +7950,7 @@ static float get_model_701_PFL3(const void *register_data, uint32_t quirks, bool
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->PFL3;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_POWER_FACTOR_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= (get_sun_spec_scale_factor(model->PF_SF) * -1.0f);
@@ -7961,7 +7961,7 @@ static float get_model_701_AL3(const void *register_data, uint32_t quirks, bool 
 {
     const struct SunSpecDERACMeasurementModel701_s *model = static_cast<const struct SunSpecDERACMeasurementModel701_s *>(register_data);
     int16_t val = model->AL3;
-    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MAX : -1;
+    int16_t not_implemented_val = (quirks & SUN_SPEC_QUIRKS_DER_PHASE_CURRENT_IS_UINT16) == 0 ? INT16_MIN : -1;
     if (val == not_implemented_val) return NAN;
     float fval = static_cast<float>(val);
     fval *= get_sun_spec_scale_factor(model->A_SF);