10da8004314e61de9100e99e9463c9ef3f94ca5f
Frederic Henrichs <frederic@tinkerforge.com>
2025-02-18 10:54:25 +0100

    web/automation: Fix time in led action text.

--- software/web/src/modules/evse_common/translation_en.tsx
+++ software/web/src/modules/evse_common/translation_en.tsx
@@ -274,9 +274,9 @@ let x = {
             "automation_led_action_text": /*FFN*/(indication_number: number, indication_text: string, duration: number, color: string) => {
                 let c = color == "" ? "" : <>in <span class="px-2 mr-1" style={"background-color: " + color + "; border: 1px solid black;"}></span></>
                 if (indication_number == 0)
-                    return <>turn the status LED <b>{indication_text}</b> for <b>{duration} seconds</b>.</>
+                    return <>turn the status LED <b>{indication_text}</b> for <b>{duration / 1000} seconds</b>.</>
                 if (indication_number == 255)
-                    return <>turn the status LED <b>{indication_text}</b> {c} for <b>{duration} seconds</b>.</>
+                    return <>turn the status LED <b>{indication_text}</b> {c} for <b>{duration / 1000} seconds</b>.</>
                 return<>show <b>{indication_text}</b> {c} for <b>{duration / 1000} seconds</b> on the status LED.</>
             }/*NF*/
         },
