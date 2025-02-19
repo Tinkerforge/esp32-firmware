Tinkerforge/esp32-firmware

commit 45efd8eaf4e2fa9d17a2ded544bff3f5febe18ac

meters-modbus-tcp: Avoid specifying address numbers twice

software/src/modules/meters_modbus_tcp/meter_modbus_tcp.cpp
@@ -36,66 +36,66 @@
 
 #define NUMBER_TO_ADDRESS(number) ((number) - 1u)
 
-#define SUNGROW_INVERTER_OUTPUT_TYPE_ADDRESS                 NUMBER_TO_ADDRESS(5002u)
-#define SUNGROW_INVERTER_GRID_FREQUENCY_ADDRESS              NUMBER_TO_ADDRESS(5036u)
-#define SUNGROW_HYBRID_INVERTER_RUNNING_STATE_ADDRESS        NUMBER_TO_ADDRESS(13001u)
-#define SUNGROW_HYBRID_INVERTER_BATTERY_CURRENT_ADDRESS      NUMBER_TO_ADDRESS(13021u)
-#define SUNGROW_HYBRID_INVERTER_BATTERY_POWER_ADDRESS        NUMBER_TO_ADDRESS(13022u)
-#define SUNGROW_STRING_INVERTER_TOTAL_ACTIVE_POWER_ADDRESS   NUMBER_TO_ADDRESS(5031u)
+#define SUNGROW_INVERTER_OUTPUT_TYPE_ADDRESS                               static_cast<size_t>(SungrowInverterOutputTypeAddress::OutputType)
+#define SUNGROW_INVERTER_GRID_FREQUENCY_ADDRESS                            static_cast<size_t>(SungrowHybridInverterGridAddress::GridFrequency)
+#define SUNGROW_HYBRID_INVERTER_RUNNING_STATE_ADDRESS                      static_cast<size_t>(SungrowHybridInverterBatteryAddress::RunningState)
+#define SUNGROW_HYBRID_INVERTER_BATTERY_CURRENT_ADDRESS                    static_cast<size_t>(SungrowHybridInverterBatteryAddress::BatteryCurrent)
+#define SUNGROW_HYBRID_INVERTER_BATTERY_POWER_ADDRESS                      static_cast<size_t>(SungrowHybridInverterBatteryAddress::BatteryPower)
+#define SUNGROW_STRING_INVERTER_TOTAL_ACTIVE_POWER_ADDRESS                 static_cast<size_t>(SungrowStringInverter1P2LAddress::TotalActivePowerExport)
 
-#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L1_ADDRESS 808u
-#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L2_ADDRESS 809u
-#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L3_ADDRESS 810u
-#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L1_ADDRESS          817u
-#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L2_ADDRESS          818u
-#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L3_ADDRESS          819u
+#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L1_ADDRESS               static_cast<size_t>(VictronEnergyGXInverterAddress::ACCoupledPVOnOutputL1)
+#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L2_ADDRESS               static_cast<size_t>(VictronEnergyGXInverterAddress::ACCoupledPVOnOutputL2)
+#define VICTRON_ENERGY_GX_AC_COUPLED_PV_ON_OUTPUT_L3_ADDRESS               static_cast<size_t>(VictronEnergyGXInverterAddress::ACCoupledPVOnOutputL3)
+#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L1_ADDRESS                        static_cast<size_t>(VictronEnergyGXLoadAddress::ACConsumptionL1)
+#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L2_ADDRESS                        static_cast<size_t>(VictronEnergyGXLoadAddress::ACConsumptionL2)
+#define VICTRON_ENERGY_GX_AC_CONSUMPTION_L3_ADDRESS                        static_cast<size_t>(VictronEnergyGXLoadAddress::ACConsumptionL3)
 
-#define DEYE_HYBRID_INVERTER_DEVICE_TYPE_ADDRESS 0u
-#define DEYE_HYBRID_INVERTER_PV1_POWER_ADDRESS   672u
-#define DEYE_HYBRID_INVERTER_PV2_POWER_ADDRESS   673u
-#define DEYE_HYBRID_INVERTER_PV3_POWER_ADDRESS   674u
-#define DEYE_HYBRID_INVERTER_PV4_POWER_ADDRESS   675u
-#define DEYE_HYBRID_INVERTER_PV1_VOLTAGE_ADDRESS 676u
-#define DEYE_HYBRID_INVERTER_PV1_CURRENT_ADDRESS 677u
-#define DEYE_HYBRID_INVERTER_PV2_VOLTAGE_ADDRESS 678u
-#define DEYE_HYBRID_INVERTER_PV2_CURRENT_ADDRESS 679u
-#define DEYE_HYBRID_INVERTER_PV3_VOLTAGE_ADDRESS 680u
-#define DEYE_HYBRID_INVERTER_PV3_CURRENT_ADDRESS 681u
-#define DEYE_HYBRID_INVERTER_PV4_VOLTAGE_ADDRESS 682u
-#define DEYE_HYBRID_INVERTER_PV4_CURRENT_ADDRESS 683u
+#define DEYE_HYBRID_INVERTER_DEVICE_TYPE_ADDRESS                           static_cast<size_t>(DeyeHybridInverterDeviceTypeAddress::DeviceType)
+#define DEYE_HYBRID_INVERTER_PV1_POWER_ADDRESS                             static_cast<size_t>(DeyeHybridInverterPVAddress::PV1Power)
+#define DEYE_HYBRID_INVERTER_PV2_POWER_ADDRESS                             static_cast<size_t>(DeyeHybridInverterPVAddress::PV2Power)
+#define DEYE_HYBRID_INVERTER_PV3_POWER_ADDRESS                             static_cast<size_t>(DeyeHybridInverterPVAddress::PV3Power)
+#define DEYE_HYBRID_INVERTER_PV4_POWER_ADDRESS                             static_cast<size_t>(DeyeHybridInverterPVAddress::PV4Power)
+#define DEYE_HYBRID_INVERTER_PV1_VOLTAGE_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV1Voltage)
+#define DEYE_HYBRID_INVERTER_PV1_CURRENT_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV1Current)
+#define DEYE_HYBRID_INVERTER_PV2_VOLTAGE_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV2Voltage)
+#define DEYE_HYBRID_INVERTER_PV2_CURRENT_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV2Current)
+#define DEYE_HYBRID_INVERTER_PV3_VOLTAGE_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV3Voltage)
+#define DEYE_HYBRID_INVERTER_PV3_CURRENT_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV3Current)
+#define DEYE_HYBRID_INVERTER_PV4_VOLTAGE_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV4Voltage)
+#define DEYE_HYBRID_INVERTER_PV4_CURRENT_ADDRESS                           static_cast<size_t>(DeyeHybridInverterPVAddress::PV4Current)
 
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_ACTIVE_POWER                 2007u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_TOTAL_ACTIVE_ENERGY          2310u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_TOTAL_ACTIVE_RETURNED_ENERGY 2312u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_ACTIVE_POWER                 2027u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_TOTAL_ACTIVE_ENERGY          2330u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_TOTAL_ACTIVE_RETURNED_ENERGY 2332u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_ACTIVE_POWER                 2047u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_TOTAL_ACTIVE_ENERGY          2350u
-#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_TOTAL_ACTIVE_RETURNED_ENERGY 2352u
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_ACTIVE_POWER                    static_cast<size_t>(ShellyEMMonophaseChannel1AsL1Address::Channel1ActivePower)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_TOTAL_ACTIVE_ENERGY             static_cast<size_t>(ShellyEMMonophaseChannel1AsL1Address::Channel1TotalActiveEnergyPerpetualCount)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_1_TOTAL_ACTIVE_RETURNED_ENERGY    static_cast<size_t>(ShellyEMMonophaseChannel1AsL1Address::Channel1TotalActiveReturnedEnergyPerpetualCount)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_ACTIVE_POWER                    static_cast<size_t>(ShellyEMMonophaseChannel2AsL1Address::Channel2ActivePower)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_TOTAL_ACTIVE_ENERGY             static_cast<size_t>(ShellyEMMonophaseChannel2AsL1Address::Channel2TotalActiveEnergyPerpetualCount)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_2_TOTAL_ACTIVE_RETURNED_ENERGY    static_cast<size_t>(ShellyEMMonophaseChannel2AsL1Address::Channel2TotalActiveReturnedEnergyPerpetualCount)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_ACTIVE_POWER                    static_cast<size_t>(ShellyEMMonophaseChannel3AsL1Address::Channel3ActivePower)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_TOTAL_ACTIVE_ENERGY             static_cast<size_t>(ShellyEMMonophaseChannel3AsL1Address::Channel3TotalActiveEnergyPerpetualCount)
+#define SHELLY_PRO_XEM_MONOPHASE_CHANNEL_3_TOTAL_ACTIVE_RETURNED_ENERGY    static_cast<size_t>(ShellyEMMonophaseChannel3AsL1Address::Channel3TotalActiveReturnedEnergyPerpetualCount)
 
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_INPUT_OR_MODEL_ID_ADDRESS NUMBER_TO_ADDRESS(40264u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCA_SF_ADDRESS            NUMBER_TO_ADDRESS(40256u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCV_SF_ADDRESS            NUMBER_TO_ADDRESS(40257u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCW_SF_ADDRESS            NUMBER_TO_ADDRESS(40258u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCWH_SF_ADDRESS           NUMBER_TO_ADDRESS(40259u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCA_ADDRESS        NUMBER_TO_ADDRESS(40313u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCV_ADDRESS        NUMBER_TO_ADDRESS(40314u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCW_ADDRESS        NUMBER_TO_ADDRESS(40315u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCWH_ADDRESS       NUMBER_TO_ADDRESS(40316u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCA_ADDRESS     NUMBER_TO_ADDRESS(40333u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCV_ADDRESS     NUMBER_TO_ADDRESS(40334u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCW_ADDRESS     NUMBER_TO_ADDRESS(40335u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCWH_ADDRESS    NUMBER_TO_ADDRESS(40336u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHASTATE_ADDRESS          NUMBER_TO_ADDRESS(40352u)
-#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHASTATE_SF_ADDRESS       NUMBER_TO_ADDRESS(40366u)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_INPUT_OR_MODEL_ID_ADDRESS       static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryTypeAddress::InputOrModelID)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCA_SF_ADDRESS                  static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DCA_SF)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCV_SF_ADDRESS                  static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DCV_SF)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCW_SF_ADDRESS                  static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DCW_SF)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DCWH_SF_ADDRESS                 static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DCWH_SF)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCA_ADDRESS              static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChargeDCA)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCV_ADDRESS              static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChargeDCV)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCW_ADDRESS              static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChargeDCW)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHARGE_DCWH_ADDRESS             static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChargeDCWH)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCA_ADDRESS           static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DischargeDCA)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCV_ADDRESS           static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DischargeDCV)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCW_ADDRESS           static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DischargeDCW)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_DISCHARGE_DCWH_ADDRESS          static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::DischargeDCWH)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHASTATE_ADDRESS                static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChaState)
+#define FRONIUS_GEN24_PLUS_HYBRID_INVERTER_CHASTATE_SF_ADDRESS             static_cast<size_t>(FroniusGEN24PlusHybridInverterBatteryIntegerAddress::ChaState_SF)
 
-#define CARLO_GAVAZZI_EM100_OR_ET100_W               0x04
-#define CARLO_GAVAZZI_EM100_OR_ET100_KWH_PLUS_TOTAL  0x10
-#define CARLO_GAVAZZI_EM100_OR_ET100_KWH_MINUS_TOTAL 0x20
-#define CARLO_GAVAZZI_EM510_W                        0x04
-#define CARLO_GAVAZZI_EM510_KWH_PLUS_TOTAL           0x10
-#define CARLO_GAVAZZI_EM510_KWH_MINUS_TOTAL          0x20
+#define CARLO_GAVAZZI_EM100_OR_ET100_W                                     static_cast<size_t>(CarloGavazziEM100andET100AtL1Address::W)
+#define CARLO_GAVAZZI_EM100_OR_ET100_KWH_POSITIVE_TOTAL                    static_cast<size_t>(CarloGavazziEM100andET100AtL1Address::KWhPositiveTotal)
+#define CARLO_GAVAZZI_EM100_OR_ET100_KWH_NEGATIVE_TOTAL                    static_cast<size_t>(CarloGavazziEM100andET100AtL1Address::KWhNegativeTotal)
+#define CARLO_GAVAZZI_EM510_W                                              static_cast<size_t>(CarloGavazziEM510atL1Address::W)
+#define CARLO_GAVAZZI_EM510_KWH_POSITIVE_TOTAL                             static_cast<size_t>(CarloGavazziEM510atL1Address::KWhPositiveTotal)
+#define CARLO_GAVAZZI_EM510_KWH_NEGATIVE_TOTAL                             static_cast<size_t>(CarloGavazziEM510atL1Address::KWhNegativeTotal)
 
 #define MODBUS_VALUE_TYPE_TO_REGISTER_COUNT(x) (static_cast<uint8_t>(x) & 0x07)
 #define MODBUS_VALUE_TYPE_TO_REGISTER_ORDER_LE(x) ((static_cast<uint8_t>(x) >> 5) & 1)
@@ -200,6 +200,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
 
     case MeterModbusTCPTableID::SungrowHybridInverter:
         sungrow_hybrid_inverter_virtual_meter = ephemeral_config->get("table")->get()->get("virtual_meter")->asEnum<SungrowHybridInverterVirtualMeter>();
+        sungrow_inverter_output_type = -1;
         device_address = static_cast<uint8_t>(ephemeral_config->get("table")->get()->get("device_address")->asUint());
 
         switch (sungrow_hybrid_inverter_virtual_meter) {
@@ -208,7 +209,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
             return;
 
         case SungrowHybridInverterVirtualMeter::Inverter:
-            table = &sungrow_hybrid_inverter_output_type_table;
+            table = &sungrow_inverter_output_type_table;
             default_location = MeterLocation::Inverter;
             break;
 
@@ -244,7 +245,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
             return;
 
         case SungrowStringInverterVirtualMeter::Inverter:
-            table = &sungrow_string_inverter_output_type_table;
+            table = &sungrow_inverter_output_type_table;
             default_location = MeterLocation::Inverter;
             break;
 
@@ -334,6 +335,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
 
     case MeterModbusTCPTableID::DeyeHybridInverter:
         deye_hybrid_inverter_virtual_meter = ephemeral_config->get("table")->get()->get("virtual_meter")->asEnum<DeyeHybridInverterVirtualMeter>();
+        deye_hybrid_inverter_device_type = -1;
         device_address = static_cast<uint8_t>(ephemeral_config->get("table")->get()->get("device_address")->asUint());
 
         switch (deye_hybrid_inverter_virtual_meter) {
@@ -660,6 +662,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
 
     case MeterModbusTCPTableID::FroniusGEN24PlusHybridInverter:
         fronius_gen24_plus_hybrid_inverter_virtual_meter = ephemeral_config->get("table")->get()->get("virtual_meter")->asEnum<FroniusGEN24PlusHybridInverterVirtualMeter>();
+        fronius_gen24_plus_hybrid_inverter_input_or_model_id = 0;
         device_address = static_cast<uint8_t>(ephemeral_config->get("table")->get()->get("device_address")->asUint());
 
         switch (fronius_gen24_plus_hybrid_inverter_virtual_meter) {
@@ -1764,15 +1767,15 @@ void MeterModbusTCP::read_done_callback()
     }
     else if (is_carlo_gavazzi_em100_or_et100()) {
         if (register_start_address == CARLO_GAVAZZI_EM100_OR_ET100_W
-         || register_start_address == CARLO_GAVAZZI_EM100_OR_ET100_KWH_PLUS_TOTAL
-         || register_start_address == CARLO_GAVAZZI_EM100_OR_ET100_KWH_MINUS_TOTAL) {
+         || register_start_address == CARLO_GAVAZZI_EM100_OR_ET100_KWH_POSITIVE_TOTAL
+         || register_start_address == CARLO_GAVAZZI_EM100_OR_ET100_KWH_NEGATIVE_TOTAL) {
             meters.update_value(slot, table->index[read_index + 1], value);
         }
     }
     else if (is_carlo_gavazzi_em510()) {
         if (register_start_address == CARLO_GAVAZZI_EM510_W
-         || register_start_address == CARLO_GAVAZZI_EM510_KWH_PLUS_TOTAL
-         || register_start_address == CARLO_GAVAZZI_EM510_KWH_MINUS_TOTAL) {
+         || register_start_address == CARLO_GAVAZZI_EM510_KWH_POSITIVE_TOTAL
+         || register_start_address == CARLO_GAVAZZI_EM510_KWH_NEGATIVE_TOTAL) {
             meters.update_value(slot, table->index[read_index + 1], value);
         }
     }

software/src/modules/meters_modbus_tcp/fronius.py
@@ -4,85 +4,85 @@ from copy import deepcopy
 
 battery_integer_values = [
     {
-        'name': 'DCA_SF: Current Scale Factor',
+        'name': 'DCA_SF: Current Scale Factor | DCA_SF',
         'value_id': 'VALUE_ID_META',
         'start_address': 40256,
         'value_type': 'S16',  # SunSpec: sunssf
     },
     {
-        'name': 'DCV_SF: Voltage Scale Factor',
+        'name': 'DCV_SF: Voltage Scale Factor | DCV_SF',
         'value_id': 'VALUE_ID_META',
         'start_address': 40257,
         'value_type': 'S16',  # SunSpec: sunssf
     },
     {
-        'name': 'DCW_SF: Power Scale Factor',
+        'name': 'DCW_SF: Power Scale Factor | DCW_SF',
         'value_id': 'VALUE_ID_META',
         'start_address': 40258,
         'value_type': 'S16',  # SunSpec: sunssf
     },
     {
-        'name': 'DCWH_SF: Energy Scale Factor',
+        'name': 'DCWH_SF: Energy Scale Factor | DCWH_SF',
         'value_id': 'VALUE_ID_META',
         'start_address': 40259,
         'value_type': 'S16',  # SunSpec: sunssf
     },
     {
-        'name': 'module/3/DCA: DC Charge Current [A]',
+        'name': 'module/3/DCA: DC Charge Current [A] | Charge DCA',
         'value_id': 'VALUE_ID_META',
         'start_address': 40313,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/3/DCV: DC Charge Voltage [V]',
+        'name': 'module/3/DCV: DC Charge Voltage [V] | Charge DCV',
         'value_id': 'VALUE_ID_META',
         'start_address': 40314,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/3/DCW: DC Charge Power [W]',
+        'name': 'module/3/DCW: DC Charge Power [W] | Charge DCW',
         'value_id': 'VALUE_ID_META',
         'start_address': 40315,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/3/DCWH: Lifetime Charge Energy [Wh]',
+        'name': 'module/3/DCWH: Lifetime Charge Energy [Wh] | Charge DCWH',
         'value_id': 'VALUE_ID_META',
         'start_address': 40316,
         'value_type': 'U32BE',  # SunSpec: acc32
     },
     {
-        'name': 'module/4/DCA: DC Discharge Current [A]',
+        'name': 'module/4/DCA: DC Discharge Current [A] | Discharge DCA',
         'value_id': 'VALUE_ID_META',
         'start_address': 40333,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/4/DCV: DC Discharge Voltage [V]',
+        'name': 'module/4/DCV: DC Discharge Voltage [V] | Discharge DCV',
         'value_id': 'VALUE_ID_META',
         'start_address': 40334,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/4/DCW: DC Discharge Power [W]',
+        'name': 'module/4/DCW: DC Discharge Power [W] | Discharge DCW',
         'value_id': 'VALUE_ID_META',
         'start_address': 40335,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'module/4/DCWH: Lifetime Discharge Energy [Wh]',
+        'name': 'module/4/DCWH: Lifetime Discharge Energy [Wh] | Discharge DCWH',
         'value_id': 'VALUE_ID_META',
         'start_address': 40336,
         'value_type': 'U32BE',  # SunSpec: acc32
     },
     {
-        'name': 'ChaState: Currently available energy as a percent of the capacity rating [%]',
+        'name': 'ChaState: Currently available energy as a percent of the capacity rating [%] | ChaState',
         'value_id': 'VALUE_ID_META',
         'start_address': 40352,
         'value_type': 'U16',  # SunSpec: uint16
     },
     {
-        'name': 'ChaState_SF: Scale factor for available energy percent',
+        'name': 'ChaState_SF: Scale factor for available energy percent | ChaState_SF',
         'value_id': 'VALUE_ID_META',
         'start_address': 40366,
         'value_type': 'S16',  # SunSpec: sunssf
@@ -135,7 +135,7 @@ specs = [
         'start_address_offset': 1,
         'values': [
             {
-                'name': 'module/1/ID: Input ID | ID: SunSpec Model ID',
+                'name': 'module/1/ID: Input ID or ID: SunSpec Model ID | Input or Model ID',
                 'value_id': 'VALUE_ID_META',
                 'start_address': 40264,
                 'value_type': 'U16',

software/src/modules/meters_modbus_tcp/meter_modbus_tcp.h
@@ -133,7 +133,7 @@ private:
     // Sungrow
     SungrowHybridInverterVirtualMeter sungrow_hybrid_inverter_virtual_meter;
     SungrowStringInverterVirtualMeter sungrow_string_inverter_virtual_meter;
-    int sungrow_inverter_output_type = -1;
+    int sungrow_inverter_output_type;
     uint16_t sungrow_hybrid_inverter_running_state;
 
     // Solarmax
@@ -150,7 +150,7 @@ private:
 
     // Deye
     DeyeHybridInverterVirtualMeter deye_hybrid_inverter_virtual_meter;
-    int deye_hybrid_inverter_device_type = -1;
+    int deye_hybrid_inverter_device_type;
     float deye_hybrid_inverter_pv1_power;
     float deye_hybrid_inverter_pv2_power;
     float deye_hybrid_inverter_pv3_power;
@@ -184,7 +184,7 @@ private:
 
     // Fronius
     FroniusGEN24PlusHybridInverterVirtualMeter fronius_gen24_plus_hybrid_inverter_virtual_meter;
-    uint16_t fronius_gen24_plus_hybrid_inverter_input_or_model_id = 0;
+    uint16_t fronius_gen24_plus_hybrid_inverter_input_or_model_id;
     size_t fronius_gen24_plus_hybrid_inverter_start_address_shift;
     int16_t fronius_gen24_plus_hybrid_inverter_dca_sf;
     int16_t fronius_gen24_plus_hybrid_inverter_dcv_sf;

software/src/modules/meters_modbus_tcp/prepare.py
@@ -27,6 +27,7 @@ for spec in specs:
         spec_name = util.FlavoredName(spec['name'].format(variant=variant_spec)).get()
 
         value_names = []
+        value_addresses = []
         value_specs = []
         value_ids_raw = []
         value_ids = []
@@ -39,19 +40,26 @@ for spec in specs:
             if variant_value != None and variant_value not in variant_spec:
                 continue
 
-            if value["name"] in value_names:
+            if value['name'] in value_names:
                 print(f'Error: Value {spec_name.space} / {value["name"]} is duplicate')
                 sys.exit(1)
 
-            value_names.append(value["name"])
+            value_names.append(value['name'])
 
             start_address_offset = value.get('start_address_offset', spec.get('start_address_offset', 0))
+            start_address = value['start_address'] - start_address_offset if value['start_address'] != 'START_ADDRESS_VIRTUAL' else 'START_ADDRESS_VIRTUAL'
+
+            if start_address != "START_ADDRESS_VIRTUAL":
+                address_name_words = value['name'].split('|', 1)[-1].split('[')[0].replace('-', ' ').replace('/', ' ').split(' ')
+                address_name = ''.join([word[0].upper() + word[1:] for word in address_name_words if len(word) > 0])
+
+                value_addresses.append(f'    {address_name} = {start_address},')
 
             value_specs.append(
                 '    {\n'
                 f'        "{value["name"]}",\n'
                 f'        ModbusRegisterType::{value.get("register_type", spec["register_type"])},\n'
-                f'        {value["start_address"] - start_address_offset if value["start_address"] != "START_ADDRESS_VIRTUAL" else "START_ADDRESS_VIRTUAL"},\n'
+                f'        {start_address},\n'
                 f'        ModbusValueType::{value.get("value_type", "None")},\n'
                 f'        {"true" if value.get("drop_sign", False) else "false"},\n'
                 f'        {value.get("offset", 0.0)}f,\n'
@@ -64,15 +72,16 @@ for spec in specs:
             elif value['value_id'] == 'VALUE_ID_DEBUG':
                 value_index.append('    VALUE_INDEX_DEBUG,')
             else:
-                if value["value_id"] in value_ids_raw:
+                if value['value_id'] in value_ids_raw:
                     print(f'Error: Value {spec_name.space} / {value["name"]} has duplicate ID {value["value_id"]}')
                     sys.exit(1)
 
-                value_ids_raw.append(value["value_id"])
+                value_ids_raw.append(value['value_id'])
                 value_ids.append(f'    MeterValueID::{value["value_id"]},')
                 value_index.append(f'    {current_index},')
                 current_index += 1
 
+        spec_values.append(f'enum class {spec_name.camel}Address : uint16_t {{\n' + '\n'.join(value_addresses) + '\n};')
         spec_values.append(f'static const MeterModbusTCP::ValueSpec {spec_name.under}_specs[] = {{\n' + '\n'.join(value_specs) + '\n};')
 
         if len(value_ids) > 0:

software/src/modules/meters_modbus_tcp/siemens.py
@@ -1332,19 +1332,19 @@ specs = [
                 'value_type': 'F32BE',
             },
             {
-                'name': '3‐Phasen‐Durchschnitt Spannung L-N [V]',
+                'name': 'Drei-Phasen-Durchschnitt Spannung L-N [V]',
                 'value_id': 'VoltageLNAvg',
                 'start_address': 57,
                 'value_type': 'F32BE',
             },
             {
-                'name': '3‐Phasen‐Durchschnitt Spannung L-L [V]',
+                'name': 'Drei-Phasen-Durchschnitt Spannung L-L [V]',
                 'value_id': 'VoltageLLAvg',
                 'start_address': 59,
                 'value_type': 'F32BE',
             },
             {
-                'name': '3‐Phasen‐Durchschnitt Strom [A]',
+                'name': 'Drei-Phasen-Durchschnitt Strom [A]',
                 'value_id': 'CurrentLAvgImExDiff',  # FIXME: direction?
                 'start_address': 61,
                 'value_type': 'F32BE',
@@ -1362,7 +1362,7 @@ specs = [
                 'value_type': 'F32BE',
             },
             {
-                'name': 'Gesamtwirkleistung [var]',
+                'name': 'Gesamtblindleistung [var]',
                 'value_id': 'PowerReactiveLSumIndCapDiff',  # FIXME: direction?
                 'start_address': 67,
                 'value_type': 'F32BE',

software/src/modules/meters_modbus_tcp/sungrow.py
@@ -161,9 +161,9 @@ inverter_values = [
         'variant': 'String',
     },
     {
-        'name': 'Total Output Energy [0.1 kWh]',
+        'name': 'Total Output Energy Alternative [0.1 kWh]',  # FIXME: use this instead of 5004, if available
         'value_id': 'VALUE_ID_DEBUG',
-        'start_address': 5144,  # FIXME: use this instead of 5004, if available
+        'start_address': 5144,
         'value_type': 'U32LE',
         'scale_factor': 0.1,
         'variant': 'String',
@@ -271,8 +271,7 @@ hybrid_inverter_phase_current_values = [
 
 specs = [
     {
-        'name': 'Sungrow {variant} Inverter Output Type',
-        'variants': ['Hybrid', 'String'],
+        'name': 'Sungrow Inverter Output Type',
         'register_type': 'InputRegister',
         'start_address_offset': 1,
         'values': [
@@ -319,7 +318,7 @@ specs = [
                 'scale_factor': 0.1,
             },
             {
-                'name': 'Grid Frequency [0.01 Hz]',  # FIXME: use this instead of 5036, if available
+                'name': 'Grid Frequency Alternative [0.01 Hz]',  # FIXME: use this instead of 5036, if available
                 'value_id': 'VALUE_ID_DEBUG',
                 'start_address': 5148,
                 'value_type': 'U16',

commit 10e17361f7aee0823083a4dc9baac50128120036

meters-modbus-tcp: Fix output type detection for Sungrow string inverters
Broken in commit 45efd8eaf4e2fa9d17a2ded544bff3f5febe18ac.

software/src/modules/meters_modbus_tcp/meter_modbus_tcp.cpp
@@ -237,6 +237,7 @@ void MeterModbusTCP::setup(Config *ephemeral_config)
 
     case MeterModbusTCPTableID::SungrowStringInverter:
         sungrow.string_inverter_virtual_meter = ephemeral_config->get("table")->get()->get("virtual_meter")->asEnum<SungrowStringInverterVirtualMeter>();
+        sungrow.inverter_output_type = -1;
         device_address = static_cast<uint8_t>(ephemeral_config->get("table")->get()->get("device_address")->asUint());
 
         switch (sungrow.string_inverter_virtual_meter) {