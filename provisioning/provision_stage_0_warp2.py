0658440067ba6909fa601d099d930089df941798
Test RPi <info@tinkerforge.com>
2025-03-11 15:50:28 +0100

    provision/{stage_0_warp2,warp_esp32}: Fix revision check again.

--- provisioning/provision_stage_0_warp2.py
+++ provisioning/provision_stage_0_warp2.py
@@ -48,8 +48,8 @@ def main():
     result = {"start": now()}
 
     print("Checking ESP state")
-    allowed_revision=3.1 if firmware_type == "warp3" else 3
-    mac_address = check_if_esp_is_sane_and_get_mac(ignore_flash_errors=True, allowed_revision=allowed_revision)
+
+    mac_address = check_if_esp_is_sane_and_get_mac(ignore_flash_errors=True, allowed_revision=[3.0, 3.1])
     print("MAC Address is {}".format(mac_address))
     result["mac"] = mac_address
 
