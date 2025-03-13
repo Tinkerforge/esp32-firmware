Tinkerforge/esp32-firmware

commit c56f9a4d621dda380614ff03cb9639234e679327

authentication: Rewrite front-end. Fixes #240.
Username can't be changed without also changing or reentering
the password because the digest hash depends on both.

software/web/src/modules/authentication/main.tsx
@@ -31,9 +31,9 @@ import { ConfigComponent } from "../../ts/components/config_component";
 import { ConfigForm } from "../../ts/components/config_form";
 import { FormRow } from "../../ts/components/form_row";
 import { InputText } from "../../ts/components/input_text";
-import { Switch } from "src/ts/components/switch";
 import { InputPassword } from "src/ts/components/input_password";
-import { Slash } from "react-feather";
+import { Button } from "react-bootstrap";
+import { Switch } from "src/ts/components/switch";
 
 type AuthenticationState = API.getType['authentication/config'] & {password: string};
 
@@ -44,49 +44,65 @@ export class Authentication extends ConfigComponent<'authentication/config', {},
               __("authentication.script.reboot_content_changed"));
     }
 
-    http_auth_allowed() {
-        return (this.state.digest_hash == null && (this.state.password !== "")) ||
-               (this.state.digest_hash == "" && this.state.password !== undefined && this.state.password !== null && this.state.password !== "");
-    };
+    hash(username: string, password?: string) {
+        if (password === "")
+            return "";
 
-    override async transformSave(cfg: AuthenticationState) {
-        cfg.digest_hash = (this.state.password != null && this.state.password != "") ? YaMD5.YaMD5.hashStr(cfg.username + ":esp32-lib:" + this.state.password) : this.state.password;
-        return cfg;
+        return YaMD5.YaMD5.hashStr(username + ":esp32-lib:" + password);
     }
 
     override render(props: {}, state: AuthenticationState) {
         if (!util.render_allowed())
             return <></>
 
-        let auth_allowed = this.http_auth_allowed();
+        let user_required = state.enable_auth || state.digest_hash != "";
+        let pass_required = (state.enable_auth && !API.get("authentication/config").enable_auth) || (state.username != API.get("authentication/config").username);
 
         return (
             <>
                 <ConfigForm id="auth_config_form" title={__("authentication.content.authentication")} isModified={this.isModified()} onSave={() => this.save()} onReset={this.reset} onDirtyChange={(d) => this.ignore_updates = d}>
                     <FormRow label={__("authentication.content.enable_authentication")}>
                         <Switch desc={__("authentication.content.enable_authentication_desc")}
-                                checked={auth_allowed && state.enable_auth}
+                                checked={state.enable_auth}
                                 onClick={this.toggle("enable_auth")}
-                                disabled={!auth_allowed}
-                                className={!auth_allowed && state.enable_auth ? "is-invalid" : ""}
                         />
-                        <div class="invalid-feedback">{__("authentication.content.enable_authentication_invalid")}</div>
                     </FormRow>
 
                     <FormRow label={__("authentication.content.username")}>
                         <InputText value={state.username}
-                                    onValue={this.set("username")}
-                                    minLength={1} maxLength={32}
-                                    required/>
+                                   onValue={this.set("username")}
+                                   maxLength={32}
+                                   required={user_required}
+                                   placeholder={user_required ? __('component.input_password.required') : __('component.input_password.not_set')} />
                     </FormRow>
+
                     <FormRow label={__("authentication.content.password")}>
-                        <InputPassword
-                            maxLength={64}
-                            value={state.password === undefined ? state.digest_hash : state.password}
-                            onValue={this.set("password")}
-                            clearPlaceholder={__("authentication.script.login_disabled")}
-                            clearSymbol={<Slash/>}
-                            allowAPIClear/>
+                        <InputPassword maxLength={64}
+                                       value={state.password}
+                                       onValue={(v) => {this.setState({password: v, digest_hash: this.hash(state.username, v)})}}
+                                       required={pass_required}
+                                       placeholder={pass_required ? __('component.input_password.required') : (API.get("authentication/config").digest_hash == "" ? __('component.input_password.not_set') : __('component.input_password.unchanged'))}
+                                       hideClear />
+                    </FormRow>
+
+                    <FormRow label="">
+                        <Button variant="danger" className="form-control" disabled={state.digest_hash == ""} onClick={async () => {
+                            const modal = util.async_modal_ref.current;
+                            if(!await modal.show({
+                                    title: __("authentication.content.disable_auth_title"),
+                                    body: __("authentication.content.disable_auth_body"),
+                                    no_text: __("authentication.content.disable_auth_abort"),
+                                    yes_text: __("authentication.content.disable_auth_confirm"),
+                                    no_variant: "secondary",
+                                    yes_variant: "danger"
+                                }))
+                                return;
+                            this.setState({
+                                enable_auth: false,
+                                username: "",
+                                password: "",
+                                digest_hash: ""}, this.save);
+                        }}>{__("authentication.content.disable_auth")}</Button>
                     </FormRow>
                 </ConfigForm>
             </>

software/src/modules/authentication/authentication.cpp
@@ -33,15 +33,16 @@ void Authentication::pre_setup()
         {"enable_auth", Config::Bool(false)},
         {"username", Config::Str("", 0, 32)},
         {"digest_hash", Config::Str("", 0, 32)},
-    }), [](Config &update) {
+    }), [this](Config &update) {
         if (update.get("enable_auth")->asBool() && update.get("digest_hash")->asString() == "")
             return String("Authentication can not be enabled if no password/digest hash is set.");
 
+        // Theoretically authentication works without a username; not in Firefox with xhr.open(..., username, password);
         if (update.get("enable_auth")->asBool() && update.get("username")->asString() == "")
             return String("Authentication can not be enabled if no username is set.");
 
-        if (!update.get("enable_auth")->asBool() && update.get("digest_hash")->asString() != "")
-            update.get("digest_hash")->updateString("");
+        if (update.get("username")->asString() != this->authentication_config.get("username")->asString() && update.get("digest_hash")->asString() == this->authentication_config.get("digest_hash")->asString())
+            return String("To change the username the digest hash also has to be updated.");
 
         return String("");
     }};

commit afcff11cf25432b85d9491ecb790729025a89dc7

authentication: Fix broken authentication when entering password first
Entering the password before the username would save
an incorrect digest, making authentication impossible.

Broken since c56f9a4.

software/web/src/modules/authentication/main.tsx
@@ -53,6 +53,12 @@ export class Authentication extends ConfigComponent<'authentication/config', {},
         return YaMD5.YaMD5.hashStr(username + ":esp32-lib:" + password);
     }
 
+    override async transformSave(cfg: AuthenticationState) {
+        cfg.digest_hash = this.hash(cfg.username, this.state.password)
+
+        return cfg;
+    }
+
     override render(props: {}, state: AuthenticationState & {password: string}) {
         if (!util.render_allowed())
             return <SubPage name="authentication" />;
@@ -81,7 +87,7 @@ export class Authentication extends ConfigComponent<'authentication/config', {},
                     <FormRow label={__("authentication.content.password")}>
                         <InputPassword maxLength={64}
                                        value={state.password}
-                                       onValue={(v) => {this.setState({password: v, digest_hash: this.hash(state.username, v)})}}
+                                       onValue={this.set("password")}
                                        required={pass_required}
                                        placeholder={pass_required ? __("component.input_password.required") : (API.get("authentication/config").digest_hash == "" ? __("component.input_password.not_set") : __("component.input_password.unchanged"))}
                                        hideClear />