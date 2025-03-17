33d62031e192482538284335758e9370161e285f
Matthias Bolte <matthias@tinkerforge.com>
2025-03-17 16:05:15 +0100

    empty: Fix undefined build_custom_app_desc symbol

--- software/src/empty.cpp
+++ software/src/empty.cpp
@@ -18,6 +18,19 @@
  */
 
 #include <Arduino.h>
+#include <stdint.h>
+
+typedef struct {
+    uint32_t magic; // CUSTOM_APP_DESC_MAGIC
+    uint8_t version; // BUILD_CUSTOM_APP_DESC_VERSION
+    uint8_t padding[3];
+    uint8_t fw_version[4]; // major, minor, patch, beta
+    uint32_t fw_build_time;
+} build_custom_app_desc_t;
+
+static_assert(sizeof(build_custom_app_desc_t) == 16, "build_custom_app_desc_t has wrong size");
+
+extern const __attribute__((section(".rodata_custom_desc"))) build_custom_app_desc_t build_custom_app_desc = {0, 0, {0, 0, 0}, {0, 0, 0, 0}, 0};
 
 void setup()
 {
