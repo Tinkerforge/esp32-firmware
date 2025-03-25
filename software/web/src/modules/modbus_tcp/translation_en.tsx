fbc2ec19ff981758bd861347d7f8bae96c05fa60
Matthias Bolte <matthias@tinkerforge.com>
2025-03-25 19:07:36 +0100

    web/modbus-tcp-debug: Add to normal web interface
    Add as debug section of the existing Modbus/TCP server subpage.
    
    The energy manager didn't have a Modbus/TCP section yet. Add one with
    just the debug section for now.

--- software/web/src/modules/modbus_tcp/translation_en.tsx
+++ software/web/src/modules/modbus_tcp/translation_en.tsx
@@ -17,6 +17,7 @@ let x = {
             "enable": "Modbus/TCP mode",
             "port": "Port",
             "port_muted": "typically 502",
+            "debug": "Debug",
             "table_docu": "WARP register documentation",
             "table_content": <>
                 <thead class="thead-light">
