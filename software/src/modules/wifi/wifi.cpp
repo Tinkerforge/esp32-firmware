Tinkerforge/esp32-firmware

commit cba0c272f26ae98112058fcd7b8937dde80eee5d

wifi: Remove softAPdisconnect call on init.
Crashes randomly. Also the AP should not be active there.

software/src/modules/wifi/wifi.cpp
@@ -652,9 +652,6 @@ void Wifi::setup()
 
     if (enable_ap && !ap_fallback_only) {
         apply_soft_ap_config_and_start();
-    } else {
-        LogSilencer ls;
-        WiFi.softAPdisconnect(true);
     }
 
     if (enable_ap) {

commit 6b8a6ca1730be13e6960e6331e8dd5cedb6f73c7

wifi: Fix open AP when fallback mode is inactive after boot
Using WiFi.mode(...) to enable the AP when fallback mode is enabled
will immediately open an unencrypted AP until the correct security
settings are configured. If the fallback mode doesn't turn on,
the unencrypted AP will stay open forever.

Disconnecting the unencrypted AP immediately after enabling it
introduces a race condition. Instead, don't enable the AP by
default because it is enabled automatically when fallback mode
turns on.

The bug was introduced in cba0c27 and not released.

software/src/modules/wifi/wifi.cpp
@@ -617,14 +617,8 @@ void Wifi::setup()
     WiFi.setHostname((String(BUILD_HOST_PREFIX) + "-" + local_uid_str).c_str());
 #endif
 
-    if (enable_sta && enable_ap) {
-        WiFi.mode(WIFI_AP_STA);
-    } else if (enable_ap) {
-        WiFi.mode(WIFI_AP);
-    } else {
-        WiFi.mode(WIFI_STA);
-    }
-
+    // STA mode is always on so that we can scan for WiFis and the HWRNG has entropy.
+    WiFi.mode(WIFI_STA);
     WiFi.setAutoReconnect(false);
 
     // Check if WiFi connected automatically and erase configuration in that case.