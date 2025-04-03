87c38d83c18ba206040d348aabeb63923a78784b
Olaf Lüke <olaf@tinkerforge.com>
2025-04-03 16:53:57 +0200

    iso15118/tools: Add open_latest bash script
    This finds the latest debug report in ~/Downloads folder,
    turns it into pcap and opens it with wireshark

---
+++ software/src/modules/iso15118/tools/open_latest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+latest=$(ls -t /home/olaf/Downloads/warp3-*Debug-Report* | head -1)
+./debug_report_to_pcap.py $latest
+base_name=$(basename ${latest})
+wireshark "${base_name%.txt}.pcap"
