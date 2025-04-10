596db2544dadabd532ee0b9e473d9fa00befe1ed
Matthias Bolte <matthias@tinkerforge.com>
2025-04-10 19:14:18 +0200

    web/batteries: Move battery config into table (same as Meters)

--- software/web/src/modules/batteries/translation_de.tsx
+++ software/web/src/modules/batteries/translation_de.tsx
@@ -17,14 +17,26 @@ let x = {
             "block_discharge_during_fast_charge": "Batterieentladung beim Schnellladen verbieten",
             "block_discharge_during_fast_charge_desc": "Verbietet dem Speicher, sich zu entladen, während ein Fahrzeug schnell lädt oder sein Eco-Ladeplan es erlaubt.",
 
-            "header_battery": "Batteriespeicher",
+            "managed_batteries": "Kontrollierte Batteriespeicher",
 
-            "battery_class": "Klasse",
-            "battery_class_none": "Keine"
+            "table_display_name": "Anzeigename",
+            "table_class": "Klasse",
+
+            "add_battery_title": "Batteriespeicher hinzufügen",
+            "add_battery_prefix": "",
+            "add_battery_infix": " von ",
+            "add_battery_suffix": " Batteriespeichern konfiguriert",
+            "add_battery_slot": "Nummer",
+            "add_battery_class": "Klasse",
+
+            "edit_battery_title": "Batteriespeicher bearbeiten",
+            "edit_battery_slot": "Nummer",
+            "edit_battery_class": "Klasse"
         },
         "script": {
             "save_failed": "Speichern der Speichersteuerungs-Einstellungen fehlgeschlagen.",
-            "reboot_content_changed": "Speichersteuerungs-Einstellungen"
+            "reboot_content_changed": "Speichersteuerungs-Einstellungen",
+            "battery": /*SFN*/(slot: number|"?") => "Speicher #" + slot/*NF*/
         },
         "automation": {
             "permit_grid_charge": "Laden des Speichers vom Netz erlauben",
--- software/web/src/modules/batteries/translation_en.tsx
+++ software/web/src/modules/batteries/translation_en.tsx
@@ -17,14 +17,26 @@ let x = {
             "block_discharge_during_fast_charge": "Forbid discharge during fast charge",
             "block_discharge_during_fast_charge_desc": "Will forbid the battery to discharge while a vehicle is fast charging or is allowed to do so by its Eco charge plan.",
 
-            "header_battery": "Battery",
+            "managed_batteries": "Managed battery storages",
 
-            "battery_class": "Class",
-            "battery_class_none": "None"
+            "table_display_name": "Display name",
+            "table_class": "Class",
+
+            "add_battery_title": "Add battery storage",
+            "add_battery_prefix": "",
+            "add_battery_infix": " of ",
+            "add_battery_suffix": " battery storages configured",
+            "add_battery_slot": "Number",
+            "add_battery_class": "Class",
+
+            "edit_battery_title": "Edit battery storage",
+            "edit_battery_slot": "Number",
+            "edit_battery_class": "Class"
         },
         "script": {
             "save_failed": "Failed to save the battery control settings.",
-            "reboot_content_changed": "battery control settings"
+            "reboot_content_changed": "battery control settings",
+            "battery": /*SFN*/(slot: number|"?") => "Battery #" + slot/*NF*/
         },
         "automation": {
             "permit_grid_charge": "Permit battery grid charge",
