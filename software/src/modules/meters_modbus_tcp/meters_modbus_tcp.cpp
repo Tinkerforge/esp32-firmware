21dbcdc3dfbe47307355a7a356181c284986bfa7
Matthias Bolte <matthias@tinkerforge.com>
2025-04-15 17:59:38 +0200

    meters-modbus-tcp: Fix typo
    Both enums map None to the same value, no functional change.

--- software/src/modules/meters_modbus_tcp/meters_modbus_tcp.cpp
+++ software/src/modules/meters_modbus_tcp/meters_modbus_tcp.cpp
@@ -177,12 +177,12 @@ void MetersModbusTCP::pre_setup()
     table_prototypes.push_back({MeterModbusTCPTableID::TinkerforgeWARPCharger, *Config::Null()});
 
     table_prototypes.push_back({MeterModbusTCPTableID::SAXPowerHomeBasicMode, Config::Object({
-        {"virtual_meter", Config::Enum(SolaredgeInverterVirtualMeter::None)},
+        {"virtual_meter", Config::Enum(SAXPowerVirtualMeter::None)},
         {"device_address", Config::Uint(64, 1, 247)},
     })});
 
     table_prototypes.push_back({MeterModbusTCPTableID::SAXPowerHomeExtendedMode, Config::Object({
-        {"virtual_meter", Config::Enum(SolaredgeInverterVirtualMeter::None)},
+        {"virtual_meter", Config::Enum(SAXPowerVirtualMeter::None)},
         {"device_address", Config::Uint(40, 1, 247)},
     })});
 
