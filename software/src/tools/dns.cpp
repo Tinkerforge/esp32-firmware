be76d98336eabccfaac209abc7a862a7419adf1c
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-05-22 19:26:53 +0200

    tools/dns: Add shortcuts for hostname lookups for raw IPs
    Detect and immediately fulfill lookup requests for raw IPs,
    instead of making a full round trip through lwIP.

--- software/src/tools/dns.cpp
+++ software/src/tools/dns.cpp
@@ -18,6 +18,8 @@
  */
 
 #include <esp_netif.h>
+#include <lwip/dns.h>
+#include <lwip/ip_addr.h>
 
 #include "dns.h"
 #include "main_dependencies.h"
@@ -40,6 +42,10 @@ static esp_err_t gethostbyname_lwip_ctx(void *ctx)
 
 err_t dns_gethostbyname_lwip_ctx(const char *host, ip_addr_t *addr, dns_found_callback found_callback, void *callback_arg)
 {
+    if (ipaddr_aton(host, addr)) {
+        return ERR_OK;
+    }
+
     gethostbyname_parameters parameters;
     parameters.host = host;
     parameters.addr = addr;
@@ -47,6 +53,27 @@ err_t dns_gethostbyname_lwip_ctx(const char *host, ip_addr_t *addr, dns_found_ca
     parameters.callback_arg = callback_arg;
 
     return static_cast<err_t>(esp_netif_tcpip_exec(gethostbyname_lwip_ctx, &parameters));
+}
+
+static int ipaddr_aton_addrtype(const char *host, ip_addr_t *addr, u8_t dns_addrtype)
+{
+    if (dns_addrtype == IPADDR_TYPE_V4) {
+        const int success = ip4addr_aton(host, &addr->u_addr.ip4);
+        if (success) {
+            addr->type = IPADDR_TYPE_V4;
+        }
+        return success;
+    }
+    if (dns_addrtype == IPADDR_TYPE_V6) {
+        const int success = ip6addr_aton(host, &addr->u_addr.ip6);
+        if (success) {
+            addr->type = IPADDR_TYPE_V6;
+        }
+        return success;
+    }
+
+    // If neither only IPv4 or IPv6 addresses were requested, accept any valid IP address.
+    return ipaddr_aton(host, addr);
 }
 
 static esp_err_t gethostbyname_addrtype_lwip_ctx(void *ctx)
@@ -57,6 +84,10 @@ static esp_err_t gethostbyname_addrtype_lwip_ctx(void *ctx)
 
 err_t dns_gethostbyname_addrtype_lwip_ctx(const char *host, ip_addr_t *addr, dns_found_callback found_callback, void *callback_arg, u8_t dns_addrtype)
 {
+    if (ipaddr_aton_addrtype(host, addr, dns_addrtype)) {
+        return ERR_OK;
+    }
+
     gethostbyname_parameters parameters;
     parameters.host = host;
     parameters.addr = addr;
@@ -92,15 +123,23 @@ void dns_gethostbyname_addrtype_lwip_ctx_async(const char *host,
                                                u8_t dns_addrtype)
 {
     callback_arg->found_callback = std::move(found_callback);
-    err_t err = dns_gethostbyname_addrtype_lwip_ctx(host, &callback_arg->addr, gethostbyname_addrtype_lwip_ctx_async, callback_arg, dns_addrtype);
 
-    // Don't set the callback_arg's err if the result is not available yet.
-    // The callback handler might be executed before dns_gethostbyname_addrtype_lwip_ctx returns.
-    if (err == ERR_INPROGRESS)
-        return;
+    const int is_ip = ipaddr_aton_addrtype(host, &callback_arg->addr, dns_addrtype);
 
-    callback_arg->err = err;
+    if (is_ip) {
+        callback_arg->err = ERR_OK;
+    } else {
+        err_t err = dns_gethostbyname_addrtype_lwip_ctx(host, &callback_arg->addr, gethostbyname_addrtype_lwip_ctx_async, callback_arg, dns_addrtype);
+
+        // Don't set the callback_arg's err if the result is not available yet.
+        // The callback handler might be executed before dns_gethostbyname_addrtype_lwip_ctx returns.
+        if (err == ERR_INPROGRESS)
+            return;
+
+        callback_arg->err = err;
+    }
+
     callback_arg->addr_ptr = &callback_arg->addr;
 
-    callback_arg->found_callback(callback_arg); // Can't call local found_callback anymore because it has been std::forward'ed.
+    callback_arg->found_callback(callback_arg); // Can't call local found_callback anymore because it has been std::move'd.
 }
