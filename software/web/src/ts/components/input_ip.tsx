2c2cf15b06d53d420e4b3dde5a771ccdb59df29c
Matthias Bolte <matthias@tinkerforge.com>
2025-05-23 11:17:57 +0200

    web: Don't allow entering static IP addresses with leading zeros
    Leading zeros result in unwanted octal interpretation.

--- software/web/src/ts/components/input_ip.tsx
+++ software/web/src/ts/components/input_ip.tsx
@@ -38,7 +38,7 @@ export function InputIP(props: InputIPProps) {
                minLength={7}
                maxLength={15}
                size={15}
-               pattern="^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+               pattern="^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)$"
                onInput={(e) => props.onValue((e.target as HTMLInputElement).value)}
                {...props}/>
         <div class="invalid-feedback">{props.invalidFeedback}</div>
