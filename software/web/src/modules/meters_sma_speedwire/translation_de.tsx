5cde8a48dd42e81a8f69041d0ec117b2760134e6
Matthias Bolte <matthias@tinkerforge.com>
2025-05-28 15:21:36 +0200

    web: Fix typo

--- software/web/src/modules/meters_sma_speedwire/translation_de.tsx
+++ software/web/src/modules/meters_sma_speedwire/translation_de.tsx
@@ -8,7 +8,7 @@ let x = {
             "config_display_name": "Anzeigename",
             "config_serial_number": "Seriennummer",
             "config_serial_number_muted": "optional",
-            "config_serial_number_invalid": "Muss leer, oder eine Zahl zwischen 1 und 4294967295.",
+            "config_serial_number_invalid": "Muss leer, oder eine Zahl zwischen 1 und 4294967295 sein.",
             "config_location": "Messort"
         }
     }
