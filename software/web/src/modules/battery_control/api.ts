f7a3d06879baf4f02d1691ca40db55df0d2a869e
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-05-28 15:55:54 +0200

    battery_control: Add control rule API

--- software/web/src/modules/battery_control/api.ts
+++ software/web/src/modules/battery_control/api.ts
@@ -2,6 +2,18 @@ export interface config {
     block_discharge_during_fast_charge: boolean;
 }
 
+interface RuleConfig {
+    soc_cond:      number;
+    soc_th:        number;
+    price_cond:    number;
+    price_th:      number;
+    forecast_cond: number;
+    forecast_th:   number;
+}
+
+export type rules_forbid_discharge   = RuleConfig[];
+export type rules_permit_grid_charge = RuleConfig[];
+
 export interface low_level_config {
     rewrite_period: number;
 }
