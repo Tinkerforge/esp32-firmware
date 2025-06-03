89f572570af5e77a7d7075684b3ea67ad2628dee
Matthias Bolte <matthias@tinkerforge.com>
2025-06-03 13:18:14 +0200

    batteries-modbus-tcp: Test what is in the frontend, not what is saved
    Before the test buttons in the frontend would execute the saved actions.
    This is not very user friendly. Instead execute the action that is
    currently visible in the frontend.
    
    Also show the response from the action for debugging.

--- software/src/modules/batteries_modbus_tcp/batteries_modbus_tcp.cpp
+++ software/src/modules/batteries_modbus_tcp/batteries_modbus_tcp.cpp
@@ -21,6 +21,8 @@
 
 #include "batteries_modbus_tcp.h"
 
+#include <TFJson.h>
+
 #include "battery_modbus_tcp.h"
 #include "event_log_prefix.h"
 #include "module_dependencies.h"
@@ -28,6 +30,29 @@
 #include "tools.h"
 
 #include "gcc_warnings.h"
+
+BatteriesModbusTCP::ValueTable *BatteriesModbusTCP::read_table_config(const Config *config)
+{
+    ValueTable *table = new ValueTable;
+
+    table->device_address = static_cast<uint8_t>(config->get("device_address")->asUint());
+
+    const Config *registers_config = static_cast<const Config *>(config->get("registers"));
+    size_t registers_count         = registers_config->count();
+
+    ValueSpec *values = new ValueSpec[registers_count];
+
+    for (size_t i = 0; i < registers_count; ++i) {
+        values[i].register_type = registers_config->get(i)->get("rtype")->asEnum<ModbusRegisterType>();
+        values[i].start_address = static_cast<uint16_t>(registers_config->get(i)->get("addr")->asUint());
+        values[i].value         = static_cast<uint16_t>(registers_config->get(i)->get("value")->asUint());
+    }
+
+    table->values = values;
+    table->values_length = registers_count;
+
+    return table;
+}
 
 void BatteriesModbusTCP::pre_setup()
 {
@@ -97,7 +122,124 @@ void BatteriesModbusTCP::pre_setup()
         {"timeout", Config::Uint32(0)},
     });*/
 
+    execute_table_prototypes.push_back({BatteryModbusTCPTableID::Custom, Config::Object({
+        {"device_address", Config::Uint8(1)},
+        {"registers", Config::Array({},
+            &table_custom_registers_prototype,
+            0,
+            BATTERIES_MODBUS_TCP_MAX_CUSTOM_REGISTERS,
+            Config::type_id<Config::ConfObject>()
+        )},
+    })});
+
+    execute_config = ConfigRoot{Config::Object({
+        {"host", Config::Str("", 0, 64)},
+        {"port", Config::Uint16(502)},
+        {"table", Config::Union<BatteryModbusTCPTableID>(*Config::Null(),
+            BatteryModbusTCPTableID::None,
+            execute_table_prototypes.data(),
+            static_cast<uint8_t>(execute_table_prototypes.size())
+        )},
+        {"cookie", Config::Uint32(0)},
+    })};
+
     batteries.register_battery_generator(get_class(), this);
+}
+
+[[gnu::format(__printf__, 2, 3)]] static void report_errorf(uint32_t cookie, const char *fmt, ...);
+static void report_errorf(uint32_t cookie, const char *fmt, ...)
+{
+    va_list args;
+    char buf[256];
+    TFJsonSerializer json{buf, sizeof(buf)};
+
+    json.addObject();
+    json.addMemberNumber("cookie", cookie);
+    va_start(args, fmt);
+    json.addMemberStringVF("error", fmt, args);
+    va_end(args);
+    json.endObject();
+    json.end();
+
+    ws.pushRawStateUpdate(buf, "batteries_modbus_tcp/execute_result");
+}
+
+static void report_success(uint32_t cookie)
+{
+    char buf[256];
+    TFJsonSerializer json{buf, sizeof(buf)};
+
+    json.addObject();
+    json.addMemberNumber("cookie", cookie);
+    json.addMemberNull("error");
+    json.endObject();
+    json.end();
+
+    ws.pushRawStateUpdate(buf, "batteries_modbus_tcp/execute_result");
+}
+
+void BatteriesModbusTCP::register_urls()
+{
+    api.addCommand("batteries_modbus_tcp/execute", &execute_config, {}, [this](String &errmsg) {
+        uint32_t cookie = execute_config.get("cookie")->asUint();
+
+        if (execute_client != nullptr) {
+            report_errorf(cookie, "Another action is already in progress");
+            return;
+        }
+
+        BatteryModbusTCPTableID table_id = execute_config.get("table")->getTag<BatteryModbusTCPTableID>();
+
+        switch (table_id) {
+        case BatteryModbusTCPTableID::None:
+            report_errorf(cookie, "No table");
+            return;
+
+        case BatteryModbusTCPTableID::Custom:
+            break;
+
+        default:
+            report_errorf(cookie, "Unknown table: %u", static_cast<uint8_t>(table_id));
+            return;
+        }
+
+        String host = execute_config.get("host")->asString();
+        uint16_t port = static_cast<uint16_t>(execute_config.get("port")->asUint());
+        const Config *table = static_cast<const Config *>(execute_config.get("table")->get());
+
+        execute_cookie = cookie;
+        execute_table = BatteriesModbusTCP::read_table_config(table);
+
+        modbus_tcp_client.get_pool()->acquire(host.c_str(), port,
+        [this, cookie, host, port](TFGenericTCPClientConnectResult connect_result, int error_number, TFGenericTCPSharedClient *shared_client) {
+            if (connect_result != TFGenericTCPClientConnectResult::Connected) {
+                char connect_error[256] = "";
+
+                GenericTCPClientConnectorBase::format_connect_error(connect_result, error_number, host.c_str(), port, connect_error, sizeof(connect_error));
+                report_errorf(cookie, "%s", connect_error);
+
+                delete execute_table;
+                execute_table = nullptr;
+
+                return;
+            }
+
+            execute_client = shared_client;
+            current_execute_index = 0;
+
+            write_next();
+        },
+        [this](TFGenericTCPClientDisconnectReason reason, int error_number, TFGenericTCPSharedClient *shared_client) {
+            if (execute_client != shared_client) {
+                return;
+            }
+
+            execute_client = nullptr;
+
+            delete execute_table;
+            execute_table = nullptr;
+        });
+    }, true);
 }
 
 [[gnu::const]] BatteryClassID BatteriesModbusTCP::get_class() const
@@ -123,4 +265,78 @@ const Config *BatteriesModbusTCP::get_state_prototype()
 const Config *BatteriesModbusTCP::get_errors_prototype()
 {
     return Config::Null();//&errors_prototype;
+}
+
+void BatteriesModbusTCP::write_next()
+{
+    if (execute_table == nullptr) {
+        return;
+    }
+
+    if (execute_client == nullptr) {
+        // FIXME: maybe retry if connection is lost in the middle of an action, instead of aborting
+        return;
+    }
+
+    if (current_execute_index >= execute_table->values_length) {
+        report_success(execute_cookie);
+        release_client(); // execution is done
+        return;
+    }
+
+    const ValueSpec *spec = &execute_table->values[current_execute_index];
+    TFModbusTCPFunctionCode function_code;
+
+    switch (spec->register_type) {
+    case ModbusRegisterType::HoldingRegister:
+        function_code = TFModbusTCPFunctionCode::WriteMultipleRegisters;
+        break;
+
+    case ModbusRegisterType::Coil:
+        function_code = TFModbusTCPFunctionCode::WriteMultipleCoils;
+        break;
+
+    case ModbusRegisterType::InputRegister:
+    case ModbusRegisterType::DiscreteInput:
+    default:
+        esp_system_abort("batteries_modbus_tcp: Unsupported register type to write.");
+    }
+
+    static_cast<TFModbusTCPSharedClient *>(execute_client)->transact(execute_table->device_address,
+                                                                     function_code,
+                                                                     spec->start_address,
+                                                                     1,
+                                                                     const_cast<uint16_t *>(&spec->value),
+                                                                     2_s,
+    [this, spec, function_code](TFModbusTCPClientTransactionResult result) {
+        if (result != TFModbusTCPClientTransactionResult::Success) {
+            report_errorf(execute_cookie, "Execution failed at value %zu of %zu: %s (%d)",
+                          current_execute_index, execute_table->values_length,
+                          get_tf_modbus_tcp_client_transaction_result_name(result),
+                          static_cast<int>(result));
+
+            // FIXME: maybe retry on error in the middle of an action, instead of aborting
+            release_client();
+            return;
+        }
+
+        ++current_execute_index;
+
+        if (current_execute_index >= execute_table->values_length) {
+            report_success(execute_cookie);
+            release_client(); // execution is done
+            return;
+        }
+
+        write_next(); // FIXME: maybe add a little delay between writes to avoid bursts?
+    });
+}
+
+void BatteriesModbusTCP::release_client()
+{
+    task_scheduler.scheduleOnce([this]() {
+        if (execute_client != nullptr) {
+            modbus_tcp_client.get_pool()->release(execute_client);
+        }
+    });
 }
