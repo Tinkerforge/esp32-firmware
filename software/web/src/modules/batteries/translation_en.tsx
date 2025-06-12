55ed4c13c39b6b89244142f9bae9bb0872eaf47f
Matthias Bolte <matthias@tinkerforge.com>
2025-06-12 16:51:16 +0200

    batteries: Add rule frontend

--- software/web/src/modules/batteries/translation_en.tsx
+++ software/web/src/modules/batteries/translation_en.tsx
@@ -24,8 +24,8 @@ let x = {
 
             "managed_batteries": "Managed battery storages",
 
-            "table_display_name": "Display name",
-            "table_class": "Class",
+            "table_battery_display_name": "Display name",
+            "table_battery_class": "Class",
 
             "add_battery_title": "Add battery storage",
             "add_battery_message": /*SFN*/(have: number, max: number) => `${have} of ${max} battery storages configured`/*NF*/,
@@ -40,7 +40,33 @@ let x = {
             "battery_import_error_class_unknown": "Class is unknown.",
             "battery_import_error_import_unsupported": "Import is not supported by this class.",
             "battery_import_error_config_malformed": "Config is malformed.",
-            "battery_export_basename": "battery-storage-config"
+            "battery_export_basename": "battery-storage-config",
+
+            "rules_permit_grid_charge": "Rules for charge from grid permission",
+            "rules_forbid_discharge": "Rules for battery discharge forbiddance",
+            "rules_forbid_charge": "Rules for battery charge forbiddance",
+
+            "table_rule_desc": "Description",
+            "table_rule_soc": "State of charge",
+            "table_rule_price": "Dynamic tariff",
+            "table_rule_forecast": "Solar forecast",
+
+            "add_rule_title": "Add rule",
+            "add_rule_message": /*SFN*/(have: number, max: number) => `${have} of ${max} rules configured`/*NF*/,
+            "add_rule_desc": "Description",
+            "add_rule_soc": "State of charge",
+            "add_rule_price": "Dynamic tariff",
+            "add_rule_forecast": "Solar forecast",
+
+            "edit_rule_title": "Edit rule",
+            "edit_rule_desc": "Description",
+            "edit_rule_soc": "State of charge",
+            "edit_rule_price": "Dynamic tariff",
+            "edit_rule_forecast": "Solar forecast",
+
+            "condition_ignore": "Is ignored",
+            "condition_below": "Smaller than",
+            "condition_above": "Greater than"
         },
         "script": {
             "save_failed": "Failed to save the battery control settings.",
