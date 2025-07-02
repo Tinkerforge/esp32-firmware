30c0f03b151ce17907804fd00a9811a8aad56e63
Julius Dill <dilljulius@gmail.com>
2025-07-02 11:25:56 +0200

    eebus: Restructuring sending logic in preparation for eebus subscriptions

--- software/src/modules/eebus/spine_types.h
+++ software/src/modules/eebus/spine_types.h
@@ -20832,5 +20832,6 @@ class SpineDataTypeHandler {
         Function function_from_string(const String function_name);
         Function handle_cmd(JsonObjectConst obj);
         String function_to_string(Function function);
+        void last_cmd_to_json(JsonVariant &dst);
 };
     
