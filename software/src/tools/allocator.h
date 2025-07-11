6ae35d1feefad1a2b115e4ccb9a38b6b19629893
Julius Dill <dilljulius@gmail.com>
2025-07-11 13:23:16 +0200

    tools/allocator.h: Add Custom allocator for JsonDocuments

--- software/src/tools/allocator.h
+++ software/src/tools/allocator.h
@@ -102,3 +102,22 @@ template <class T, class U>
 bool operator==(const IRAMAlloc<T>&, const IRAMAlloc<U>&) { return true; }
 template <class T, class U>
 bool operator!=(const IRAMAlloc<T>&, const IRAMAlloc<U>&) { return false; }
+
+
+
+struct ArduinoJsonPsramAllocator
+{
+    void *allocate(size_t n)
+    {
+        return malloc_psram_or_dram(n);
+    }
+    void deallocate(void *p)
+    {
+        free_any(p);
+    }
+    void *reallocate(void *p, size_t n)
+    {
+        return heap_caps_realloc(p, n, MALLOC_CAP_SPIRAM);
+    }
+};
+using JsonDocumentPsram = DynamicJsonDocument<ArduinoJsonPsramAllocator>;
