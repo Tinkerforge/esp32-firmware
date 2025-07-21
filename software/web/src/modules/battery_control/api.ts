219145b09d4e629d18e1228ea5dd5174f662b10d
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-07-21 20:37:22 +0200

    batteries, battery_control: Add dynamic tariff schedule
    Add creating a schedule and using it in rules for battery storage control.

--- software/web/src/modules/battery_control/api.ts
+++ software/web/src/modules/battery_control/api.ts
@@ -2,6 +2,8 @@ import { RuleConfig } from "./types";
 
 export interface config {
     forbid_discharge_during_fast_charge: boolean;
+    cheap_tariff_quarters: number;
+    expensive_tariff_quarters: number;
 }
 
 export type rules_permit_grid_charge = RuleConfig[];
