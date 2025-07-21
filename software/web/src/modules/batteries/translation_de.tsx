ae0a562bebd94e05001731b5c5c1cc8ee199521c
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-07-21 20:37:22 +0200

    batteries, battery_control: Add dynamic tariff schedule
    Add creating a schedule and using it in rules for battery storage control.

--- software/web/src/modules/batteries/translation_de.tsx
+++ software/web/src/modules/batteries/translation_de.tsx
@@ -19,6 +19,13 @@ let x = {
             "discharge_forbidden": "Batterieentladung",
             "charge_forbidden": "Batterieladung",
 
+            "dynamic_tariff_schedule": "Zeitplan für dynamischen Strompreis",
+            "schedule_cheap_hours": "Günstige Stunden",
+            "schedule_cheap_hours_help": "Wähle die x günstigsten Stunden in einem 24-Stunden-Zeitfenster zwischen 20:00 und 20:00 des Folgetages aus. Wenn der Batteriespeicher aus dem Netz geladen werden soll, ist es sinnvoll, hier die Ladezeit des Speichers einzutragen. Viertelstunden können eingegeben werden, indem Vielfache von 0,25 eingegeben werden. 1,25 h ist eine Stunde und 15 Minuten.",
+            "schedule_expensive_hours": "Teure Stunden",
+            "schedule_expensive_hours_help": "Wähle die x teuersten Stunden in einem 24-Stunden-Zeitfenster zwischen 20:00 und 20:00 des Folgetages aus. Viertelstunden können eingegeben werden, indem Vielfache von 0,25 eingegeben werden. 1,25 h ist eine Stunde und 15 Minuten.",
+            "schedule_hours_muted": "in 0,25 h-Schritten",
+
             "forbid_discharge_during_fast_charge": "Batterieentladung beim Schnellladen verbieten",
             "forbid_discharge_during_fast_charge_desc": "Verbietet dem Speicher, sich zu entladen, während ein Fahrzeug schnell lädt oder sein Eco-Ladeplan es erlaubt.",
 
@@ -50,6 +57,7 @@ let x = {
             "table_rule_soc": "Batterieladestand",
             "table_rule_price": "Dynamischer Strompreis",
             "table_rule_forecast": "PV-Ertragsprognose",
+            "table_rule_schedule": "Dynamischer Strompreisplan",
 
             "add_rule_title": "Regel hinzufügen",
             "add_rule_message": /*SFN*/(have: number, max: number) => `${have} von ${max} Regeln konfiguriert`/*NF*/,
@@ -57,16 +65,26 @@ let x = {
             "add_rule_soc": "Batterieladestand",
             "add_rule_price": "Dynamischer Strompreis",
             "add_rule_forecast": "PV-Ertragsprognose",
+            "add_rule_schedule": "Dynamischer Strompreisplan",
 
             "edit_rule_title": "Regel bearbeiten",
             "edit_rule_desc": "Beschreibung",
             "edit_rule_soc": "Batterieladestand",
             "edit_rule_price": "Dynamischer Strompreis",
             "edit_rule_forecast": "PV-Ertragsprognose",
+            "edit_rule_schedule": "Dynamischer Strompreisplan",
 
             "condition_ignore": "Wird ignoriert",
             "condition_below": "Kleiner als",
-            "condition_above": "Größer als"
+            "condition_above": "Größer als",
+
+            "condition_schedule_cheap": "Günstige Stunden",
+            "condition_schedule_not_cheap": "Nicht günstige Stunden",
+            "condition_schedule_expensive": "Teure Stunden",
+            "condition_schedule_not_expensive": "Nicht teure Stunden",
+            "condition_schedule_moderate": "Preislich moderate Stunden (weder günstig noch teuer)",
+
+            "invalid_feedback_all_ignored": "Es können nicht alle Bedingungen gleichzeitig ignoriert werden."
         },
         "script": {
             "save_failed": "Speichern der Speichersteuerungs-Einstellungen fehlgeschlagen.",
