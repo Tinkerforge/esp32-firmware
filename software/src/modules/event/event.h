99ee92a93dac42d509f44d6e21fa04ae702db62e
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-07-21 20:26:12 +0200

    event: Add API backend flag helper
    Allow retrieving the event module's API backend flag so that other modules can check the "updated" state of sub-configs.

--- software/src/modules/event/event.h
+++ software/src/modules/event/event.h
@@ -49,6 +49,8 @@ public:
     int64_t registerEvent(const String &path, const std::vector<ConfPath> values, std::function<EventResult(const Config *)> &&callback);
     void deregisterEvent(int64_t eventID);
 
+    inline uint8_t get_api_backend_flag() { return api_backend_flag; }
+
     // IAPIBackend implementation
     void addCommand(size_t commandIdx, const CommandRegistration &reg) override;
     void addState(size_t stateIdx, const StateRegistration &reg) override;
