2bbe2211faf117047c08bea89e839d5ead9cae1b
Matthias Bolte <matthias@tinkerforge.com>
2025-08-01 10:14:51 +0200

    build: Avoid mixed path separators

--- software/merge_firmware_hook.py
+++ software/merge_firmware_hook.py
@@ -12,18 +12,18 @@ import re
 import shutil
 import subprocess
 
-with open(os.path.join(env.subst('$BUILD_DIR'), 'firmware_basename'), 'r', encoding='utf-8') as f:
+with open(env.subst(f'$BUILD_DIR{os.sep}firmware_basename'), 'r', encoding='utf-8') as f:
     firmware_basename = f.read().strip()
 
 product_id = env.GetProjectOption('custom_product_id')
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
     env.VerboseAction(lambda **kwargs: os.makedirs("build", exist_ok=True), "Ensuring build directory exists")
 )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
     env.VerboseAction(lambda **kwargs: os.makedirs("build_latest", exist_ok=True), "Ensuring build_latest directory exists")
 )
 
@@ -31,7 +31,7 @@ if env.GetProjectOption("custom_autoclean_build_dir", default="false") == "true"
     firmware_name = product_id + "_firmware"
 
     def delete_old(fwname):
-        fileList = glob.glob('build/{}*'.format(fwname))
+        fileList = glob.glob(f'build{os.sep}{fwname}*')
         for filePath in fileList:
             try:
                 os.remove(filePath)
@@ -40,7 +40,7 @@ if env.GetProjectOption("custom_autoclean_build_dir", default="false") == "true"
         return None
 
     env.AddPostAction(
-        "$BUILD_DIR/${PROGNAME}.elf",
+        f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
         env.VerboseAction(lambda **kwargs: delete_old(firmware_name),
                           "Cleaning old {}*".format(firmware_name))
     )
@@ -50,9 +50,9 @@ def copy2(src, dst): # hide shutil.copy2 return value
     return None
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
-    env.VerboseAction(lambda env, **kwargs: copy2(env.subst("$BUILD_DIR/${PROGNAME}.elf"),
-                                                  f"build/{firmware_basename}.elf"),
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
+    env.VerboseAction(lambda env, **kwargs: copy2(env.subst(f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf"),
+                                                  f"build{os.sep}{firmware_basename}.elf"),
                       f"Copying $BUILD_DIR{os.sep}${{PROGNAME}}.elf -> build{os.sep}{firmware_basename}.elf")
 )
 
@@ -71,24 +71,24 @@ def remove_and_symlink(remove_pattern, src_path, dst_path):
     return symlink(src_path, dst_path)
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
-    env.VerboseAction(lambda env, **kwargs: remove_and_symlink(f"build_latest/{product_id}_firmware*.elf",
-                                                               f"../build/{firmware_basename}.elf",
-                                                               f"build_latest/{firmware_basename}.elf"),
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
+    env.VerboseAction(lambda env, **kwargs: remove_and_symlink(f"build_latest{os.sep}{product_id}_firmware*.elf",
+                                                               f"..{os.sep}build{os.sep}{firmware_basename}.elf",
+                                                               f"build_latest{os.sep}{firmware_basename}.elf"),
                       f"Symlinking build{os.sep}{firmware_basename}.elf -> build_latest{os.sep}{firmware_basename}.elf")
 )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
     env.VerboseAction(lambda env, **kwargs: symlink(f"{firmware_basename}.elf",
-                                                    f"build/{product_id}_firmware_latest.elf"),
+                                                    f"build{os.sep}{product_id}_firmware_latest.elf"),
                       f"Symlinking build{os.sep}{firmware_basename}.elf -> build{os.sep}{product_id}_firmware_latest.elf")
 )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.elf",
     env.VerboseAction(lambda env, **kwargs: symlink(f"{firmware_basename}.elf",
-                                                    f"build/firmware_latest.elf"),
+                                                    f"build{os.sep}firmware_latest.elf"),
                       f"Symlinking build{os.sep}{firmware_basename}.elf -> build{os.sep}firmware_latest.elf")
 )
 
@@ -97,20 +97,20 @@ def check_call(*args): # hide subprocess.check_call return value
     return None
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.bin",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
     env.VerboseAction(lambda env, **kwargs: check_call(
         env.subst('$PYTHONEXE'),
         "-u",
-        env.subst("$PROJECT_PACKAGES_DIR/tool-esptoolpy/esptool.py"),
+        env.subst(f"$PROJECT_PACKAGES_DIR{os.sep}tool-esptoolpy{os.sep}esptool.py"),
         "--chip", "esp32",
         "merge-bin",
-        "-o", env.subst("$BUILD_DIR/${PROGNAME}_merged.bin"),
+        "-o", env.subst(f"$BUILD_DIR{os.sep}${{PROGNAME}}_merged.bin"),
         "--target-offset", "0x1000",
-        "0x1000", env.subst("$BUILD_DIR/bootloader.bin"),
-        "0x8000", env.subst("$BUILD_DIR/partitions.bin"),
-        "0xd000", env.subst("$BUILD_DIR/firmware_info.bin"),
+        "0x1000", env.subst(f"$BUILD_DIR{os.sep}bootloader.bin"),
+        "0x8000", env.subst(f"$BUILD_DIR{os.sep}partitions.bin"),
+        "0xd000", env.subst(f"$BUILD_DIR{os.sep}firmware_info.bin"),
         "0xe000", "boot_app0.bin",
-        "0x10000", env.subst("$BUILD_DIR/${PROGNAME}.bin")
+        "0x10000", env.subst(f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin")
     ), "Merging {0}.bin -> {0}_merged.bin".format(env.subst(f"$BUILD_DIR{os.sep}$PROGNAME")))
 )
 
@@ -118,42 +118,42 @@ signature_preset = env.GetProjectOption('custom_signature_preset')
 
 if len(signature_preset) > 0:
     env.AddPostAction(
-        "$BUILD_DIR/${PROGNAME}.bin",
+        f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
         env.VerboseAction(lambda env, **kwargs: check_call(
             env.subst('$PYTHONEXE'),
             "-u",
-            env.subst("$PROJECT_DIR/signature/sign.py"),
+            env.subst(f"$PROJECT_DIR{os.sep}signature{os.sep}sign.py"),
             signature_preset,
-            env.subst("$BUILD_DIR/${PROGNAME}_merged.bin"),
-            f"build/{firmware_basename}_merged.bin"
+            env.subst(f"$BUILD_DIR{os.sep}${{PROGNAME}}_merged.bin"),
+            f"build{os.sep}{firmware_basename}_merged.bin"
         ), f"Signing $BUILD_DIR{os.sep}${{PROGNAME}}_merged.bin -> build{os.sep}{firmware_basename}_merged.bin")
     )
 else:
     env.AddPostAction(
-        "$BUILD_DIR/${PROGNAME}.bin",
-        env.VerboseAction(lambda env, **kwargs: copy2(env.subst("$BUILD_DIR/${PROGNAME}_merged.bin"),
-                                                      f"build/{firmware_basename}_merged.bin"),
+        f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
+        env.VerboseAction(lambda env, **kwargs: copy2(env.subst(f"$BUILD_DIR{os.sep}${{PROGNAME}}_merged.bin"),
+                                                      f"build{os.sep}{firmware_basename}_merged.bin"),
                           f"Copying $BUILD_DIR{os.sep}${{PROGNAME}}_merged.bin -> build{os.sep}{firmware_basename}_merged.bin")
     )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.bin",
-    env.VerboseAction(lambda env, **kwargs: remove_and_symlink(f"build_latest/{product_id}_firmware*_merged.bin",
-                                                               f"../build/{firmware_basename}_merged.bin",
-                                                               f"build_latest/{firmware_basename}_merged.bin"),
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
+    env.VerboseAction(lambda env, **kwargs: remove_and_symlink(f"build_latest{os.sep}{product_id}_firmware*_merged.bin",
+                                                               f"..{os.sep}build{os.sep}{firmware_basename}_merged.bin",
+                                                               f"build_latest{os.sep}{firmware_basename}_merged.bin"),
                       f"Symlinking build{os.sep}{firmware_basename}_merged.bin -> build_latest{os.sep}{firmware_basename}_merged.bin")
 )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.bin",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
     env.VerboseAction(lambda env, **kwargs: symlink(f"{firmware_basename}_merged.bin",
-                                                    f"build/{product_id}_firmware_latest_merged.bin"),
+                                                    f"build{os.sep}{product_id}_firmware_latest_merged.bin"),
                       f"Symlinking build{os.sep}{firmware_basename}_merged.bin -> build{os.sep}{product_id}_firmware_latest_merged.bin")
 )
 
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.bin",
+    f"$BUILD_DIR{os.sep}${{PROGNAME}}.bin",
     env.VerboseAction(lambda env, **kwargs: symlink(f"{firmware_basename}_merged.bin",
-                                                    f"build/firmware_latest_merged.bin"),
+                                                    f"build{os.sep}firmware_latest_merged.bin"),
                       f"Symlinking build{os.sep}{firmware_basename}_merged.bin -> build{os.sep}firmware_latest_merged.bin")
 )
