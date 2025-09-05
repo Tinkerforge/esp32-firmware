34a225a41b9d22e0e3e8640f2f6bf5b3986604a1
Erik Fleckstein <erik@tinkerforge.com>
2025-09-05 12:36:51 +0200

    charge_manager: Use Config::Timestamp in decisions.

--- software/src/modules/charge_manager/Global Decision.union.py
+++ software/src/modules/charge_manager/Global Decision.union.py
@@ -7,13 +7,13 @@ from software.util import *
 spec = Union("Global Decision", 16, variants=[
     Variant('None'),
     Variant('PV Excess Overloaded Hysteresis Blocks Until', [
-        Member('Timestamp', Types.Seconds),
+        Member('Timestamp', Types.Timestamp),
         Member('Overload mA', Types.U32),
     ]),
     Variant('Hysteresis Elapses At', [
-        Member('Timestamp', Types.Seconds)
+        Member('Timestamp', Types.Timestamp)
     ]),
     Variant('Next Rotation At', [
-        Member('Timestamp', Types.Seconds)
+        Member('Timestamp', Types.Timestamp)
     ]),
 ])
