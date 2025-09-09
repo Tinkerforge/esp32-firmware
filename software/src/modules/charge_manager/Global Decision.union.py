db2bdac7ec8e89f0e4a9de1071ead509b5db1fef
Erik Fleckstein <erik@tinkerforge.com>
2025-09-09 15:04:15 +0200

    util.py, charge_manager: Adapt to Timestamp -> Uptime change.

--- software/src/modules/charge_manager/Global Decision.union.py
+++ software/src/modules/charge_manager/Global Decision.union.py
@@ -7,13 +7,13 @@ from software.util import *
 spec = Union("Global Decision", 16, variants=[
     Variant('None'),
     Variant('PV Excess Overloaded Hysteresis Blocks Until', [
-        Member('Timestamp', Types.Timestamp),
+        Member('Timestamp', Types.Uptime),
         Member('Overload mA', Types.S32),
     ]),
     Variant('Hysteresis Elapses At', [
-        Member('Timestamp', Types.Timestamp)
+        Member('Timestamp', Types.Uptime)
     ]),
     Variant('Next Rotation At', [
-        Member('Timestamp', Types.Timestamp)
+        Member('Timestamp', Types.Uptime)
     ]),
 ])
