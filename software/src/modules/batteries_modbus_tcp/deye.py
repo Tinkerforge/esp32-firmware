9e64adf938099988db7b1c75d4160a414d8a3a28
Matthias Bolte <matthias@tinkerforge.com>
2025-09-10 18:43:49 +0200

    batteries-modbus-tcp: Add support for Deye hybrid inverter

---
+++ software/src/modules/batteries_modbus_tcp/deye.py
@@ -0,0 +1,128 @@
+specs = [
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Permit Grid Charge',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Time Points',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 148,
+                'values': [
+                    0,
+                    2359,
+                    2359,
+                    2359,
+                    2359,
+                    2359,
+                ],
+            },
+            {
+                'description': 'Target SoC (Full)',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 166,
+                'values': [
+                    100,
+                    100,
+                    100,
+                    100,
+                    100,
+                    100,
+                ],
+            },
+            {
+                'description': 'Time Points Charge Enable',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 172,
+                'values': [
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                ],
+            },
+        ],
+    },
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Revoke Grid Charge Override',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Target SoC (Minimum)',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 166,
+                'values': [
+                    10,
+                    10,
+                    10,
+                    10,
+                    10,
+                    10,
+                ],
+            },
+        ],
+    },
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Forbid Discharge',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Max A discharge',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 109,
+                'values': [
+                    0,
+                ],
+            },
+        ],
+    },
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Revoke Discharge Override',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Max A discharge',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 109,
+                'values': [
+                    50,
+                ],
+            },
+        ],
+    },
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Forbid Charge',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Max A charge',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 108,
+                'values': [
+                    0,
+                ],
+            },
+        ],
+    },
+    {
+        'group': 'Deye Hybrid Inverter',
+        'action': 'Revoke Charge Override',
+        'default_device_address': 1,
+        'register_blocks': [
+            {
+                'description': 'Max A charge',
+                'function_code': 'WriteMultipleRegisters',
+                'start_address': 108,
+                'values': [
+                    50,
+                ],
+            },
+        ],
+    },
+]
