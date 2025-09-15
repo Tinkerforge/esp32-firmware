bfcebda4ac8cb8eac565ac7ccc8042da3a3af985
Matthias Bolte <matthias@tinkerforge.com>
2025-09-15 10:27:56 +0200

    mqtt: Whitespace

--- software/src/modules/mqtt_auto_discovery/prepare.py
+++ software/src/modules/mqtt_auto_discovery/prepare.py
@@ -78,64 +78,59 @@ topic_template = """    {{
         .type = MqttDiscoveryType::{discovery_type},
     }}"""
 
-
-
-
-
-
 entities = [
 
 Entity(True, Component.SENSOR, Feature.METER, "powernow", "meter/values", "Leistungsaufnahme", "Power draw", "", "", "",
-    {"value_template":"{{value_json.power | round(0)}}",
-     "unit_of_measurement":"W",
-     "device_class":"power",
+    {"value_template": "{{value_json.power | round(0)}}",
+     "unit_of_measurement": "W",
+     "device_class": "power",
      "state_class": "measurement"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.METER, "energyabs", "meter/values", "Stromverbrauch absolut", "Energy consumption (absolute)", "", "", "",
-    {"value_template":"{{value_json.energy_abs | round(3)}}",
-     "unit_of_measurement":"kWh",
-     "device_class":"energy",
+    {"value_template": "{{value_json.energy_abs | round(3)}}",
+     "unit_of_measurement": "kWh",
+     "device_class": "energy",
      "state_class": "total"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.METER, "energyrel", "meter/values", "Stromverbrauch relativ", "Energy consumption (relative)", "", "", "",
-    {"value_template":"{{value_json.energy_rel | round(3)}}",
-     "unit_of_measurement":"kWh",
-     "device_class":"energy",
+    {"value_template": "{{value_json.energy_rel | round(3)}}",
+     "unit_of_measurement": "kWh",
+     "device_class": "energy",
      "state_class": "total"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.METER_PHASES, "current_l1", "meter/all_values", "Strom L1", "Current L1", "", "", "",
-    {"value_template":"{{value_json[3] | round(3)}}",
-     "unit_of_measurement":"A",
-     "device_class":"current",
+    {"value_template": "{{value_json[3] | round(3)}}",
+     "unit_of_measurement": "A",
+     "device_class": "current",
      "state_class": "measurement"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.METER_PHASES, "current_l2", "meter/all_values", "Strom L2", "Current L2", "", "", "",
-    {"value_template":"{{value_json[4] | round(3)}}",
-     "unit_of_measurement":"A",
-     "device_class":"current",
+    {"value_template": "{{value_json[4] | round(3)}}",
+     "unit_of_measurement": "A",
+     "device_class": "current",
      "state_class": "measurement"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.METER_PHASES, "current_l3", "meter/all_values", "Strom L3", "Current L3", "", "", "",
-    {"value_template":"{{value_json[5] | round(3)}}",
-     "unit_of_measurement":"A",
-     "device_class":"current",
+    {"value_template": "{{value_json[5] | round(3)}}",
+     "unit_of_measurement": "A",
+     "device_class": "current",
      "state_class": "measurement"},
     {}),
 
 Entity(True, Component.SENSOR, Feature.EVSE, "chargerstate", "evse/state", "Ladestatus", "Charge state", "", "", "",
-    {"value_template":"{{value_json.charger_state}}",
+    {"value_template": "{{value_json.charger_state}}",
      "icon": "mdi:ev-plug-type2"},
     {}),
 
 Entity(True, Component.NUMBER, Feature.EVSE, "globalcurrent", "evse/external_current", "Ladestromlimit", "charging current limit", "", "", "",
-    {"value_template":"{{(value_json.current | float / 1000) | round(3)}}",
+    {"value_template": "{{(value_json.current | float / 1000) | round(3)}}",
      "command_template": "{{ value * 1000 }}",
-     "unit_of_measurement":"A",
+     "unit_of_measurement": "A",
      "min": 0,
      "max": 32,
      "mode": "auto",
@@ -143,9 +138,9 @@ Entity(True, Component.NUMBER, Feature.EVSE, "globalcurrent", "evse/external_cur
     {}),
 
 Entity(True, Component.SENSOR, Feature.EVSE, "allowedcurrent", "evse/state", "Erlaubter Ladestrom", "Allowed charging current", "", "", "",
-    {"value_template":"{{(value_json.allowed_charging_current | float / 1000) | round(3)}}",
-     "unit_of_measurement":"A",
-     "device_class":"current",
+    {"value_template": "{{(value_json.allowed_charging_current | float / 1000) | round(3)}}",
+     "unit_of_measurement": "A",
+     "device_class": "current",
      "state_class": "measurement",
      "min": 0,
      "max": 32},
@@ -153,46 +148,46 @@ Entity(True, Component.SENSOR, Feature.EVSE, "allowedcurrent", "evse/state", "Er
 
 Entity(True, Component.BUTTON, Feature.EVSE, "startcharge", "evse/start_charging", "Ladevorgang starten", "Start charging", "", "", "",
     {"payload_press": "null",   # for Home Assistant
-     "payload_on":"null",       # for Domoticz
+     "payload_on": "null"       # for Domoticz
      "icon": "mdi:flash"},
     {}),
 
 Entity(True, Component.BUTTON, Feature.EVSE, "stopcharge", "evse/stop_charging", "Ladevorgang beenden", "Stop charging", "", "", "",
     {"payload_press": "null",   # for Home Assistant
-     "payload_on":"null",       # for Domoticz
+     "payload_on": "null",      # for Domoticz
      "icon": "mdi:flash-off"},
     {}),
 
 Entity(False, Component.BINARY_SENSOR, Feature.EVSE, "cable", "evse/state", "Wallbox-Ladekabel verbunden", "Charger cable connected", "", "", "",
-    {"device_class":"plug"},
-    {"value_template":"{{value_json.charger_state in [1, 2, 3]}}",
-     "payload_on":"True",
-     "payload_off":"False"}),
+    {"device_class": "plug"},
+    {"value_template": "{{value_json.charger_state in [1, 2, 3]}}",
+     "payload_on": "True",
+     "payload_off": "False"}),
 
 Entity(False, Component.BINARY_SENSOR, Feature.EVSE, "ready", "evse/state", "Wallbox ladebereit", "Charger ready to charge", "", "", "",
-    {"device_class":"power"},
-    {"value_template":"{{value_json.charger_state in [2, 3]}}",
-     "payload_on":"True",
-     "payload_off":"False"}),
+    {"device_class": "power"},
+    {"value_template": "{{value_json.charger_state in [2, 3]}}",
+     "payload_on": "True",
+     "payload_off": "False"}),
 
 Entity(False, Component.BINARY_SENSOR, Feature.EVSE, "charging", "evse/state", "Wallbox lädt", "Charger charging", "", "", "",
-    {"device_class":"battery_charging"},
-    {"value_template":"{{value_json.charger_state in [3]}}",
-     "payload_on":"True",
-     "payload_off":"False"}),
+    {"device_class": "battery_charging"},
+    {"value_template": "{{value_json.charger_state in [3]}}",
+     "payload_on": "True",
+     "payload_off": "False"}),
 
 Entity(False, Component.BINARY_SENSOR, Feature.EVSE, "error", "evse/state", "Wallbox-Fehler", "Charger error", "", "", "",
-    {"device_class":"problem"},
-    {"value_template":"{{value_json.charger_state in [4]}}",
-     "payload_on":"True",
-     "payload_off":"False"}),
+    {"device_class": "problem"},
+    {"value_template": "{{value_json.charger_state in [4]}}",
+     "payload_on": "True",
+     "payload_off": "False"}),
 
 Entity(False, Component.BINARY_SENSOR, Feature.EVSE, "online", "evse/low_level_state", "Wallbox verfügbar", "Charger online", "", "", "",
-    {"device_class":"connectivity",
+    {"device_class": "connectivity",
      "expire_after": "30"},
-    {"value_template":"{{value_json.uptime>0}}",
-     "payload_on":"True",
-     "payload_off":"False"}),
+    {"value_template": "{{value_json.uptime>0}}",
+     "payload_on": "True",
+     "payload_off": "False"}),
 ]
 
 topics = [topic_template.format(
