Tinkerforge/esp32-firmware

commit 08be9c3254b927f0a0a3c060f28cdaaa31cfd59c

charge_manager: Remove GlobalDecision.
This also changes the control flow in stage 4 to be able to report
ZeroPhaseDecision::NoCloudFilterBlocksUntil and
ZeroPhaseDecision::NoHysteresisBlocksUntil
for all chargers that will potentially be shut down soon.

software/src/modules/charge_manager/current_allocator.cpp
@@ -32,8 +32,6 @@
 #include "current_allocator_private.h"
 #include "tools/string_builder.h"
 
-#include "global_decision.union.h"
-
 //#include "gcc_warnings.h"
 
 #define LOCAL_LOG_FULL(indent, fmt, ...) \
@@ -351,17 +349,6 @@ static void clear_charger_decision_all(StageContext &sc, T tag) {
     for (int i = 0; i < sc.charger_count; ++i) {
         clear_charger_decision(sc, i, tag);
     }
-}
-
-static void set_global_decision(StageContext &sc, GlobalDecision &&decision) {
-    *sc.global_decision = decision;
-}
-
-static void clear_global_decision(StageContext &sc, GlobalDecisionTag tag) {
-    if (sc.global_decision->tag != tag)
-        return;
-
-    *sc.global_decision = GlobalDecision::None();
 }
 
 // Stage 1: Deactivate non-active chargers and handle rotation.
@@ -817,9 +804,10 @@ static void stage_3(StageContext &sc) {
         // to create a better status message. We would like to know which chargers
         // are going to be shut down first.
 
-        if (wnd_min.pv <= sc.limits->max_pv)  {
-            // Window minimum less than max pv limit -> PV not permanently overloaded
-            trace("3: wnd_min %d <= max_pv %d", wnd_min.pv, sc.limits->max_pv);
+        if (wnd_min.pv >= sc.limits->raw[GridPhase::PV])  {
+            // The currently available PV excess is more than the window minimum
+            // -> Everything's fine.
+            // TODO clear decisions here
             break;
         }
 
@@ -838,13 +826,20 @@ static void stage_3(StageContext &sc) {
         if (state->observe_pv_limit && min_cost.pv <= state->guaranteed_pv_current)
             continue;
 
+        if (wnd_min.pv <= sc.limits->max_pv)  {
+            // Window minimum less than max pv limit -> PV not permanently overloaded
+            trace("3: wnd_min %d <= max_pv %d", wnd_min.pv, sc.limits->max_pv);
+            set_charger_decision(sc, sc.idx_array[i], ZeroPhaseDecision::NoCloudFilterBlocksUntil(sc.limits->max_pv_expiration_ts, wnd_min.pv - sc.limits->max_pv));
+            continue;
+        }
+
         // This check looks to be too late, but at this point we've found a charger that
         // would be shut down if the hysteresis was elapsed. Don't log that we will start
         // to shut down chargers if we don't find one that will be shut down soon.
         if (!sc.ca_state->global_hysteresis_elapsed) {
-            auto hyst_elapsed_at = (sc.ca_state->last_hysteresis_reset + sc.cfg->global_hysteresis).to<seconds_t>();
-            set_global_decision(sc, GlobalDecision::PVExcessOverloadedHysteresisBlocksUntil(hyst_elapsed_at, wnd_min.pv - sc.limits->max_pv));
-            break;
+            auto hyst_elapsed_at = sc.ca_state->last_hysteresis_reset + sc.cfg->global_hysteresis;
+            set_charger_decision(sc, sc.idx_array[i], ZeroPhaseDecision::NoHysteresisBlocksUntil(hyst_elapsed_at, wnd_min.pv - sc.limits->max_pv));
+            continue;
         }
 
         trace("3: wnd_min %d > max_pv %d", wnd_min.pv, sc.limits->max_pv);
@@ -1685,8 +1680,7 @@ int allocate_current(
     CurrentAllocatorState *ca_state,
     ChargerAllocationState *charger_allocation_state,
     uint32_t *allocated_current,
-    ChargerDecision *charger_decisions,
-    GlobalDecision *global_decision
+    ChargerDecision *charger_decisions
     )
 {
     logger.trace_timestamp(charge_manager.trace_buffer_index);
@@ -1831,7 +1825,6 @@ int allocate_current(
         current_array,
         phases_array,
         charger_decisions,
-        global_decision,
         limits,
         charger_state,
         cfg->charger_count,
@@ -1841,9 +1834,6 @@ int allocate_current(
         charger_allocation_state,
         0xFFFFFFFF
     };
-
-    if (ca_state->global_hysteresis_elapsed)
-        clear_global_decision(sc, GlobalDecisionTag::HysteresisElapsesAt);
 
     trace_alloc(0, sc);
     trace("__all__");

software/src/modules/charge_manager/Global Allocator Decision.uint8.enum
@@ -1,4 +1,0 @@
-None0
-Next Rotation At2
-PV Excess Overloaded Hysteresis Blocks Until3
-Hysteresis Elapses At2

software/src/modules/charge_manager/Global Decision.union.py
@@ -1,19 +1,0 @@
-import tinkerforge_util as tfutil
-tfutil.create_parent_module(__file__, 'software')
-from software.util import *
-
-#from ....util import *
-
-spec = Union("Global Decision", 16, variants=[
-    Variant('None'),
-    Variant('PV Excess Overloaded Hysteresis Blocks Until', [
-        Member('Timestamp', Types.Uptime),
-        Member('Overload mA', Types.S32),
-    ]),
-    Variant('Hysteresis Elapses At', [
-        Member('Timestamp', Types.Uptime)
-    ]),
-    Variant('Next Rotation At', [
-        Member('Timestamp', Types.Uptime)
-    ]),
-])

software/src/modules/charge_manager/Zero Phase Decision.union.py
@@ -19,4 +19,12 @@ spec = Union("Zero Phase Decision", 20, variants=[
         Member('Phase', Types.U8)
     ]),
     Variant('Yes Unknown'),
+    Variant('No Cloud Filter Blocks Until', [
+        Member('Timestamp', Types.Uptime),
+        Member('Overload mA', Types.S32),
+    ]),
+    Variant('No Hysteresis Blocks Until', [
+        Member('Timestamp', Types.Uptime),
+        Member('Overload mA', Types.S32),
+    ]),
 ])

software/src/modules/charge_manager/charge_manager.cpp
@@ -35,7 +35,6 @@
 #include "one_phase_decision.union.h"
 #include "three_phase_decision.union.h"
 #include "current_decision.union.h"
-#include "global_decision.union.h"
 
 static constexpr micros_t WATCHDOG_TIMEOUT = 30_s;
 
@@ -178,7 +177,6 @@ void ChargeManager::pre_setup()
         {"l_spread", Config::Tuple(4, Config::Int32(0))},
         {"l_max_pv", Config::Int32(0)},
         {"alloc", Config::Tuple(4, Config::Int32(0))},
-        {"d", GlobalDecision::getUnion()},
         {"chargers", Config::Array(
             {},
             &state_chargers_prototype,
@@ -512,7 +510,6 @@ void ChargeManager::setup()
     this->charger_state = (ChargerState *)calloc_psram_or_dram(this->charger_count, sizeof(ChargerState));
     this->charger_allocation_state = (ChargerAllocationState *)calloc_psram_or_dram(this->charger_count, sizeof(ChargerAllocationState));
     this->charger_decisions = (ChargerDecision *)calloc(this->charger_count, sizeof(ChargerDecision));
-    this->global_decision = (GlobalDecision *)calloc(1, sizeof(GlobalDecision));
 
     for (size_t i = 0; i < charger_count; ++i) {
         charger_state[i].phase_rotation = convert_phase_rotation(config.get("chargers")->get(i)->get("rot")->asEnum<CMPhaseRotation>());
@@ -579,8 +576,7 @@ void ChargeManager::setup()
                 this->ca_state,
                 this->charger_allocation_state,
                 &allocated_current,
-                this->charger_decisions,
-                this->global_decision
+                this->charger_decisions
             );
 
             for (size_t i = 0; i < 4; i++) {
@@ -602,7 +598,6 @@ void ChargeManager::setup()
                 this->charger_decisions[i].three.writeToConfig((Config *)this->state.get("chargers")->get(i)->get("d3"));
                 this->charger_decisions[i].current.writeToConfig((Config *)this->state.get("chargers")->get(i)->get("dc"));
             }
-            this->global_decision->writeToConfig((Config *)this->state.get("d"));
 
             this->state.get("state")->updateUint(result);
         }, 1_s);

software/src/modules/charge_manager/charge_manager.h
@@ -35,7 +35,6 @@ struct CurrentAllocatorState;
 struct ChargerState;
 struct ChargerAllocationState;
 struct ChargerDecision;
-struct GlobalDecision;
 
 namespace ChargeMode {
     enum Type {
@@ -163,7 +162,6 @@ private:
     CurrentAllocatorState *ca_state = nullptr;
 
     ChargerDecision *charger_decisions = nullptr;
-    GlobalDecision *global_decision = nullptr;
 
     std::array<uint8_t, 2> supported_charge_mode_bitmask;
 };

software/src/modules/charge_manager/charge_manager_private.h
@@ -19,7 +19,6 @@
 #include "one_phase_decision.union.h"
 #include "three_phase_decision.union.h"
 #include "current_decision.union.h"
-#include "global_decision.union.h"
 
 struct ChargerDecision {
     ZeroPhaseDecision zero;

software/src/modules/charge_manager/current_allocator.h
@@ -39,8 +39,7 @@ int allocate_current(
         CurrentAllocatorState *ca_state,
         ChargerAllocationState *charger_allocation_state,
         uint32_t *allocated_current,
-        ChargerDecision *charger_decisions,
-        GlobalDecision *global_decision);
+        ChargerDecision *charger_decisions);
 
 bool update_from_client_packet(
     uint8_t client_id,

commit 849d86cc286a3b00367abab1968b5408dedb34c2

charge_manager: Fix deactivation because of missing PV current.
Broken in 08be9c3254b927f0a0a3c060f28cdaaa31cfd59c

software/src/modules/charge_manager/current_allocator.cpp
@@ -804,8 +804,8 @@ static void stage_3(StageContext &sc) {
         // to create a better status message. We would like to know which chargers
         // are going to be shut down first.
 
-        if (wnd_min.pv >= sc.limits->raw[GridPhase::PV])  {
-            // The currently available PV excess is more than the window minimum
+        if (sc.limits->raw[GridPhase::PV] >= wnd_min.pv)  {
+            // The currently available PV excess is equal or more than the window minimum
             // -> Everything's fine.
             // TODO clear decisions here
             break;
@@ -826,9 +826,9 @@ static void stage_3(StageContext &sc) {
         if (state->observe_pv_limit && min_cost.pv <= state->guaranteed_pv_current)
             continue;
 
-        if (wnd_min.pv <= sc.limits->max_pv)  {
+        if (wnd_min.pv < sc.limits->max_pv)  {
             // Window minimum less than max pv limit -> PV not permanently overloaded
-            trace("3: wnd_min %d <= max_pv %d", wnd_min.pv, sc.limits->max_pv);
+            trace("3: wnd_min %d < max_pv %d", wnd_min.pv, sc.limits->max_pv);
             set_charger_decision(sc, sc.idx_array[i], ZeroPhaseDecision::NoCloudFilterBlocksUntil(sc.limits->max_pv_expiration_ts, wnd_min.pv - sc.limits->max_pv));
             continue;
         }