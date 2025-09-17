4354061a16121e24770b87f09666d60faf17f9a5
Frederic Henrichs <frederic@tinkerforge.com>
2025-09-17 10:07:16 +0200

    web/network: show alerts in status alerts

--- software/web/src/modules/network/main.tsx
+++ software/web/src/modules/network/main.tsx
@@ -21,7 +21,7 @@
 
 import * as util from "../../ts/util";
 import * as API from "../../ts/api";
-import { h, Component, Fragment, RefObject } from "preact";
+import { h } from "preact";
 import { __, translate_unchecked } from "../../ts/translation";
 import { Switch } from "../../ts/components/switch";
 import { ConfigComponent } from "../../ts/components/config_component";
@@ -32,65 +32,14 @@ import { InputNumber } from "../../ts/components/input_number";
 import { SubPage } from "../../ts/components/sub_page";
 import { NavbarItem } from "../../ts/components/navbar_item";
 import { Settings } from "react-feather";
-import { StatusSection } from "ts/components/status_section";
-import { Alert } from "react-bootstrap";
+import { useEffect } from "react";
 
 export function NetworkNavbar() {
     return <NavbarItem name="network" module="network" title={__("network.navbar.network")} symbol={<Settings />} />;
 }
 
 type NetworkConfig = API.getType["network/config"];
-
-export class Network extends ConfigComponent<'network/config', {status_ref: RefObject<NetworkStatus> }> {
-    constructor() {
-        super('network/config',
-              () => __("network.script.save_failed"),
-              () => __("network.script.reboot_content_changed"));
-    }
-
-    render(props: {}, state: Readonly<NetworkConfig>) {
-        if (!util.render_allowed())
-            return <SubPage name="network" />;
-
-        return (
-            <SubPage name="network">
-                <ConfigForm id="network_config_form"
-                            title={__("network.content.network")}
-                            isModified={this.isModified()}
-                            isDirty={this.isDirty()}
-                            onSave={this.save}
-                            onReset={this.reset}
-                            onDirtyChange={this.setDirty}>
-                    <FormRow label={__("network.content.hostname")}>
-                        <InputTextPatterned maxLength={32}
-                                   pattern="[a-zA-Z0-9\-]*"
-                                   required
-                                   value={state.hostname}
-                                   onValue={this.set("hostname")}
-                                   invalidFeedback={__("network.content.hostname_invalid")}
-                                   />
-                    </FormRow>
-
-                    <FormRow label={__("network.content.enable_mdns")}>
-                        <Switch desc={__("network.content.enable_mdns_desc")}
-                                checked={state.enable_mdns}
-                                onClick={this.toggle('enable_mdns')}/>
-                    </FormRow>
-
-                    <FormRow label={__("network.content.web_server_port")}>
-                        <InputNumber required
-                                     min={1}
-                                     max={65535}
-                                     value={state.web_server_port}
-                                     onValue={this.set("web_server_port")}/>
-                    </FormRow>
-                </ConfigForm>
-            </SubPage>
-        );
-    }
-}
-
-interface NetworkStatusState {
+interface NetworkState {
 //#if MODULE_ETHERNET_AVAILABLE
     ethernet: API.getType["ethernet/state"];
     ethernetConfig: API.getType["ethernet/config"];
@@ -111,9 +60,11 @@ interface NetworkStatusState {
 //#endif
 }
 
-export class NetworkStatus extends Component<{}, NetworkStatusState> {
+export class Network extends ConfigComponent<'network/config', {}, NetworkState> {
     constructor() {
-        super();
+        super('network/config',
+              () => __("network.script.save_failed"),
+              () => __("network.script.reboot_content_changed"));
 
 //#if MODULE_ETHERNET_AVAILABLE
         util.addApiEventListener('ethernet/state', () => {
@@ -150,7 +101,7 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
 //#endif
     }
 
-    getEqualSubnets(state: Readonly<NetworkStatusState>) {
+    getEqualSubnets() {
         const connectedSubnets: {
             network: number;
             subnet: string;
@@ -160,9 +111,9 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
         }[] = [];
 
 //#if MODULE_ETHERNET_AVAILABLE
-        const ethernetIP = util.parseIP(state.ethernet.ip);
+        const ethernetIP = util.parseIP(this.state.ethernet.ip);
         if (ethernetIP !== 0) {
-            const ethernetSubnet = util.parseIP(state.ethernet.subnet);
+            const ethernetSubnet = util.parseIP(this.state.ethernet.subnet);
             const ethernetNetwork = ethernetIP & ethernetSubnet;
             const subnetString = `/${util.countBits(ethernetSubnet)}`;
             const dhcp = API.get_unchecked("ethernet/config").ip === "0.0.0.0";
@@ -171,18 +122,18 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
 //#endif
 
 //#if MODULE_WIFI_AVAILABLE
-        const wifiIP = util.parseIP(state.wifi.sta_ip);
+        const wifiIP = util.parseIP(this.state.wifi.sta_ip);
         if (wifiIP !== 0) {
-            const wifiSubnet = util.parseIP(state.wifi.sta_subnet);
+            const wifiSubnet = util.parseIP(this.state.wifi.sta_subnet);
             const wifiNetwork = wifiIP & wifiSubnet;
             const subnetString = `/${util.countBits(wifiSubnet)}`;
             const dhcp = API.get("wifi/sta_config").ip === "0.0.0.0";
             connectedSubnets.push({network: wifiNetwork, name: __("wifi.navbar.wifi_sta"), subnet: subnetString, href: "#wifi_sta", dhcp});
         }
 
-        if (state.apConfig.enable_ap && !state.apConfig.ap_fallback_only) {
-            const apIP = util.parseIP(state.apConfig.ip);
-            const apSubnet = util.parseIP(state.apConfig.subnet);
+        if (this.state.apConfig.enable_ap && !this.state.apConfig.ap_fallback_only) {
+            const apIP = util.parseIP(this.state.apConfig.ip);
+            const apSubnet = util.parseIP(this.state.apConfig.subnet);
             const apNetwork = apIP & apSubnet;
             const subnetString = `/${util.countBits(apSubnet)}`;
             connectedSubnets.push({network: apNetwork, name: __("wifi.navbar.wifi_ap"), subnet: subnetString, href: "#wifi_ap"});
@@ -190,9 +141,9 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
 //#endif
 
 //#if MODULE_WIREGUARD_AVAILABLE
-        if (state.wireguardConfig.enable) {
-            const wireguardIP = util.parseIP(state.wireguardConfig.internal_ip);
-            const wireguardSubnet = util.parseIP(state.wireguardConfig.internal_subnet);
+        if (this.state.wireguardConfig.enable) {
+            const wireguardIP = util.parseIP(this.state.wireguardConfig.internal_ip);
+            const wireguardSubnet = util.parseIP(this.state.wireguardConfig.internal_subnet);
             const wireguardNetwork = wireguardIP & wireguardSubnet;
             const subnetString = `/${util.countBits(wireguardSubnet)}`;
             connectedSubnets.push({network: wireguardNetwork, name: __("wireguard.navbar.wireguard"), subnet: subnetString, href: "#wireguard"});
@@ -200,8 +151,8 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
 //#endif
 
 //#if MODULE_REMOTE_ACCESS_AVAILABLE
-        if (state.remoteAccessConfig.enable) {
-            for (let i = 0; i < state.remoteAccessConfig.users.length * 5; i++) {
+        if (this.state.remoteAccessConfig.enable) {
+            for (let i = 0; i < this.state.remoteAccessConfig.users.length * 5; i++) {
                 const remoteAccessNetwork = util.parseIP(`10.123.${i}.0`);
                 connectedSubnets.push({network: remoteAccessNetwork, name: __("remote_access.navbar.remote_access"), subnet: "/24", href: "#remote_access"});
             }
@@ -247,20 +198,22 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
         return conflictSubnets;
     }
 
-    checkDNS(state: Readonly<NetworkStatusState>) {
+    checkDNS() {
         const interfacesMissingDNS: string[] = [];
 //#if MODULE_ETHERNET_AVAILABLE
-        if (state.ethernetConfig.ip !== "0.0.0.0" &&
-            state.ethernetConfig.dns === "0.0.0.0" &&
-            state.ethernetConfig.dns2 === "0.0.0.0") {
+        if (this.state.ethernetConfig.ip !== "0.0.0.0" &&
+            this.state.ethernetConfig.dns === "0.0.0.0" &&
+            this.state.ethernetConfig.dns2 === "0.0.0.0" &&
+            this.state.ethernetConfig.enable_ethernet) {
                 interfacesMissingDNS.push(translate_unchecked("ethernet.navbar.ethernet"));
         }
 //#endif
 
 //#if MODULE_WIFI_AVAILABLE
-        if (state.wifiConfig.ip !== "0.0.0.0" &&
-            state.wifiConfig.dns === "0.0.0.0" &&
-            state.wifiConfig.dns2 === "0.0.0.0") {
+        if (this.state.wifiConfig.ip !== "0.0.0.0" &&
+            this.state.wifiConfig.dns === "0.0.0.0" &&
+            this.state.wifiConfig.dns2 === "0.0.0.0" &&
+            this.state.wifiConfig.enable_sta) {
                 interfacesMissingDNS.push(translate_unchecked("wifi.navbar.wifi_sta"));
         }
 //#endif
@@ -268,26 +221,73 @@ export class NetworkStatus extends Component<{}, NetworkStatusState> {
         return interfacesMissingDNS;
     }
 
-    render(props: {}, state: Readonly<NetworkStatusState>) {
-        if (!util.render_allowed()) {
-            return <StatusSection name="network" />;
-        }
-        const conflictSubnets = this.getEqualSubnets(state);
-        const conflictSubnetsFragment = conflictSubnets.length === 0 ? <></> :
-        <FormRow label={__("network.status.subnet_conflict")}>
-            <Alert className="mb-0" variant="danger" >{__("network.status.subnet_text")(conflictSubnets)}</Alert>
-        </FormRow>
+    render(props: {}, state: Readonly<NetworkConfig>) {
+        if (!util.render_allowed())
+            return <SubPage name="network" />;
 
-        const interfacesMissingDNS = this.checkDNS(state);
-        const dnsFragment = interfacesMissingDNS.length === 0 ? <></> :
-        <FormRow label={__("network.status.dns_not_configured")}>
-            <Alert className="mb-0" variant="warning">{__("network.status.dns_not_configured_text")(interfacesMissingDNS)}</Alert>
-        </FormRow>
+        const conflictSubnets = this.getEqualSubnets();
+        useEffect(() => {
+            if (conflictSubnets.length > 0) {
+                util.add_status_alert(
+                    "conflict_subnets",
+                    "danger",
+                    () => __("network.status.subnet_conflict"),
+                    () => __("network.status.subnet_text")(conflictSubnets)
+                );
+            } else {
+                util.remove_status_alert("conflict_subnets");
+            }
+        }, [JSON.stringify(conflictSubnets)]);
 
-        return <StatusSection name="network">
-            {conflictSubnetsFragment}
-            {dnsFragment}
-        </StatusSection>;
+        const interfacesMissingDNS = this.checkDNS();
+        useEffect(() => {
+            if (interfacesMissingDNS.length > 0) {
+                util.add_status_alert(
+                    "dns_not_configured",
+                    "warning",
+                    () => __("network.status.dns_not_configured"),
+                    () => __("network.status.dns_not_configured_text")(interfacesMissingDNS)
+            );
+            } else {
+                util.remove_status_alert("dns_not_configured");
+            }
+        }, [JSON.stringify(interfacesMissingDNS)]);
+
+        return (
+            <SubPage name="network">
+                <ConfigForm id="network_config_form"
+                            title={__("network.content.network")}
+                            isModified={this.isModified()}
+                            isDirty={this.isDirty()}
+                            onSave={this.save}
+                            onReset={this.reset}
+                            onDirtyChange={this.setDirty}>
+                    <FormRow label={__("network.content.hostname")}>
+                        <InputTextPatterned maxLength={32}
+                                   pattern="[a-zA-Z0-9\-]*"
+                                   required
+                                   value={state.hostname}
+                                   onValue={this.set("hostname")}
+                                   invalidFeedback={__("network.content.hostname_invalid")}
+                                   />
+                    </FormRow>
+
+                    <FormRow label={__("network.content.enable_mdns")}>
+                        <Switch desc={__("network.content.enable_mdns_desc")}
+                                checked={state.enable_mdns}
+                                onClick={this.toggle('enable_mdns')}/>
+                    </FormRow>
+
+                    <FormRow label={__("network.content.web_server_port")}>
+                        <InputNumber required
+                                     min={1}
+                                     max={65535}
+                                     value={state.web_server_port}
+                                     onValue={this.set("web_server_port")}/>
+                    </FormRow>
+                </ConfigForm>
+            </SubPage>
+        );
     }
 }
 
