15a92462353d59617697f24886f41af61dfbddb9
Mattias Schäffersmann <mattias@tinkerforge.com>
2025-10-06 15:43:47 +0200

    tools/memory: Fix type of section labels
    Section labels have no type, so don't pretend they're uint32_t.

--- software/src/tools/memory.h
+++ software/src/tools/memory.h
@@ -19,27 +19,27 @@
 
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
+// Section start and end labels have no type and cannot be accessed safely, but their addresses can be used.
+struct _LabelWithNoData;
 
 // 0x3f400000 - 0x3f7fffff flash rodata
-extern uint32_t _rodata_start;
-extern uint32_t _rodata_end;
+extern _LabelWithNoData _rodata_start;
+extern _LabelWithNoData _rodata_end;
 
 // 0x3f800000 - 0x3fbfffff PSRAM
-extern uint32_t _ext_ram_bss_start;
+extern _LabelWithNoData _ext_ram_bss_start;
 
 // 0x3ffae000 - 0x3fffffff SRAM
-extern uint32_t _data_start;
-extern uint32_t _noinit_start;
-extern uint32_t _bss_start;
-extern uint32_t _heap_start;
+extern _LabelWithNoData _data_start;
+extern _LabelWithNoData _noinit_start;
+extern _LabelWithNoData _bss_start;
+extern _LabelWithNoData _heap_start;
 
 // 0x40080000 - 0x4009ffff IRAM
-extern uint32_t _iram_start;
+extern _LabelWithNoData _iram_start;
 
 // 0x400c2000 - 0x40bfffff flash text
-extern uint32_t _text_start;
+extern _LabelWithNoData _text_start;
 
 inline bool address_is_in_rodata(const void *ptr)
 {
