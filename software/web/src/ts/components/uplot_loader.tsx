427d4a307dd5f32098886d7352bf58b570aa9d3f
Olaf Lüke <olaf@tinkerforge.com>
2025-10-15 18:48:55 +0200

    web/uplot: Use Maximize-Icon from react-feather

--- software/web/src/ts/components/uplot_loader.tsx
+++ software/web/src/ts/components/uplot_loader.tsx
@@ -19,6 +19,8 @@
 
 import { h, Fragment, Component, ComponentChildren, createRef, cloneElement } from "preact";
 import { Modal, Button } from "react-bootstrap";
+import { Maximize } from "react-feather";
+import { __ } from "../../ts/translation";
 
 interface UplotLoaderProps {
     show: boolean;
@@ -102,9 +104,7 @@ export class UplotLoader extends Component<UplotLoaderProps, {show_fullscreen: b
                         onClick={() => this.setState({show_fullscreen: true})}
                         title={__("component.uplot.fullscreen")}
                     >
-                        <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
+                        <Maximize />
                     </Button>
                 )}
 
