a52e1639e6dd3b694d1bf0b51c289d3628e700ab
Erik Fleckstein <erik@tinkerforge.com>
2025-12-02 16:24:30 +0100

    charge_manager: Move auth and tracking out of current allocator.

--- software/src/modules/charge_manager/current_allocator.h
+++ software/src/modules/charge_manager/current_allocator.h
@@ -48,8 +48,6 @@ bool update_from_client_packet(
     cm_state_v1 *v1,
     cm_state_v2 *v2,
     cm_state_v3 *v3,
-    cm_state_v4 *v4,
-    cm_state_v5 *v5,
     const CurrentAllocatorConfig *cfg,
     ChargerState *charger_state,
     ChargerAllocationState *charger_allocation_state,
