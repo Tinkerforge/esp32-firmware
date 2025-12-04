Tinkerforge/esp32-firmware

commit d4823b64eb5d13ded09ab67bbdc2c0527751c0e9

config: Avoid additional string copies in error messages
The error might be caused by high memory pressure and an allocation
to copy the string is more likely to fail as well.

Instead, overwrite the zero-termination with the newline
for the log message and log the string as-is.

Also log StringBuilder overflows. A StringBuilder that happes to be filled
to the exact limit will incorrectly be considered overflowed as well,
but that is hopefully not that likely.

software/src/config.cpp
@@ -666,10 +666,15 @@ void Config::save_to_file(File &file)
             logger.printfln("JSON doc overflow while writing file %s! Doc capacity is zero but needed %u.", file.name(), json_size(false));
         } else {
             logger.printfln("JSON doc overflow while writing file %s! Doc capacity is %u. Truncated doc follows.", file.name(), capacity);
+
             String str;
             serializeJson(doc, str);
-            str += "\n";
-            logger.print_plain(str.c_str(), str.length());
+            char *wbuffer = str.begin();
+            size_t len = str.length();
+
+            // Overwrite zero-termination with newline. This is safe because print_plain doen't require termination and the container String is discarded afterwards.
+            wbuffer[len] = '\n';
+            logger.print_plain(wbuffer, len + 1);
         }
     }
     serializeJson(doc, file);
@@ -690,10 +695,15 @@ void Config::write_to_stream_except(Print &output, const char *const *keys_to_ce
             logger.printfln("JSON doc overflow while writing to stream! Doc capacity is zero but needed %u.", json_size(false));
         } else {
             logger.printfln("JSON doc overflow while writing to stream! Doc capacity is %u. Truncated doc follows.", capacity);
+
             String str;
             serializeJson(doc, str);
-            str += "\n";
-            logger.print_plain(str.c_str(), str.length());
+            char *wbuffer = str.begin();
+            size_t len = str.length();
+
+            // Overwrite zero-termination with newline. This is safe because print_plain doen't require termination and the container String is discarded afterwards.
+            wbuffer[len] = '\n';
+            logger.print_plain(wbuffer, len + 1);
         }
     }
     serializeJson(doc, output);
@@ -718,7 +728,14 @@ String Config::to_string_except(const char *const *keys_to_censor, size_t keys_t
             logger.printfln("JSON doc overflow while converting to string! Doc capacity is zero but needed %u.", json_size(false));
         } else {
             logger.printfln("JSON doc overflow while converting to string! Doc capacity is %u. Truncated doc follows.", capacity);
-            logger.print_plain((result + "\n").c_str(), result.length() + 1);
+
+            char *wbuffer = result.begin();
+            size_t len = result.length();
+
+            // Temporarily overwrite zero-termination with newline, to avoid copying the whole string to add the newline. This is safe because print_plain doesn't require termination.
+            wbuffer[len] = '\n';
+            logger.print_plain(wbuffer, len + 1);
+            wbuffer[len] = 0;
         }
     }
     return result;
@@ -741,6 +758,12 @@ void Config::to_string_except(const char *const *keys_to_censor, size_t keys_to_
             logger.print_plain(ptr, written);
             logger.print_plain("\n", 1);
         }
+    }
+
+    if (sb->getRemainingLength() == 0) {
+        logger.printfln("StringBuilder overflow while converting JSON to string! Doc size is %zu. Truncated string follows.", doc.size());
+        logger.print_plain(ptr, written);
+        logger.print_plain("\n", 1);
     }
 }
 

commit 1b2be652f9bd1a2e79e59e17afb07ca41a0e4e34

config: Fix truncation check when converting to string.
Bug introduced in d4823b64eb5d13ded09ab67bbdc2c0527751c0e9.

software/src/config.cpp
@@ -1066,7 +1066,15 @@ void Config::to_string_except(const char *const *keys_to_censor, size_t keys_to_
     // Asserts checked in ::to_json.
     auto doc = this->to_json(keys_to_censor, keys_to_censor_len);
     char *ptr = sw->getRemainingPtr();
-    size_t written = serializeJson(doc, ptr, sw->getRemainingLength());
+
+    // serializeJson only adds the null terminator if there is enough room in the buffer.
+    // To detect truncation, allow writing one byte more than what is still remaining.
+    // StringWriter always keeps one byte extra to be able to null-terminate, so writing one byte extra is safe.
+    auto remaining_before = sw->getRemainingLength();
+    size_t written = serializeJson(doc, ptr, sw->getRemainingLength() + 1);
+
+    // If serializeJson has written the extra byte from above, the output is truncated.
+    bool truncated = written == remaining_before + 1;
 
     sw->setLength(sw->getLength() + written);
 
@@ -1082,7 +1090,7 @@ void Config::to_string_except(const char *const *keys_to_censor, size_t keys_to_
     }
 
     if (sw->getRemainingLength() == 0) {
-        logger.printfln("StringBuilder overflow while converting JSON to string! Doc size is %zu. Truncated string follows.", doc.size());
+        logger.printfln("StringBuilder overflow while converting JSON to string! Capacity %zu, remaining before serialize %zu. Truncated string follows.", sw->getCapacity(), capacity_before);
         logger.print_plain(ptr, written);
         logger.print_plain("\n", 1);
     }