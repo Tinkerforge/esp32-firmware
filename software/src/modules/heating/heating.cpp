174c2358d87a3d6c571f96a15e83fea214eaa6f4
Matthias Bolte <matthias@tinkerforge.com>
2025-12-17 13:48:20 +0100

    heating: Remove useless trace log prefix

--- software/src/modules/heating/heating.cpp
+++ software/src/modules/heating/heating.cpp
@@ -16,6 +16,9 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+
+#define TRACE_LOG_PREFIX nullptr
+
 #include "heating.h"
 
 #include <time.h>
