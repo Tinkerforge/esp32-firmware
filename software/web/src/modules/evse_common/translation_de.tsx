743680c1286512ffb255f0549479e924df3fdca1
Olaf Lüke <olaf@tinkerforge.com>
2026-01-21 19:00:21 +0100

    web/bs5: Use default border color for better dark mode support

--- software/web/src/modules/evse_common/translation_de.tsx
+++ software/web/src/modules/evse_common/translation_de.tsx
@@ -307,7 +307,7 @@ let x = {
                 return <>limitiere den erlaubten Ladestrom auf <b>{current} A</b>.</>
             }/*NF*/,
             "automation_led_action_text": /*FFN*/(indication_number: number, indication_text: string, duration: number, color: string) => {
-                let c = color == "" ? "" : <>in <span class="px-2 me-1" style={"background-color: " + color + "; border: 1px solid black;"}></span></>;
+                let c = color == "" ? "" : <>in <span class="px-2 me-1 border" style={"background-color: " + color}></span></>;
                 if (indication_number == 0)
                     return <>schalte die Status-LED für <b>{toLocaleFixed(duration / 1000)} Sekunden</b> <b>{indication_text}</b>.</>;
                 if (indication_number == 255)
