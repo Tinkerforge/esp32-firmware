7bb8f94a8297ca2cf07a07203975f07ccf6704d5
Mattias Schäffersmann <mattias@tinkerforge.com>
2026-02-16 19:02:44 +0100

    front_panel: Don't show >1GW for a charger with no data
    Division by zero, cast to int.

--- software/src/modules/front_panel/front_panel.cpp
+++ software/src/modules/front_panel/front_panel.cpp
@@ -436,9 +436,12 @@ int FrontPanel::update_front_page_charger(const uint8_t index, const TileType ty
 #if MODULE_CHARGE_MANAGER_AVAILABLE()
     size_t charger_count = charge_manager.get_charger_count();
     if (charger_count > 0) {
-        auto *charger = charge_manager.get_charger_state(param);
-        const int32_t watt = charger->power_total_sum/charger->power_total_count;
-        str2 = watt_value_to_display_string(watt);
+        const auto *charger = charge_manager.get_charger_state(param);
+        
+        if (charger->power_total_count > 0) {
+            const int32_t watt = charger->power_total_sum / charger->power_total_count;
+            str2 = watt_value_to_display_string(watt);
+        }
     }
 #endif
 
