591891f7c08fe491bde7cc5c21d6c4b2093f2226
Olaf Lüke <olaf@tinkerforge.com>
2026-02-26 20:20:30 +0100

    web/ethernet: Hide unnecessary status information when not enabeld

--- software/web/src/modules/ethernet/main.tsx
+++ software/web/src/modules/ethernet/main.tsx
@@ -87,23 +87,25 @@ export class Ethernet extends ConfigComponent<'ethernet/config', {status_ref?: R
                             ]}/>
                     </FormRow>
 
-                    <FormRow label={__("ethernet.content.status_ip")}>
-                        <InputText
-                            value={eth_state.ip != "0.0.0.0"
-                                ? eth_state.ip + " (/" + util.countBits(util.parseIP(eth_state.subnet)) + ")"
-                                : __("ethernet.content.status_ip_none")}
-                        />
-                    </FormRow>
+                    {eth_state.connection_state != EthernetState.NotConfigured && <>
+                        <FormRow label={__("ethernet.content.status_ip")}>
+                            <InputText
+                                value={eth_state.ip != "0.0.0.0"
+                                    ? eth_state.ip + " (/" + util.countBits(util.parseIP(eth_state.subnet)) + ")"
+                                    : __("ethernet.content.status_ip_none")}
+                            />
+                        </FormRow>
 
-                    <FormRow label={__("ethernet.content.status_link")}>
-                        <InputText
-                            value={eth_state.link_speed > 0
-                                ? eth_state.link_speed + " Mbps, " + (eth_state.full_duplex
-                                    ? __("ethernet.content.full_duplex")
-                                    : __("ethernet.content.half_duplex"))
-                                : __("ethernet.content.status_link_none")}
-                        />
-                    </FormRow>
+                        <FormRow label={__("ethernet.content.status_link")}>
+                            <InputText
+                                value={eth_state.link_speed > 0
+                                    ? eth_state.link_speed + " Mbps, " + (eth_state.full_duplex
+                                        ? __("ethernet.content.full_duplex")
+                                        : __("ethernet.content.half_duplex"))
+                                    : __("ethernet.content.status_link_none")}
+                            />
+                        </FormRow>
+                    </>}
 
                     <FormRow label={__("ethernet.content.mac")}>
                         <InputText
