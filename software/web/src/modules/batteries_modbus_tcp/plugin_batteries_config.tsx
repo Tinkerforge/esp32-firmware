6c964db35fe76fb1d647f3ce34d25cebbf65b552
Matthias Bolte <matthias@tinkerforge.com>
2026-03-12 11:10:48 +0100

    web/batteries-modbus-tcp: Add hint about unsupported inverter series

--- software/web/src/modules/batteries_modbus_tcp/plugin_batteries_config.tsx
+++ software/web/src/modules/batteries_modbus_tcp/plugin_batteries_config.tsx
@@ -924,6 +924,9 @@ export function pre_init() {
                     if (config[1].table[0] == BatteryModbusTCPTableID.SMAHybridInverter) {
                         edit_children.push(
                             <FormRow>
+                                <Alert variant="warning" className="mb-0">{__("batteries_modbus_tcp.content.sma_unsupported_warning")}</Alert>
+                            </FormRow>,
+                            <FormRow>
                                 <Alert variant="warning" className="mb-0">{__("batteries_modbus_tcp.content.sma_forecast_based_charging_warning")}</Alert>
                             </FormRow>);
                     }
