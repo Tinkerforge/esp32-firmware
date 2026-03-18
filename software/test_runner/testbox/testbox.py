400bd555f1806f3cebb067ef9e5e785099e21bb6
Erik Fleckstein <erik@tinkerforge.com>
2026-03-18 10:36:42 +0100

    tests: Simplify testbox default methods.

--- software/test_runner/testbox/testbox.py
+++ software/test_runner/testbox/testbox.py
@@ -1,4 +1,5 @@
 from typing import Protocol, Literal, TYPE_CHECKING, Any
+import inspect
 
 CP = Literal['A', 'B', 'C', 'D']
 Meter = Literal['real', 'fake']
@@ -13,33 +14,36 @@ else:
 class TestBox(Protocol):
     tc: tc_type = None
 
-    def set_cp(self, cp: CP): self.tc.skip("No suitable testbox connected to call set_cp")
-    def get_cp(self) -> CP: self.tc.skip("No suitable testbox connected to call get_cp")
+    def __not_implemented(self):
+        self.tc.skip(f"No suitable testbox connected to call {inspect.stack()[1][3]}")
 
-    def set_diode_faulted(self, fault: bool, clear_after_s: int = 0) -> None: self.tc.skip("No suitable testbox connected to call set_diode_faulted")
-    def is_diode_fauled(self) -> bool: self.tc.skip("No suitable testbox connected to call is_diode_fauled")
+    def set_cp(self, cp: CP): self.__not_implemented()
+    def get_cp(self) -> CP: self.__not_implemented()
 
-    def connect_contactor_aux(self, contactor: int, aux: Aux) -> None: self.tc.skip("No suitable testbox connected to call connect_contactor_aux")
-    def get_contactor_aux_state(self, contactor: int) -> Aux: self.tc.skip("No suitable testbox connected to call get_contactor_aux_state")
+    def set_diode_faulted(self, fault: bool, clear_after_s: int = 0) -> None: self.__not_implemented()
+    def is_diode_fauled(self) -> bool: self.__not_implemented()
 
-    def set_button_pressed(self, pressed: bool) -> None: self.tc.skip("No suitable testbox connected to call set_button_pressed")
-    def is_button_pressed(self) -> bool: self.tc.skip("No suitable testbox connected to call is_button_pressed")
+    def connect_contactor_aux(self, contactor: int, aux: Aux) -> None: self.__not_implemented()
+    def get_contactor_aux_state(self, contactor: int) -> Aux: self.__not_implemented()
 
-    def set_dc_faulted(self, fault: bool, clear_after_s: int = 0) -> None: self.tc.skip("No suitable testbox connected to call set_dc_faulted")
-    def is_dc_fauled(self) -> bool: self.tc.skip("No suitable testbox connected to call is_dc_fauled")
+    def set_button_pressed(self, pressed: bool) -> None: self.__not_implemented()
+    def is_button_pressed(self) -> bool: self.__not_implemented()
 
-    def set_gpio_shutdown_closed(self, val: bool) -> None: self.tc.skip("No suitable testbox connected to call set_gpio_shutdown_closed")
-    def is_gpio_shutdown_closed(self) -> None: self.tc.skip("No suitable testbox connected to call is_gpio_shutdown_closed")
+    def set_dc_faulted(self, fault: bool, clear_after_s: int = 0) -> None: self.__not_implemented()
+    def is_dc_fauled(self) -> bool: self.__not_implemented()
 
-    def set_gpio_input_closed(self, val: bool) -> None: self.tc.skip("No suitable testbox connected to call set_gpio_input_closed")
-    def is_gpio_input_closed(self) -> None: self.tc.skip("No suitable testbox connected to call is_gpio_input_closed")
+    def set_gpio_shutdown_closed(self, val: bool) -> None: self.__not_implemented()
+    def is_gpio_shutdown_closed(self) -> None: self.__not_implemented()
 
-    def is_gpio_output_closed(self) -> bool: self.tc.skip("No suitable testbox connected to call is_gpio_output_closed")
+    def set_gpio_input_closed(self, val: bool) -> None: self.__not_implemented()
+    def is_gpio_input_closed(self) -> None: self.__not_implemented()
 
-    def connect_meter(self, m: Meter): self.tc.skip("No suitable testbox connected to call connect_meter")
-    def get_meter_connected(self): self.tc.skip("No suitable testbox connected to call get_meter_connected")
+    def is_gpio_output_closed(self) -> bool: self.__not_implemented()
 
-    def is_contactor_closed(self, contactor: int): self.tc.skip("No suitable testbox connected to call is_contactor_closed")
+    def connect_meter(self, m: Meter): self.__not_implemented()
+    def get_meter_connected(self): self.__not_implemented()
+
+    def is_contactor_closed(self, contactor: int): self.__not_implemented()
 
     def reset(self): ...
     def start(self, host, port): ...
