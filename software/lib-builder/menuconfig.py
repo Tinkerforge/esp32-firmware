7344503d81ff5ba8483ca778aa86592a8e3c4920
Matthias Bolte <matthias@tinkerforge.com>
2026-03-19 15:49:28 +0100

    lib-builder: Use tinkerforge_util

--- software/lib-builder/menuconfig.py
+++ software/lib-builder/menuconfig.py
@@ -4,22 +4,10 @@ import shutil
 import subprocess
 import sys
 import os
+import tinkerforge_util as tfutil
 
 import create_defconfig
 import merge_defconfig
-
-# use "with ChangedDirectory('/path/to/abc')" instead of "os.chdir('/path/to/abc')"
-class ChangedDirectory(object):
-    def __init__(self, path):
-        self.path = path
-        self.previous_path = None
-
-    def __enter__(self):
-        self.previous_path = os.getcwd()
-        os.chdir(self.path)
-
-    def __exit__(self, type_, value, traceback):
-        os.chdir(self.previous_path)
 
 if len(sys.argv) != 3:
     print("Usage: {} path/to/esp32-arduino-lib-builder path/to/defconfig".format(sys.argv[0]))
@@ -34,7 +22,7 @@ if not os.path.isfile(os.path.join(root, "configs", "defconfig.esp32")):
 shutil.move(os.path.join(root, "configs", "defconfig.esp32"), os.path.join(root, "configs", "defconfig.esp32vanilla"))
 try:
     shutil.copy(os.path.join(sys.argv[2]), os.path.join(root, "configs", "defconfig.esp32"))
-    with ChangedDirectory(os.path.join(root)):
+    with tfutil.ChangedDirectory(os.path.join(root)):
         subprocess.call(['bash', '-c', '. esp-idf/export.sh && ./build.sh -s -t esp32 -b menuconfig'], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
 
     defconfig = create_defconfig.main(["create_defconfig", os.path.join(root, "sdkconfig.vanilla"), os.path.join(root, "sdkconfig")])
