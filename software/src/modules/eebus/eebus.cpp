26e49af09240d375199c483d8c9560e96febea12
Olaf Lüke <olaf@tinkerforge.com>
2026-03-26 15:00:36 +0100

    eebus: Fix scan guard blocking its own discover_ship_peers() call

--- software/src/modules/eebus/eebus.cpp
+++ software/src/modules/eebus/eebus.cpp
@@ -665,7 +665,6 @@ void EEBus::register_urls()
                 return "scan in progress";
             }
             if (ship.discovery_state == ShipDiscoveryState::Ready || ship.discovery_state == ShipDiscoveryState::ScanDone) {
-                ship.discovery_state = ShipDiscoveryState::Scanning;
                 state.get("discovery_state")->updateEnum(ShipDiscoveryState::Scanning);
 
                 task_scheduler.scheduleOnce(
