c74ec9fef9009aadf0bce737ffe7a8517fb913a4
Erik Fleckstein <erik@tinkerforge.com>
2026-03-27 14:35:46 +0100

    charge_limits: Add comment.

--- software/src/modules/charge_limits/charge_limits.cpp
+++ software/src/modules/charge_limits/charge_limits.cpp
@@ -182,6 +182,9 @@ void ChargeLimits::apply_energy_override(uint32_t energy_wh) {
 #if OPTIONS_PRODUCT_ID_IS_WARP4()
 void ChargeLimits::apply_soc_target_override(uint32_t soc_target) {
     was_triggered = false;
+    // This is not the same pattern as the apply_duration/energy_override functions,
+    // because the SoC target is an absolute value (i.e. "charge to 80%"), not a relative
+    // one such as the duration ("charge for one hour") or the energy "charge 10 kWh").
     active_limits.get("soc_target_pct")->updateUint(override_soc.get("soc_target_pct")->asUint());
     state.get("soc_target_pct")->updateUint(override_soc.get("soc_target_pct")->asUint());
 }
