aa1e280ee20144403ecba54cd5fe2b9fd2e631f9
Frederic Henrichs <frederic@tinkerforge.com>
2026-04-13 11:25:58 +0200

    web/users: add UI to allow adding NFC-tags when adding or editing a user
    Also refactors creation of the Table component for better readability

--- software/web/src/modules/users/translation_de.tsx
+++ software/web/src/modules/users/translation_de.tsx
@@ -2,8 +2,7 @@
 import { h } from "preact";
 let x = {
     "users": {
-        "status": {
-        },
+        "status": {},
         "navbar": {
             "users": "Benutzerverwaltung"
         },
@@ -11,8 +10,10 @@ let x = {
             "users": "Benutzerverwaltung",
 
             "enable_authentication": "Anmeldung aktiviert",
-            "enable_authentication_desc": "Beim Aufrufen des Webinterfaces oder bei Verwendung der HTTP-API muss eine Anmeldung als einer der konfigurieren Benutzer durchgeführt werden",
-            "enable_authentication_invalid": "Damit die Anmeldung aktiviert sein kann, muss mindestens ein Benutzer mit einem konfigurierten Passwort vorhanden sein.",
+            "enable_authentication_desc":
+                "Beim Aufrufen des Webinterfaces oder bei Verwendung der HTTP-API muss eine Anmeldung als einer der konfigurieren Benutzer durchgeführt werden",
+            "enable_authentication_invalid":
+                "Damit die Anmeldung aktiviert sein kann, muss mindestens ein Benutzer mit einem konfigurierten Passwort vorhanden sein.",
 
             "authorized_users": "Berechtigte Benutzer",
 
@@ -27,31 +28,44 @@ let x = {
             "add_user_username": "Benutzername",
             "add_user_username_desc": "zur Anmeldung beim Webinterface",
             "add_user_display_name": "Anzeigename",
-            "add_user_display_name_desc": "zur Anzeige im Ladelog und Webinterface",
+            "add_user_display_name_desc":
+                "zur Anzeige im Ladelog und Webinterface",
             "add_user_current": "Maximaler Ladestrom",
             "add_user_password": "Passwort",
             "add_user_password_desc": "Anmeldung deaktiviert",
-            "add_user_message": /*SFN*/(have: number, max: number) => `${have} von ${max} Benutzern konfiguriert`/*NF*/,
-            "add_user_user_ids_exhausted": "Die maximale Anzahl an Benutzern hat Ladevorgänge aufgezeichnet. Damit ein neuer Benutzer angelegt werden kann, müssen aufgezeichnete Ladevorgänge oder ein existierender Benutzer ohne aufgezeichnete Ladevorgänge gelöscht werden.",
+            "add_user_message": /*SFN*/ (have: number, max: number) =>
+                `${have} von ${max} Benutzern konfiguriert` /*NF*/,
+            "add_user_user_ids_exhausted":
+                "Die maximale Anzahl an Benutzern hat Ladevorgänge aufgezeichnet. Damit ein neuer Benutzer angelegt werden kann, müssen aufgezeichnete Ladevorgänge oder ein existierender Benutzer ohne aufgezeichnete Ladevorgänge gelöscht werden.",
 
             "edit_user_title": "Benutzer bearbeiten",
             "edit_user_username": "Benutzername",
             "edit_user_username_desc": "zur Anmeldung beim Webinterface",
             "edit_user_display_name": "Anzeigename",
-            "edit_user_display_name_desc": "zur Anzeige im Ladelog und Webinterface",
+            "edit_user_display_name_desc":
+                "zur Anzeige im Ladelog und Webinterface",
             "edit_user_current": "Maximaler Ladestrom",
             "edit_user_password": "Passwort",
 
             "evse_user_description": "Ladefreigabe",
 
-            "evse_user_enable": <>Verlangt eine Freigabe des Ladevorgangs durch einen Benutzer zum Laden (z.B. per <a href="#nfc">NFC-Tag</a>)</>,
-            "evse_user_enable_invalid": "Für die Ladefreigabe ist mindestens ein Benutzer notwendig."
+            "evse_user_enable": <> Verlangt eine Freigabe des Ladevorgangs durch einen Benutzer zum Laden (z.B. per <a href="#nfc">NFC-Tag</a>) </>,
+            "evse_user_enable_invalid":
+                "Für die Ladefreigabe ist mindestens ein Benutzer notwendig.",
+
+            "nfc_tags": "NFC-Tags",
+            "nfc_no_tags_assigned": "Keine NFC-Tags zugeordnet",
+            "nfc_no_seen_tags":
+                "In letzter Zeit wurden keine NFC-Tags erkannt. Halte ein Tag an die Wallbox, um es zu erkennen.",
+            "nfc_tag_already_assigned":
+                "Bereits einem anderen Benutzer zugeordnet"
         },
         "script": {
             "reboot_content_changed": "Benutzereinstellungen",
             "login_disabled": "Anmeldung deaktiviert",
             "save_failed": "Speichern der Benutzereinstellungen fehlgeschlagen.",
-            "username_already_tracked": "Benutzername ist bereits in aufgezeichneten Ladevorgängen",
+            "username_already_tracked":
+                "Benutzername ist bereits in aufgezeichneten Ladevorgängen",
             "username_already_used": "Benutzername ist bereits vergeben ",
             "all_user_ids_in_use": "Keine freien Benutzer-IDs mehr."
         }
