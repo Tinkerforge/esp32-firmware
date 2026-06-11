1f6af07746fb96314b97568cd99bdad005d2ff37
Matthias Bolte <matthias@tinkerforge.com>
2026-06-11 12:24:44 +0200

    meters-meta: Fix typo

--- software/web/src/modules/meters_meta/translation_de.tsx
+++ software/web/src/modules/meters_meta/translation_de.tsx
@@ -12,7 +12,7 @@ let x = {
             "mode_diff": "Differenz (A - B)",
             "mode_add": "Addition (A + Konstante)",
             "mode_mul": "Multiplikation (A * Konstante)",
-            "mode_pf2current": "Gerichteter Strom abhänging vom Leistungsfaktor",
+            "mode_pf2current": "Gerichteter Strom abhängig vom Leistungsfaktor",
             "source_meter_a": "Quell-Zähler A",
             "source_meter_b": "Quell-Zähler B",
             "constant": "Konstante"
