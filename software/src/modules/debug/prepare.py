4e40c73047a6993d0a12fb74bbba408ebb578ac2
Mattias Schäffersmann <mattias@tinkerforge.com>
2026-06-15 19:57:36 +0200

    debug: Improve warning for missing embedded bootloader/partition table

--- software/src/modules/debug/prepare.py
+++ software/src/modules/debug/prepare.py
@@ -15,7 +15,7 @@ try:
     with open(bootloader_path, 'rb') as f:
         bootloader_data = f.read()
 except FileNotFoundError:
-    print("bootloader.bin not found. Re-run build when done.", file=sys.stderr)
+    print("bootloader.bin for Debug module not found. Re-run build when done.", file=sys.stderr)
 
 util.embed_data_with_digest(bootloader_data, 'generated', 'embedded_bootloader', 'uint8_t', 'size_t')
 
@@ -26,7 +26,7 @@ try:
     with open(partition_table_path, 'rb') as f:
         partition_table_data = f.read()
 except FileNotFoundError:
-    print("partitions.bin not found. Re-run build when done.", file=sys.stderr)
+    print("partitions.bin for Debug module not found. Re-run build when done.", file=sys.stderr)
 
 for i in range(len(partition_table_data), 0, -1):
     if partition_table_data[i - 1] != 0xFF:
