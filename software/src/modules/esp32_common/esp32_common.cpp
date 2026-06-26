63c3787e01836a0c4c829d0717a2e0e2c4163464
Erik Fleckstein <erik@tinkerforge.com>
2026-06-26 15:26:04 +0200

    esp32_common: Extract LAST_BASE58_UID_NUM as option.

--- software/src/modules/esp32_common/esp32_common.cpp
+++ software/src/modules/esp32_common/esp32_common.cpp
@@ -57,7 +57,7 @@
 
 #include "gcc_warnings.h"
 
-static constexpr uint32_t LAST_BASE58_UID_NUM = 257899; // 2jEx (base58); 855m (zbase32) first zbase32 uid is 855c
+static constexpr uint32_t LAST_BASE58_UID_NUM = OPTION_ESP32_COMMON_LAST_BASE58_UID_NUM();
 static constexpr uint32_t LAST_DIRECT_WIFI_PASSPHRASE_UID_NUM = LAST_BASE58_UID_NUM;
 
 // TF_HAL hal;
