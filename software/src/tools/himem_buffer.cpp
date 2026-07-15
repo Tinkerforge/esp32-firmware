4aa6d2faaaf6218429a5991510fe59181e8e2e0a
Erik Fleckstein <erik@tinkerforge.com>
2026-07-15 09:56:25 +0200

    tools: Add himem_buffer. To be used for trace logs.

---
+++ software/src/tools/himem_buffer.cpp
@@ -0,0 +1,160 @@
+/* esp32-firmware
+ * Copyright (C) 2026 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "himem_buffer.h"
+
+#include <stdint.h>
+#include <string.h>
+
+#include <esp_system.h>
+
+#include "gcc_warnings.h"
+
+HimemBuffer::HimemBuffer() : start(0),
+                             end(0),
+                             buf_size(0),
+                             block_count(0),
+                             buf_size_bits(0),
+                             block_count_bits(0),
+                             mh(nullptr)
+    {}
+
+void HimemBuffer::setup(size_t size)
+{
+    static_assert(std::has_single_bit(static_cast<uint32_t>(BLOCK_SIZE)));
+    if (!std::has_single_bit(size))
+        esp_system_abort("HimemBuffer: size must be power of two");
+
+    if (size < 2 * BLOCK_SIZE)
+        esp_system_abort("HimemBuffer: size must be at least BLOCK_SIZE * 2");
+
+    if (size % BLOCK_SIZE != 0)
+        esp_system_abort("HimemBuffer: size must be multiple of BLOCK_SIZE");
+
+    this->buf_size = size;
+    esp_himem_alloc(this->buf_size, &mh);
+
+    this->block_count = this->buf_size / BLOCK_SIZE;
+
+    if (!std::has_single_bit(this->block_count))
+        esp_system_abortf<96>("HimemBuffer: block_count must be power of two; is %zu", this->block_count);
+
+    this->buf_size_bits    = static_cast<size_t>(31 - __builtin_clz(this->buf_size));
+    this->block_count_bits = static_cast<size_t>(31 - __builtin_clz(this->block_count));
+}
+
+void HimemBuffer::clear()
+{
+    start = 0;
+    end = 0;
+}
+
+void HimemBuffer::push_n(const void *val, size_t n, esp_himem_rangehandle_t rh) {
+    if (n > size())
+        return;
+
+    bool fits = n <= free();
+
+    while (end + n > buf_size) {
+        size_t to_write = buf_size - end;
+        this->write(rh, end, val, to_write);
+        val = static_cast<const void *>(static_cast<const std::byte *>(val) + to_write);
+        n -= to_write;
+        end = 0;
+    }
+
+    this->write(rh, end, val, n);
+    end = mod_buf_size(end + n);
+
+    if (!fits) {
+        start = mod_buf_size(end + 1);
+    }
+}
+
+size_t HimemBuffer::used_blocks() {
+    if (empty())
+        return 0;
+
+    size_t start_block;
+    size_t start_offset;
+    block_and_offset(start, &start_block, &start_offset);
+
+    size_t end_block;
+    size_t end_offset;
+    block_and_offset(end, &end_block, &end_offset);
+
+    return end_block + (end >= start ? 0 : block_count) - start_block + (end_offset == 0 ? 0 : 1);
+}
+
+void *HimemBuffer::map_block(size_t block_idx, esp_himem_rangehandle_t rh, size_t *out_block_len) {
+    size_t start_block;
+    size_t start_offset;
+    block_and_offset(start, &start_block, &start_offset);
+
+    size_t end_block;
+    size_t end_offset;
+    block_and_offset(end, &end_block, &end_offset);
+
+    size_t block = ((start_block + block_idx) % block_count);
+
+    void *result;
+    esp_himem_map(this->mh, rh, block << BLOCK_SIZE_BITS, 0, BLOCK_SIZE, 0, &result);
+
+    size_t block_len = BLOCK_SIZE;
+    // block_idx == 0 is almost identical to block == start_block
+    // Only if used_blocks returned block_count + 1, i.e. when all blocks are in use
+    // and start is behind end, block == start_block, but block_idx != 0
+    // Don't apply start offset in that case.
+    if (block_idx == 0) {
+        result = static_cast<void *>(static_cast<std::byte *>(result) + start_offset);
+        block_len -= start_offset;
+    }
+
+    if (block == end_block && (start < end || block_idx != 0)) {
+        block_len -= BLOCK_SIZE - end_offset;
+    }
+
+    if (out_block_len != nullptr) {
+        *out_block_len = block_len;
+    }
+    return result;
+}
+
+void HimemBuffer::unmap_block(void *block, esp_himem_rangehandle_t rh) {
+    esp_himem_unmap(rh,
+                    reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(block) & (~(BLOCK_SIZE - 1))),
+                    BLOCK_SIZE);
+}
+
+void HimemBuffer::write(esp_himem_rangehandle_t rh, size_t offset, const void *src, size_t len) {
+    while(len > 0) {
+        size_t offset_in_block = mod_block_size(offset);
+        size_t block_offset = offset - offset_in_block;
+        size_t to_write = std::min(BLOCK_SIZE - offset_in_block, len);
+        void *block;
+
+        esp_himem_map(this->mh, rh, block_offset, 0, BLOCK_SIZE, 0, &block);
+        memcpy(static_cast<std::byte *>(block) + offset_in_block, src, to_write);
+        esp_himem_unmap(rh, block, BLOCK_SIZE);
+
+        len -= to_write;
+        offset += to_write;
+        src = static_cast<const void *>(static_cast<const std::byte *>(src) + to_write);
+    }
+}
---
+++ software/src/tools/himem_buffer.h
@@ -0,0 +1,101 @@
+/* esp32-firmware
+ * Copyright (C) 2026 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <bit>
+
+#include <esp32/himem.h>
+
+#include "tools.h"
+
+class HimemBuffer {
+public:
+    HimemBuffer();
+
+    void setup(size_t size);
+
+    [[gnu::always_inline]] inline bool  empty() { return start == end;}
+    [[gnu::always_inline]] inline size_t size() { return buf_size - 1; }
+    [[gnu::always_inline]] inline size_t used() { return mod_buf_size(buf_size + end - start); }
+    [[gnu::always_inline]] inline size_t free() { return size() - used(); }
+
+    // write
+    void clear();
+    void push_n(const void *val, size_t n, esp_himem_rangehandle_t rh);
+    template<typename T> void pop_until(T needle, esp_himem_rangehandle_t rh) {
+        static_assert(std::has_single_bit(sizeof(T)));
+        static_assert(BLOCK_SIZE % sizeof(T) == 0);
+
+        size_t blocks = this->used_blocks();
+
+        for (size_t block_idx = 0; block_idx < blocks; ++block_idx) {
+            size_t block_len;
+            T *block = reinterpret_cast<T *>(this->map_block(block_idx, rh, &block_len));
+            defer {this->unmap_block(block, rh); };
+
+            for (size_t i = 0; i < block_len / sizeof(T); ++i) {
+                if (block[i] == needle) {
+                    start = mod_buf_size(start + block_idx * BLOCK_SIZE + i * sizeof(T) + 1);
+                    return;
+                }
+            }
+        }
+    }
+
+    // read
+    size_t used_blocks();
+    void *map_block(size_t block_idx, esp_himem_rangehandle_t rh, size_t *out_block_len);
+    void unmap_block(void *block, esp_himem_rangehandle_t rh);
+
+    static constexpr size_t BLOCK_SIZE = ESP_HIMEM_BLKSZ;
+    static constexpr size_t BLOCK_SIZE_BITS = 31 - __builtin_clz(BLOCK_SIZE);
+
+    static constexpr size_t MIN_BUFFER_SIZE = 2 * BLOCK_SIZE;
+
+private:
+    // index of first valid byte
+    size_t start;
+    // index of first invalid byte
+    size_t end;
+    size_t buf_size;
+    size_t block_count;
+
+    size_t buf_size_bits;
+    size_t block_count_bits;
+
+    esp_himem_handle_t mh;
+
+    [[gnu::always_inline]]
+    void block_and_offset(size_t index, size_t *out_block, size_t *out_offset) {
+        *out_offset = mod_block_size(index);
+        *out_block  = div_block_size(index);
+    }
+
+    void write(esp_himem_rangehandle_t rh, size_t offset, const void *src, size_t len);
+
+    //void read(esp_himem_rangehandle_t rh, size_t offset, void *dst, size_t len);
+
+    [[gnu::always_inline]] inline size_t mod_buf_size(size_t x) { return x & (buf_size - 1); }
+
+    [[gnu::always_inline]] inline size_t mod_block_size(size_t x) { return x & (BLOCK_SIZE - 1); }
+    [[gnu::always_inline]] inline size_t div_block_size(size_t x) { return x >> BLOCK_SIZE_BITS; }
+};
