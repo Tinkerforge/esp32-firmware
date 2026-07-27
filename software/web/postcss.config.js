3b387367d832cabdaa3fdac2e78a1ce667bf9b77
Frederic Henrichs <frederic@tinkerforge.com>
2026-07-27 14:05:32 +0200

    web: adjust to TypeScript 7 changes

--- software/web/postcss.config.js
+++ software/web/postcss.config.js
@@ -2,7 +2,12 @@ module.exports = {
     plugins: [
         require('autoprefixer'),
         require('cssnano') ({
-            preset: 'advanced',
+            preset: ['advanced', {
+                // postcss-svgo warns on Bootstrap's percent-encoded inline SVG values
+                // generated for form switch backgrounds (e.g. `rgb(...%%29`) in newer Sass).
+                // Disable only svgo to keep the rest of cssnano optimizations.
+                svgo: false,
+            }],
         }),
     ],
 };
