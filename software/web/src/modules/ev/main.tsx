4dd9e83bf2b46299ef834f7d827ba2bbd853d565
Olaf Lüke <olaf@tinkerforge.com>
2026-08-21 13:41:17 +0200

    web/ev: Make sure PageHeader is also shown on WEM

--- software/web/src/modules/ev/main.tsx
+++ software/web/src/modules/ev/main.tsx
@@ -35,6 +35,9 @@ import { DiscoveryResultItem, DiscoveryResultGroup } from "../../ts/components/d
 import { Plus } from "react-feather";
 import { get_charge_manager_auth_info } from "../users/main"
 import { CMAuthType } from "../cm_networking/generated/cm_auth_type.enum";
+//#ifn MODULE_EVSE_COMMON_AVAILABLE
+import { PageHeader } from "../../ts/components/page_header";
+//#endif
 
 export const EvSymbol = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2"/>
@@ -278,6 +281,9 @@ export class Ev extends Component<{}, EvState> {
                         <InputText value={__("ev.content.no_active_ev")} />
                     </FormRow>}
                 </SubPage.Status>
+{/*#else*/}
+                {/* Without a SubPage.Status section, there is no header. Add one explicitly for WEM. */}
+                <PageHeader title={__("ev.content.ev")} />
 {/*#endif*/}
 
                 <FormRow label={__("ev.content.ev_profiles")} label_muted={__("ev.content.ev_profiles_desc")}>
