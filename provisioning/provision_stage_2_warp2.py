4355dab0bda5d4dac169f8035afcb68ec51c3d16
Matthias Bolte <matthias@tinkerforge.com>
2026-09-03 11:36:02 +0200

    provisioning: numlockx needs DISPLAY to be set

--- provisioning/provision_stage_2_warp2.py
+++ provisioning/provision_stage_2_warp2.py
@@ -389,10 +389,13 @@ def reset_evse():
 
 class Scanner:
     def __init__(self):
-        try:
-            subprocess.check_call(['numlockx', 'on'])
-        except:
-            fatal_error('Could not turn numlock on. Is numlockx installed?')
+        if os.getenv('DISPLAY') == None:
+            print('DISPLAY not set, skipping "numlockx on" call')
+        else:
+            try:
+                subprocess.check_call(['numlockx', 'on'])
+            except:
+                fatal_error('Could not turn numlock on. Is numlockx installed?')
 
         # T:WARP2-CP-22KW-50;V:2.1;S:5000000001;B:2021-09;A:0;;;
         pattern_4 = r'^T:(WARP(4)-C(S|P|E)-(SS|PC)-((?:11|22)?(?:50|75)?|CC)-(W|C));V:(\d+\.\d+);S:(5\d{9});B:(\d{4}-\d{2})(?:;A:(0|1))?(?:;Z:(0|1))?(?:;O:(SO/(:?B\d{7}|\d{5})|))?;;;*$'
