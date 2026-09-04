7c1c513b842ee01b1e1c9082178f6a5cbde65bd1
Olaf Lüke <olaf@tinkerforge.com>
2026-09-04 18:47:12 +0200

    web_server: Add support for https redirect in https-only mode

--- software/src/modules/web_server/tests/connectivity.py
+++ software/src/modules/web_server/tests/connectivity.py
@@ -8,6 +8,9 @@ tfutil.create_parent_module(__file__, 'software')
 from software.test_runner.test_context import run_testsuite, TestContext
 
 def test_http_connectivity(tc: TestContext):
+    https_port = tc.api('network/config/web_server_port_secure')
+    https_authority = tc._esp_host if https_port == 443 else f'{tc._esp_host}:{https_port}'
+
     # Check if the index page can be downloaded via HTTP and is larger than 100 KiB
     try:
         data = tc.http_request('GET', '/')
@@ -21,31 +24,34 @@ def test_http_connectivity(tc: TestContext):
         tc.api('network/config/transport_mode', 2)
         tc.reboot()
 
-    https_index_response = requests.get(f'https://{tc._esp_host}/', verify=False)
+    https_index_response = requests.get(f'https://{https_authority}/', verify=False)
 
     tc.assert_eq(200, https_index_response.status_code)
     tc.assert_gt(50*1024, len(https_index_response.content))
 
-    # Enable HTTPS-only mode, execute requests and enable HTTP+HTTPS mode again before checking assertions
+    # Enable HTTPS-only mode and reboot through HTTP while it is still available.
     tc.api('network/config/transport_mode', 1)
 
-    try:
-        tc.reboot()
-    except HTTPError as e:
-        forbidden_message = e.msg
-
-    http_forbidden_response = requests.get(f'http://{tc._esp_host}/', verify=False)
-
-    api_response = requests.put(f'https://{tc._esp_host}/network/config/transport_mode', data='2', verify=False)
-    requests.get(f'https://{tc._esp_host}/reboot', verify=False)
+    tc.api('reboot', '')
     time.sleep(10)
 
-    # Check for 403 when checking status after reboot
-    tc.assert_eq('Forbidden:HTTP disabled; use HTTPS instead', forbidden_message)
+    http_redirect_response = requests.get(
+        f'http://{tc._esp_host}/redirect_test?foo=bar',
+        allow_redirects=False,
+    )
+    redirected_index_response = requests.get(f'http://{tc._esp_host}/', verify=False)
 
-    # Check that the index page returned 403 via HTTP when HTTP was off
-    tc.assert_eq(403, http_forbidden_response.status_code)
-    tc.assert_eq('HTTP disabled; use HTTPS instead', http_forbidden_response.text)
+    api_response = requests.put(f'https://{https_authority}/network/config/transport_mode', data='2', verify=False)
+    requests.get(f'https://{https_authority}/reboot', verify=False)
+    time.sleep(10)
+
+    # Check that HTTP redirects to HTTPS while HTTP is off.
+    tc.assert_eq(307, http_redirect_response.status_code)
+    tc.assert_eq(f'https://{https_authority}/redirect_test?foo=bar', http_redirect_response.headers['Location'])
+    tc.assert_eq(200, redirected_index_response.status_code)
+    tc.assert_eq('https', redirected_index_response.url.split(':', 1)[0])
+    tc.assert_eq(1, len(redirected_index_response.history))
+    tc.assert_eq(307, redirected_index_response.history[0].status_code)
 
     # Check final API call to make sure the test exits with the host correctly configured
     tc.assert_eq(200, api_response.status_code)
