Tinkerforge/esp32-firmware

commit 5c8c703d4e9fe3f94dacda04ef62bc31d536c0fe

debug_logger, event_log: Merge debug report downloads.
This ensures that an EVSE debug protocol also includes trace log
and coredump and adds separators between each log part. Use
^___.*___$ to find all separators or ^___.*BEGIN.*___$ to split log
into parts.

software/web/src/ts/components/debug_logger.tsx
@@ -36,6 +36,91 @@ interface DebugLoggerProps {
     filename: string
 }
 
+interface FetchDebugReportParams {
+        prefix?: string,
+        progress?: (status: 'running' | 'done' | 'error', msg: string) => Promise<void>,
+        content?: ('debug_report' | 'event_log' | 'trace_log' | 'coredump')[]
+        override_fetch_event_log?: () => Promise<string>
+}
+
+export async function fetch_debug_report(p: Partial<FetchDebugReportParams>) {
+    let text = "";
+
+    const {
+        prefix = "",
+        progress = ((status, msg) => {}),
+        content = ['debug_report', 'event_log', 'trace_log', 'coredump'],
+        override_fetch_event_log = () => util.download("/event_log", true).then(blob => blob.text())
+    } = p;
+
+    if (content.indexOf('debug_report') >= 0) {
+        await progress('running', __("component.debug_logger.loading_debug_report"));
+        text += `___${prefix}DEBUG_REPORT_START___\n\n`;
+
+        try {
+            text += await util.download("/debug_report", true).then(blob => blob.text()) + "\n\n";
+        } catch (e) {
+            await progress('error', __("component.debug_logger.loading_debug_report_failed"));
+            throw __("component.debug_logger.loading_debug_report_failed") + ": " + e;
+        }
+
+        text += `___${prefix}DEBUG_REPORT_END___\n\n`;
+    }
+
+    if (content.indexOf('event_log') >= 0) {
+        await progress('running', __("component.debug_logger.loading_event_log"));
+        text += `___${prefix}EVENT_LOG_START___\n\n`;
+
+        try {
+            text += await override_fetch_event_log() + "\n\n";
+        } catch (e) {
+            await progress('error', __("component.debug_logger.loading_event_log_failed"));
+            throw __("component.debug_logger.loading_event_log_failed") + ": " + e;
+        }
+
+        text += `___${prefix}EVENT_LOG_END___\n\n`;
+    }
+
+    if (content.indexOf('trace_log') >= 0) {
+        await progress('running', __("component.debug_logger.loading_trace_log"));
+        text += `___${prefix}TRACE_LOG_START___\n\n`;
+
+        try {
+            text += (await util.download("/trace_log", true, 40000).then(blob => blob.text())).replace(/\s+$/, "") + "\n\n";
+        }
+        catch (e) {
+            await progress('error', __("component.debug_logger.loading_trace_log_failed"));
+            throw __("component.debug_logger.loading_trace_log_failed") + ": " + e;
+        }
+
+        text += `___${prefix}TRACE_LOG_END___\n\n`;
+    }
+
+    if (content.indexOf('coredump') >= 0) {
+        await progress('running', __("component.debug_logger.loading_coredump"));
+        text += `___${prefix}COREDUMP_START___\n\n`;
+
+        try {
+            text += (await util.download("/coredump/coredump.elf", true).then(util.blobToBase64)).replace(/(.{80})/g, "$1\n").trim() + "\n\n";
+        }
+        catch (e) {
+            const msg = typeof(e) == "string" ? e : e?.message;
+            if (!msg || !msg.startsWith("404")) {
+                await progress('error', __("component.debug_logger.loading_coredump_failed"));
+                throw __("component.debug_logger.loading_coredump_failed") + ": " + e;
+
+            }
+            text += "No core dump stored\n\n";
+        }
+
+        text += `___${prefix}COREDUMP_END___\n\n`;
+    }
+
+    await progress('done', "");
+
+    return text;
+}
+
 export class DebugLogger extends Component<DebugLoggerProps, DebugLoggerState>
 {
     debug_prefix: string = '';
@@ -66,43 +151,6 @@ export class DebugLogger extends Component<DebugLoggerProps, DebugLoggerState>
         }, false);
     }
 
-    async get_debug_report_and_event_log() {
-        let text = '';
-
-        try {
-            this.setState({debug_status: __("component.debug_logger.loading_debug_report")});
-            text += await util.download("/debug_report", true).then(blob => blob.text()) + "\n\n";
-        } catch (error) {
-            this.setState({debug_running: false, debug_status: __("component.debug_logger.loading_debug_report_failed")});
-            throw __("component.debug_logger.loading_debug_report_failed") + ": " + error;
-        }
-
-        try {
-            this.setState({debug_status: __("component.debug_logger.loading_event_log")});
-            text += await util.download("/event_log", true).then(blob => blob.text()) + "\n";
-        } catch (error) {
-            this.setState({debug_running: false, debug_status: __("component.debug_logger.loading_event_log_failed")});
-            throw __("component.debug_logger.loading_event_log_failed") + ": " + error;
-        }
-
-        try {
-            this.setState({debug_status: __("component.debug_logger.loading_trace_log")});
-            const trace_log_uri = "/trace_log";
-            const trace_log = (await util.download(trace_log_uri, true, 40000).then(blob => blob.text())).replace(/\s+$/, "");
-
-            if (trace_log.length > 0) {
-                text += "\n\n___TRACE_LOG_START___\n\n";
-                text += trace_log + "\n";
-            }
-        }
-        catch (error) {
-            this.setState({debug_running: false, debug_status: __("component.debug_logger.loading_trace_log_failed")});
-            throw __("component.debug_logger.loading_trace_log_failed") + ": " + error;
-        }
-
-        return text;
-    }
-
     debugTimeout: number;
 
     async resetDebugWd() {
@@ -123,7 +171,11 @@ export class DebugLogger extends Component<DebugLoggerProps, DebugLoggerState>
         this.setState({debug_running: true});
 
         try {
-            this.debug_prefix = await this.get_debug_report_and_event_log();
+            this.debug_prefix = await fetch_debug_report({
+                prefix: "PRE_",
+                progress: async (status, msg) => this.setState({debug_status: msg}),
+                content: ['debug_report', 'event_log', 'trace_log']
+            });
 
             this.setState({debug_status: __("component.debug_logger.starting_debug")(this.props.name)});
         } catch(error) {
@@ -155,7 +207,10 @@ export class DebugLogger extends Component<DebugLoggerProps, DebugLoggerState>
 
         try {
             this.setState({debug_status: __("component.debug_logger.debug_stopped")(this.props.name)});
-            this.debug_suffix = "\n" + await this.get_debug_report_and_event_log();
+            this.debug_suffix = "\n" + await fetch_debug_report({
+                prefix: "POST_",
+                progress: async (status, msg) => this.setState({debug_status: msg})
+            });
             this.setState({debug_status: __("component.debug_logger.debug_done")});
         } catch (error) {
             this.debug_suffix = "\nError while stopping charge protocol: " + error;
@@ -164,13 +219,18 @@ export class DebugLogger extends Component<DebugLoggerProps, DebugLoggerState>
 
         let full_log = [this.debug_prefix];
 
+        full_log.push("___DEBUG_PROTOCOL_START___\n\n");
+
         if (this.debug_protocol_lines_dropped > 0) {
             full_log.push('' + this.debug_protocol_lines_dropped + ' lines have been dropped from the following table.\n\n');
         }
-
         full_log.push(this.debug_protocol_header);
         full_log = full_log.concat(this.debug_protocol_lines);
+
+        full_log.push("___DEBUG_PROTOCOL_END___\n\n");
+
         full_log.push(this.debug_suffix);
+
 
         // Download log in any case: Even an incomplete log can be useful for debugging.
         util.downloadToTimestampedFile(full_log.join(''), this.props.filename, "txt", "text/plain");

software/web/src/modules/event_log/main.tsx
@@ -26,7 +26,7 @@ import { SubPage } from "../../ts/components/sub_page";
 import { OutputTextarea } from "../../ts/components/output_textarea";
 import { NavbarItem } from "../../ts/components/navbar_item";
 import { Download, FileText } from "react-feather";
-import { blobToBase64 } from "../../ts/util";
+import { fetch_debug_report } from "../../ts/components/debug_logger";
 
 export function EventLogNavbar() {
     return <NavbarItem name="event_log" module="event_log" title={__("event_log.navbar.event_log")} symbol={<FileText />} />;
@@ -170,38 +170,10 @@ export class EventLog extends Component<{}, EventLogState> {
 
         try {
             let timestamp = new Date();
-            let debug_log = util.iso8601ButLocal(timestamp) + "\nScroll down for event log!\n\n";
 
-            debug_log += await util.download("/debug_report", true).then(blob => blob.text());
-            debug_log += "\n\n";
-            debug_log += this.state.log;
-
-            //const trace_log_uri = "/trace_log" + (util.remoteAccessMode ? "/10020" : ""); // Use greedy level 20 compression to download the trace log when in remote access mode.
-            const trace_log_uri = "/trace_log";
-            const trace_log = (await util.download(trace_log_uri, true, 40000).then(blob => blob.text())).replace(/\s+$/, "");
-
-            if (trace_log.length > 0) {
-                debug_log += "\n\n___TRACE_LOG_START___\n\n";
-                debug_log += trace_log + "\n";
-            }
-
-            try {
-                let blob = await util.download("/coredump/coredump.elf", true);
-                let base64 = await blobToBase64(blob);
-                base64 = base64.replace(/(.{80})/g, "$1\n");
-                debug_log += "\n\n___CORE_DUMP_START___\n\n";
-                debug_log += base64;
-            }
-            catch (e) {
-                const msg = typeof(e) == "string" ? e : e?.message;
-                if (!msg) {
-                    debug_log += "\n\nAn unknown error occurred while trying to download the core dump.";
-                } else if (msg.startsWith("404")) {
-                    debug_log += "\n\nNo core dump stored";
-                } else {
-                    debug_log += "\n\nFailed to download core dump: '" + msg + "'";
-                }
-            }
+            const debug_log = await fetch_debug_report({
+                override_fetch_event_log: async () => this.state.log
+            })
 
             util.downloadToTimestampedFile(debug_log, __("event_log.content.debug_report_file"), "txt", "text/plain", timestamp);
         } catch (e) {

software/web/translation_de.tsx
@@ -128,6 +128,8 @@ let x = {
             "loading_event_log_failed": "Laden des Event-Logs fehlgeschlagen",
             "loading_trace_log": "Lade Trace-Log",
             "loading_trace_log_failed": "Laden des Trace-Logs fehlgeschlagen",
+            "loading_coredump": "Lade Coredump",
+            "loading_coredump_failed": "Laden des Coredumps fehlgeschlagen",
             "tab_close_warning": /*SFN*/ (debug_module: string) => `Die Aufzeichnung des ${debug_module}-Logs wird abgebrochen, wenn der Tab geschlossen wird.`/*NF*/,
             "starting_debug": /*SFN*/ (debug_module: string) => `Aktiviere Aufzeichnung des ${debug_module}-Logs.`/*NF*/,
             "starting_debug_failed": /*SFN*/ (debug_module: string) => `Aktivierung der Aufzeichnung des ${debug_module}-Logs fehlgeschlagen.`/*NF*/,

software/web/translation_en.tsx
@@ -128,6 +128,8 @@ let x = {
             "loading_event_log_failed": "Loading event log failed",
             "loading_trace_log": "Loading trace log",
             "loading_trace_log_failed": "Loading trace log failed",
+            "loading_coredump": "Loading coredump",
+            "loading_coredump_failed": "Loading coredump failed",
             "tab_close_warning": /*SFN*/ (debug_module: string) => `${debug_module} capture will be aborted if this tab is closed.`/*NF*/,
             "starting_debug": /*SFN*/ (debug_module: string) => `Starting to record ${debug_module} log`/*NF*/,
             "starting_debug_failed": /*SFN*/ (debug_module: string) => `Starting to record ${debug_module} log failed`/*NF*/,

commit 11e75fd318543b05ed41ff2fd75cf234c29d2909

debug_logger. Fix accidential rename. Broken in 5c8c703d.

software/web/src/ts/components/debug_logger.tsx
@@ -98,7 +98,7 @@ export async function fetch_debug_report(p: Partial<FetchDebugReportParams>) {
 
     if (content.indexOf('coredump') >= 0) {
         await progress('running', __("component.debug_logger.loading_coredump"));
-        text += `___${prefix}COREDUMP_START___\n\n`;
+        text += `___${prefix}CORE_DUMP_START___\n\n`;
 
         try {
             text += (await util.download("/coredump/coredump.elf", true).then(util.blobToBase64)).replace(/(.{80})/g, "$1\n").trim() + "\n\n";
@@ -113,7 +113,7 @@ export async function fetch_debug_report(p: Partial<FetchDebugReportParams>) {
             text += "No core dump stored\n\n";
         }
 
-        text += `___${prefix}COREDUMP_END___\n\n`;
+        text += `___${prefix}CORE_DUMP_END___\n\n`;
     }
 
     await progress('done', "");